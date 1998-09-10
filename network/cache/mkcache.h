035351d94000b8a1d356314e1049d2d7078b08b4
gagan%netscape.com <gagan%netscape.com>
1998-09-10 20:11:13 +0000

    Remaining changes from Nu_Cache. Added nucacheproto.c/h.

--- network/cache/mkcache.h
+++ network/cache/mkcache.h
@@ -25,14 +25,16 @@
 #include "extcache.h"
 #endif
 
+#ifndef NU_CACHE
 /* trace variable for cache testing */
 extern PRBool NET_CacheTraceOn;
+#endif /* This is handled by CacheTrace_Enable() and CacheTrace_IsEnabled() in the NU world */
 
 PR_BEGIN_EXTERN_C
-
+#ifndef NU_CACHE
 /* public accessor function for netcaster */
 extern PRBool NET_CacheStore(net_CacheObject *cacheObject, URL_Struct *url_s, PRBool accept_partial_files);
-
+#endif /* NU_CACHE */
 PR_END_EXTERN_C
 
 #endif /* MKCACHE_H */
