3a066d624c129d446aaec9070bd456853067b22f
Jerry.Kirk%Nexwarecorp.com <Jerry.Kirk%Nexwarecorp.com>
1999-06-03 01:13:06 +0000

    - These files bring the Photon UI uptodate. - They still include lots of debug that will be cleaned out later -  once everything works.

---
+++ widget/src/photon/nsLabel.h
@@ -0,0 +1,61 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * The contents of this file are subject to the Netscape Public License
+ * Version 1.0 (the "NPL"); you may not use this file except in
+ * compliance with the NPL.  You may obtain a copy of the NPL at
+ * http://www.mozilla.org/NPL/
+ *
+ * Software distributed under the NPL is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the NPL
+ * for the specific language governing rights and limitations under the
+ * NPL.
+ *
+ * The Initial Developer of this code under the NPL is Netscape
+ * Communications Corporation.  Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.  All Rights
+ * Reserved.
+ */
+
+#ifndef nsLabel_h__
+#define nsLabel_h__
+
+#include "nsWidget.h"
+#include "nsIAppShell.h"
+#include "nsILabel.h"
+
+/**
+ * Native Photon Label wrapper
+ */
+class nsLabel :  public nsWidget,
+                 public nsILabel
+{
+
+public:
+
+  nsLabel();
+  virtual ~nsLabel();
+
+  // nsISupports
+  NS_IMETHOD_(nsrefcnt) AddRef();
+  NS_IMETHOD_(nsrefcnt) Release();
+  NS_IMETHOD QueryInterface(const nsIID& aIID, void** aInstancePtr);
+
+  // nsILabel part
+  NS_IMETHOD SetLabel(const nsString &aText);
+  NS_IMETHOD GetLabel(nsString &aBuffer);
+  NS_IMETHOD SetAlignment(nsLabelAlignment aAlignment);
+
+  NS_IMETHOD PreCreateWidget(nsWidgetInitData *aInitData);
+
+  virtual PRBool OnMove(PRInt32 aX, PRInt32 aY);
+  virtual PRBool OnPaint(nsPaintEvent &aEvent);
+  virtual PRBool OnResize(nsSizeEvent &aEvent);
+
+protected:
+  NS_METHOD CreateNative( PtWidget_t *aParent );
+
+  nsLabelAlignment mAlignment;
+
+};
+
+#endif // nsLabel_h__
