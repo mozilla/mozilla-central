c253059afafb09ee6b27e3767bb1e487e050ba0f
rginda%netscape.com <rginda%netscape.com>
2002-07-18 00:54:32 +0000

    - venkman only - bug 157638, "Land VENKMAN_FLOATS_MY_BOAT branch", a=scc Landing the 3 month old branch, which fixes the following bugs... 121409, 103898, 116102, 116099, 127750, 127099, 121501, 127102, 127737, 127733, 150481, 156776, 156769, 153381, 153066, 152946, 127752, 116286, 143682, 130398, 129692, 156111, 127736, 130050, 139565, 128604, 127751, 127732, 127727, 103425, 85634, 139557, 125394

---
+++ extensions/venkman/resources/content/venkman-floater.js
@@ -0,0 +1,71 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/ 
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License. 
+ *
+ * The Original Code is The JavaScript Debugger
+ * 
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation
+ * Portions created by Netscape are
+ * Copyright (C) 1998 Netscape Communications Corporation.
+ *
+ * Alternatively, the contents of this file may be used under the
+ * terms of the GNU Public License (the "GPL"), in which case the
+ * provisions of the GPL are applicable instead of those above.
+ * If you wish to allow use of your version of this file only
+ * under the terms of the GPL and not to allow others to use your
+ * version of this file under the MPL, indicate your decision by
+ * deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL.  If you do not delete
+ * the provisions above, a recipient may use your version of this
+ * file under either the MPL or the GPL.
+ *
+ * Contributor(s):
+ *  Robert Ginda, <rginda@netscape.com>, original author
+ *
+ */
+
+var dd       = opener.dd;
+var console  = opener.console;
+var dispatch = console.dispatch;
+var windowId;
+
+function onLoad()
+{
+    var ary = document.location.search.match(/(?:\?|&)id=([^&]+)/);
+    if (!ary)
+    {
+        dd ("No window id in url " + document.location);
+        return;
+    }
+    
+    windowId = ary[1];
+    
+    if ("arguments" in window && 0 in window.arguments &&
+        typeof window.arguments[0] == "function")
+    {
+        window.arguments[0](window);
+    }
+
+    if (console.prefs["menubarInFloaters"])
+        console.createMainMenu (window.document);
+}
+
+function onClose()
+{
+    window.isClosing = true;
+    return true;
+}
+
+function onUnload()
+{
+    console.viewManager.destroyWindow (windowId);
+}
