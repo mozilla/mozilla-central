6673301edc3214644cf58fba44483eb33f61138d
alecf%netscape.com <alecf%netscape.com>
2003-04-17 05:42:33 +0000

    fix for bug 201982 - r=brade, sr=smfr nsIEditProperty was an interface with no methods and only static members - moved it to an entirely static class.

--- editor/libeditor/base/nsEditProperty.h
+++ editor/libeditor/base/nsEditProperty.h
@@ -20,7 +20,6 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *   Pierre Phaneuf <pp@ludusdesign.com>
  *   Daniel Glazman <glazman@netscape.com>
  *
  *
@@ -38,45 +37,30 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include "nsMemory.h"
-#include "nsStaticAtom.h"
-#include "nsEditProperty.h"
+#ifndef __nsEditProperty_h__
+#define __nsEditProperty_h__
 
+#include "nsISupports.h"
 
-#define EDITOR_ATOM(name_, value_) nsIAtom* nsEditProperty::name_ = 0;
-#include "nsEditPropertyAtomList.h"
-#undef EDITOR_ATOM
+class nsIAtom;
+class nsString;
 
-/* From the HTML 4.0 DTD, 
+/** simple interface for describing a single property as it relates to a range of content.
+  *
+  */
 
-INLINE:
-<!-- %inline; covers inline or "text-level" elements -->
- <!ENTITY % inline "#PCDATA | %fontstyle; | %phrase; | %special; | %formctrl;">
- <!ENTITY % fontstyle "TT | I | B | BIG | SMALL">
- <!ENTITY % phrase "EM | STRONG | DFN | CODE |
-                    SAMP | KBD | VAR | CITE | ABBR | ACRONYM" >
- <!ENTITY % special
-    "A | IMG | OBJECT | BR | SCRIPT | MAP | Q | SUB | SUP | SPAN | BDO">
- <!ENTITY % formctrl "INPUT | SELECT | TEXTAREA | LABEL | BUTTON">
-
-BLOCK:
-<!ENTITY % block
-      "P | %heading (h1-h6); | %list (UL | OL); | %preformatted (PRE); | DL | DIV | NOSCRIPT |
-       BLOCKQUOTE | FORM | HR | TABLE | FIELDSET | ADDRESS">
-
-But what about BODY, TR, TD, TH, CAPTION, COL, COLGROUP, THEAD, TFOOT, LI, DT, DD, LEGEND, etc.?
- 
-
-*/
-void
-nsEditProperty::RegisterAtoms()
+class nsEditProperty
 {
-  // inline tags
-  static const nsStaticAtom property_atoms[] = {
-#define EDITOR_ATOM(name_, value_) { value_, &name_ },
+public:
+
+    static void RegisterAtoms();
+
+#define EDITOR_ATOM(name_, value_) static nsIAtom* name_;
 #include "nsEditPropertyAtomList.h"
 #undef EDITOR_ATOM
-  };
-  
-  NS_RegisterStaticAtoms(property_atoms, NS_ARRAY_LENGTH(property_atoms));
-}
+
+};
+
+
+
+#endif
