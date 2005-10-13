1be09e612b7aa208b8796c41ab373613a29479f6
darin%meer.net <darin%meer.net>
2005-10-13 21:50:25 +0000

    fixes bug 309335 "Unknown content type dialog(helper app dialog) shows escaped file name if it has non-ASCII char" patch by biesi+darin r=bzbarsky

--- intl/uconv/src/nsGREResProperties.h
+++ intl/uconv/src/nsGREResProperties.h
@@ -1,4 +1,4 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set sts=2 sw=2 et cin: */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
@@ -12,18 +12,18 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is mozilla.org code.
+ * The Original Code is gre/res property file loading code.
  *
  * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1998
+ * Christian Biesinger <cbiesinger@web.de>.
+ * Portions created by the Initial Developer are Copyright (C) 2005
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
@@ -34,30 +34,33 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
-#ifndef nsCharsetAlias_h__
-#define nsCharsetAlias_h__
 
-#include "nsICharsetAlias.h"
-#include "nsGREResProperties.h"
+#ifndef nsGREResProperties_h__
+#define nsGREResProperties_h__
 
-//==============================================================
-class nsCharsetAlias2 : public nsICharsetAlias
-{
-  NS_DECL_ISUPPORTS
+#include "nsString.h"
+#include "nsIPersistentProperties2.h"
+#include "nsCOMPtr.h"
 
+/**
+ * This class loads a .properties file from the gre/res directory; the file
+ * to load is specified as a constructor argument.
+ */
+class nsGREResProperties {
 public:
+  /**
+   * @param aFile The file to load. Must be an ASCII string.
+   */
+  nsGREResProperties(const nsACString& aFile);
 
-  nsCharsetAlias2();
-  virtual ~nsCharsetAlias2();
+  /**
+   * Returns whether loading the file succeeded.
+   */
+  PRBool DidLoad() const;
+  nsresult Get(const nsAString& aKey, nsAString& value);
 
-  NS_IMETHOD GetPreferred(const nsACString& aAlias, nsACString& aResult);
-
-  NS_IMETHOD Equals(const nsACString& aCharset1, const nsACString& aCharset2, PRBool* oResult) ;
-  
 private:
-  nsGREResProperties* mDelegate;
+  nsCOMPtr<nsIPersistentProperties> mProps;
 };
 
-#endif // nsCharsetAlias_h__
-
-
+#endif // nsGREResProperties_h__
