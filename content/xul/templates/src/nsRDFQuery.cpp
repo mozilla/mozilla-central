5e5560555130ab553c8496dc22b7c9d627b1064e
enndeakin%sympatico.ca <enndeakin%sympatico.ca>
2006-02-13 16:02:08 +0000

    Bug 285631, new template builder, r+sr=bz

--- content/xul/templates/src/nsRDFQuery.cpp
+++ content/xul/templates/src/nsRDFQuery.cpp
@@ -12,15 +12,13 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Original Code is Mozilla Communicator client code.
+ * The Original Code is mozilla.org code.
  *
- * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1998
+ * The Initial Developer of the Original Code is Neil Deakin
+ * Portions created by the Initial Developer are Copyright (C) 2005
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *   Chris Waterson <waterson@netscape.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either of the GNU General Public License Version 2 or later (the "GPL"),
@@ -36,27 +34,40 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include "nsTemplateMatch.h"
-#include "nsTemplateRule.h"
+#include "nscore.h"
+#include "nsCOMPtr.h"
 
-#ifdef NEED_CPP_UNUSED_IMPLEMENTATIONS
-nsTemplateMatch::nsTemplateMatch(const nsTemplateMatch& aMatch) {}
-void nsTemplateMatch::operator=(const nsTemplateMatch& aMatch) {}
-#endif
+#include "nsXULTemplateQueryProcessorRDF.h"
+#include "nsRDFQuery.h"
+
+NS_IMPL_ISUPPORTS1(nsRDFQuery, nsITemplateRDFQuery)
+
+void
+nsRDFQuery::Finish()
+{
+    // the template builder is going away and the query processor likely as
+    // well. Clear the reference to avoid calling it.
+    mProcessor = nsnull;
+    mCachedResults = nsnull;
+}
 
 nsresult
-nsTemplateMatch::RuleMatched(nsTemplateQuerySet* aQuerySet,
-                             nsTemplateRule* aRule,
-                             nsIXULTemplateResult* aResult)
+nsRDFQuery::SetCachedResults(nsXULTemplateQueryProcessorRDF* aProcessor,
+                             const InstantiationSet& aInstantiations)
 {
-    mRule = aRule;
-
-    nsCOMPtr<nsIDOMNode> rulenode;
-    aRule->GetRuleNode(getter_AddRefs(rulenode));
-    if (rulenode) {
-        nsCOMPtr<nsIDOMNode> querynode = do_QueryInterface(aQuerySet->mQueryNode);
-        return aResult->RuleMatched(querynode, rulenode);
-    }
+    mCachedResults = new nsXULTemplateResultSetRDF(aProcessor, this, &aInstantiations);
+    if (! mCachedResults)
+        return NS_ERROR_OUT_OF_MEMORY;
 
     return NS_OK;
+}
+
+
+void
+nsRDFQuery::UseCachedResults(nsISimpleEnumerator** aResults)
+{
+    *aResults = mCachedResults;
+    NS_IF_ADDREF(*aResults);
+
+    mCachedResults = nsnull;
 }
