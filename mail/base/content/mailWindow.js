ca8a6fc8ee7e50aaa76aa3a3285c0332a5d9dd54
scott%scott-macgregor.org <scott%scott-macgregor.org>
2007-04-27 06:49:29 +0000

    Bug #377178 --> when the user is offline, load about:blank for the start page instead of the remote start page.
    r=phil

--- mail/base/content/mailWindow.js
+++ mail/base/content/mailWindow.js
@@ -482,18 +482,12 @@ function loadStartPage()
   try
   {
     gMessageNotificationBar.clearMsgNotifications();
-    
     var startpageenabled = pref.getBoolPref("mailnews.start_page.enabled");
     // only load the start page if we are online
-    if (startpageenabled && MailOfflineMgr.isOnline())
-    {
-      var startpage = getFormattedRegionURL(startPageUrlPref());
-      if (startpage)
-      {
-        GetMessagePaneFrame().location.href = startpage;
-        ClearMessageSelection();
-      }
-    }
+    var startpage = getFormattedRegionURL(startPageUrlPref());
+    // load about:blank as the start page if we are offline or we don't have a start page url...
+    GetMessagePaneFrame().location.href = startpageenabled && startpage && MailOfflineMgr.isOnline() ? startpage : "about:blank";
+    ClearMessageSelection();
   }
   catch (ex)
   {
