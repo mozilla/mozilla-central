ab146d7c9e71d4e6ce7b46e38a0714cb80036ad1
rrelyea%redhat.com <rrelyea%redhat.com>
2007-06-14 01:12:37 +0000

    repare the MAC build.

--- security/nss/lib/nss/config.mk
+++ security/nss/lib/nss/config.mk
@@ -129,6 +129,10 @@ SHARED_LIBRARY_DIRS = \
 	../libpkix/pkix_pl_nss/module \
 	$(NULL)
 
+ifeq ($(OS_ARCH), Darwin)
+EXTRA_SHARED_LIBS += -dylib_file @executable_path/libsqlite3.dylib:$(DIST)/lib/libsqlite3.dylib
+endif
+
 
 ifeq ($(OS_TARGET),SunOS)
 ifeq ($(BUILD_SUN_PKG), 1)
