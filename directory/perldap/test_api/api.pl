ba573a1db85bc1ea84d0bcd8134d5c182d3afb54
gerv%gerv.net <gerv%gerv.net>
2007-06-19 11:27:06 +0000

    Bug 368747 - relicense PerLDAP under the tri-license.

--- directory/perldap/test_api/api.pl
+++ directory/perldap/test_api/api.pl
@@ -1,33 +1,50 @@
 #!/usr/bin/perl -w
 #############################################################################
-# $Id: api.pl,v 1.8 2000-10-05 19:47:45 leif%netscape.com Exp $
+# $Id: api.pl,v 1.9 2007-06-19 11:27:06 gerv%gerv.net Exp $
 #
-# The contents of this file are subject to the Mozilla Public License
-# Version 1.1 (the "License"); you may not use this file except in
-# compliance with the License. You may obtain a copy of the License at
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
 # http://www.mozilla.org/MPL/
 #
-# Software distributed under the License is distributed on an "AS IS"
-# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-# License for the specific language governing rights and limitations
-# under the License.
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
 #
-# The Original Code is PerLDAP. The Initial Developer of the Original
-# Code is Netscape Communications Corp. and Clayton Donley. Portions
-# created by Netscape are Copyright (C) Netscape Communications
-# Corp., portions created by Clayton Donley are Copyright (C) Clayton
-# Donley. All Rights Reserved.
+# The Original Code is PerLDAP.
+#
+# The Initial Developer of the Original Code is
+# Netscape Communications Corp.
+# Portions created by the Initial Developer are Copyright (C) 2001
+# the Initial Developer. All Rights Reserved.
 #
 # Contributor(s):
+#   Clayton Donley
 #
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
 # DESCRIPTION
 #   api.pl - Test all LDAPv2 API function
 #   Author:  Clayton Donley <donley@wwa.com>
 #
 #   Performs all API calls directly in order to test for possible issues
 #   on a particular platform.
-#
-#############################################################################
 
 use Mozilla::LDAP::API qw(:api :constant);
 use strict;
