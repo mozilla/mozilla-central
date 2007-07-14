83d25918871db685fe3f45cc3462f0d4bb7e7d9a
nelson%bolyard.com <nelson%bolyard.com>
2007-07-14 06:01:03 +0000

    Bug 251594 � Certificate from PKCS#12 file with colon in friendlyName not selectable for signing/encryption. r=neil.williams

--- security/nss/lib/pki/pki3hack.c
+++ security/nss/lib/pki/pki3hack.c
@@ -35,7 +35,7 @@
  * ***** END LICENSE BLOCK ***** */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: pki3hack.c,v $ $Revision: 1.92 $ $Date: 2006-10-09 22:21:41 $";
+static const char CVS_ID[] = "@(#) $RCSfile: pki3hack.c,v $ $Revision: 1.93 $ $Date: 2007-07-14 06:01:03 $";
 #endif /* DEBUG */
 
 /*
@@ -677,7 +677,8 @@ STAN_GetCERTCertificateNameForInstance (
     }
     if (stanNick) {
 	/* fill other fields needed by NSS3 functions using CERTCertificate */
-	if (instance && !PK11_IsInternal(instance->token->pk11slot)) {
+	if (instance && (!PK11_IsInternal(instance->token->pk11slot) || 
+	                 PORT_Strchr(stanNick, ':') != NULL) ) {
 	    tokenName = nssToken_GetName(instance->token);
 	    tokenlen = nssUTF8_Size(tokenName, &nssrv);
 	} else {
@@ -741,7 +742,9 @@ fill_CERTCertificateFields(NSSCertificate *c, CERTCertificate *cc, PRBool forced
 	int nicklen, tokenlen, len;
 	NSSUTF8 *tokenName = NULL;
 	char *nick;
-	if (instance && !PK11_IsInternal(instance->token->pk11slot)) {
+	if (instance && 
+	     (!PK11_IsInternal(instance->token->pk11slot) || 
+	      (stanNick && PORT_Strchr(stanNick, ':') != NULL))) {
 	    tokenName = nssToken_GetName(instance->token);
 	    tokenlen = nssUTF8_Size(tokenName, &nssrv);
 	} else {
