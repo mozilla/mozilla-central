ba1c4b6833dda96799d18b1fbf346942271b3826
ted.mielczarek%gmail.com <ted.mielczarek%gmail.com>
2007-07-18 13:53:14 +0000

    bug 377100 - configure all trunk tinderboxen to use objdirs--switch win32/linux xulrunner trunk to objdirs.  r=bsmedberg

--- tools/tinderbox-configs/xulrunner/win32/tinder-config.pl
+++ tools/tinderbox-configs/xulrunner/win32/tinder-config.pl
@@ -144,7 +144,7 @@ $moz_cvsroot   = ":ext:cltbld\@cvs.mozilla.org:/cvsroot";
 #$moz_client_mk = 'client.mk';
 
 #- Set if you want to build in a separate object tree
-#$ObjDir = '';
+$ObjDir = 'obj-xulrunner';
 
 # Extra build name, if needed.
 $BuildNameExtra = 'release';
