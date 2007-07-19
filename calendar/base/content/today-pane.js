f2fdc6eec4f508389c4079fda74be2aea1e251cd
Berend.Cornelius%sun.com <Berend.Cornelius%sun.com>
2007-07-19 15:54:34 +0000

    385900# adding new files for today-pane

---
+++ calendar/base/content/today-pane.js
@@ -0,0 +1,240 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+    - The Original Code is Sun Microsystems code.
+ *
+ * The Initial Developer of the Original Code is Sun Microsystems.
+ * Portions created by the Initial Developer are Copyright (C) 2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Berend Cornelius <berend.cornelius@sun.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or 
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+
+
+var TodayPane = {
+  CurrentPaneView: 0,
+  paneViews: null,
+  stodaypaneButton: "calendar-show-todaypane-button",
+
+  onLoad: function () {
+    var addToolbarbutton = false;
+    var todaypanebox = document.getElementById("today-pane-box");
+    this.paneViews = [ calGetString("calendar", "eventsandtasks"), calGetString("calendar", "tasksonly"), calGetString("calendar", "eventsonly") ];
+    var mailToolbar = getMailBar();
+    var defaultSetString = mailToolbar.getAttribute("defaultset");
+    if (defaultSetString.indexOf(this.stodaypaneButton) == -1) {
+      defaultSetString = this.addButtonToDefaultset(defaultSetString);
+      mailToolbar.setAttribute("defaultset", defaultSetString);
+    }
+    
+    
+    // add the toolbarbutton to the mailtoolbarpalette on first startup
+    if (todaypanebox.hasAttribute("addtoolbarbutton")) {
+      addToolbarbutton = (todaypanebox.getAttribute("addtoolbarbutton") == "true");
+    }
+    if (addToolbarbutton) {
+      var currentSetString = mailToolbar.getAttribute("currentset");
+      if (currentSetString.indexOf(this.stodaypaneButton) == -1) {
+        this.addButtonToToolbarset();
+      }
+      todaypanebox.setAttribute("addtoolbarbutton", "false");
+    }    
+
+    var agendaPanel = document.getElementById("agenda-groupbox");
+    var todoPanel = document.getElementById("todo-groupbox");
+    if (agendaPanel.hasAttribute("collapsed")) {
+      if (!todoPanel.hasAttribute("collapsed")) {
+        this.CurrentPaneView = 1;
+      }
+      else{
+        dump("Cannot display todaypane with both subpanes collapsed");
+      }
+    }
+    else {
+      if (todoPanel.hasAttribute("collapsed")) {
+        this.CurrentPaneView = 2
+      }
+    }
+    var todayheader = document.getElementById("today-header");
+    todayheader.setAttribute("value", this.paneViews[this.CurrentPaneView]);
+
+    // add a menuitem to the 'View/Layout' -menu. As the respective "Layout" menupopup
+    // carries no 'id' attribute it cannot be overlaid
+    var todayMenuItem = document.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "menuitem");
+    todayMenuItem.setAttribute("id", "menu_showTodayPane");
+    todayMenuItem.setAttribute("type", "checkbox");
+    todayMenuItem.setAttribute("command", "cmd_toggleTodayPane");
+    todayMenuItem.setAttribute("accesskey", calGetString("calendar", "todaypane-accesskey"));
+    todayMenuItem.setAttribute("key", "todaypanekey");
+    todayMenuItem.setAttribute("persist", "checked");
+    todayMenuItem.setAttribute("label", todaylabel);
+    todayMenuItem.setAttribute("checked", false);
+    var messagePaneMenu = document.getElementById("menu_MessagePaneLayout");
+    var messagePanePopupMenu = messagePaneMenu.firstChild;
+    messagePanePopupMenu.appendChild(todayMenuItem);
+    updateTodayPaneDisplay();
+    var todayPaneCommand = document.getElementById('cmd_toggleTodayPane')
+    var bchecked = !todaypanebox.hasAttribute("collapsedinMailMode");
+    todayMenuItem.setAttribute("checked", bchecked);
+    var toolbar = document.getElementById(this.stodaypaneButton);
+    if (toolbar != null) {
+      toolbar.setAttribute("checked", bchecked);
+    }
+  },
+
+  addButtonToDefaultset: function(toolbarSetString)
+  {
+    var mailToolbar = getMailBar();
+    var elementcount = mailToolbar.childNodes.length;
+    var buttonisWithinSet = false;
+    // by default the todaypane-button is to be placed before the first
+    // separator of the toolbar
+    for (var i = 0; i < elementcount; i++) {
+      var element = mailToolbar.childNodes[i];
+      if (element.localName == "toolbarseparator") {
+          var separatorindex = toolbarSetString.indexOf("separator");
+          if (separatorindex > -1) {
+            var firstSubString = toolbarSetString.substring(0, separatorindex);
+            var secondSubString = toolbarSetString.substring(separatorindex);
+            toolbarSetString = firstSubString + this.stodaypaneButton + "," + secondSubString;
+          }
+          buttonisWithinSet = true;
+          break;
+      }
+    }
+    if (!buttonisWithinSet) {
+      // in case there is no separator within the toolbar we append the 
+      // toddaypane-button
+      toolbarSetString += "," + this.stodaypaneButton;
+    }
+    return toolbarSetString;
+  },
+  
+  addButtonToToolbarset: function()
+  {
+    var mailToolbar = getMailBar();
+    var elementcount = mailToolbar.childNodes.length;
+    var buttonisWithinSet = false;
+    // by default the todaypane-button is to be placed before the first
+    // separator of the toolbar
+    for (var i = 0; i < elementcount; i++) {
+      var element = mailToolbar.childNodes[i];
+      if (element.localName == "toolbarseparator") {
+          mailToolbar.insertItem(this.stodaypaneButton, element, null, false);
+          buttonisWithinSet = true;
+          break;
+      }
+    }
+    if (!buttonisWithinSet) {
+      // in case there is no separator within the toolbar we append the 
+      // toddaypane-button
+      mailToolbar.insertItem(this.stodaypaneButton, null, null, false);
+    }
+  },
+
+  // we can cycle the pane view forward or backwards
+  cyclePaneView: function(aCycleForward)
+  {
+    function _collapsePanel(oPanel, bCollapse)
+    {
+      if (bCollapse) {
+        oPanel.setAttribute("collapsed", bCollapse)
+      }
+      else{
+        oPanel.removeAttribute("collapsed")
+      }
+    }
+    
+    this.CurrentPaneView = this.CurrentPaneView + aCycleForward;
+    var nViewLen = this.paneViews.length;
+    if (this.CurrentPaneView >= nViewLen) {
+        this.CurrentPaneView = 0;
+    }
+    else if (this.CurrentPaneView == -1) {
+        this.CurrentPaneView = nViewLen -1;
+    }
+    var agendapanel = document.getElementById("agenda-groupbox");
+    var todopanel = document.getElementById("todo-groupbox");
+    var todayheader = document.getElementById("today-header");
+    todayheader.setAttribute("value", this.paneViews[this.CurrentPaneView]);
+    switch (this.CurrentPaneView) {
+      case 0:
+        _collapsePanel(agendapanel, false);
+        _collapsePanel(todopanel, false);
+        document.getElementById("today-pane-splitter").removeAttribute("hidden");
+        break;
+      case 1:
+        _collapsePanel(agendapanel, true);
+        _collapsePanel(todopanel, false);
+        document.getElementById("today-pane-splitter").setAttribute("hidden", "true");
+        break;
+      case 2:
+        _collapsePanel(agendapanel, false);
+        _collapsePanel(todopanel, true);
+        document.getElementById("today-pane-splitter").setAttribute("hidden", "true");
+        break;
+    }
+  }
+};
+
+function loadTodayPane() {
+  TodayPane.onLoad();
+}
+
+window.addEventListener("load", loadTodayPane, false);
+
+function updateTodayPaneDisplay() {
+  var deck = document.getElementById("displayDeck");
+  var id = null;
+  try { id = deck.selectedPanel.id } catch (e) { }
+
+  if (id == "calendar-view-box") {
+    // we collapse the todaypane but don't not affect the 
+    // attribute "collapsedinMailMode". Therefor this function is only to be used
+    // when switching to calendar mode
+    var oTodayPane = document.getElementById("today-pane-box");
+    oTodayPane.setAttribute("collapsed", true);
+    document.getElementById('cmd_toggleTodayPane').setAttribute("disabled","true");
+  }
+  else {
+    // only show the today-pane if was not collapsed during the last 
+    // "mail-mode session"
+    var oTodayPane = document.getElementById("today-pane-box");
+    if (!oTodayPane.hasAttribute("collapsedinMailMode")) {
+      if (oTodayPane.hasAttribute("collapsed")) {
+        oTodayPane.removeAttribute("collapsed");
+      }
+    }
+    document.getElementById('cmd_toggleTodayPane').removeAttribute("disabled");
+  }
+}
+
+
+document.getElementById("displayDeck").
+    addEventListener("select", updateTodayPaneDisplay, true);
+
