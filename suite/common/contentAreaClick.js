2a26a6eeec293a59cb393e52371a0ad1183c7b4f
cst%yecc.com <cst%yecc.com>
2007-07-22 02:42:01 +0000

    Bug 384575 - Remove SeaMonkey's autoscroll now that its improvements have been ported to toolkit's r=mano on the toolkit change r+sr=neil on the rest

--- suite/common/contentAreaClick.js
+++ suite/common/contentAreaClick.js
@@ -43,7 +43,6 @@
  * - [ Dependencies ] ---------------------------------------------------------
  *  utilityOverlay.js:
  *    - gatherTextUnder
- *    - startScrolling
  */
 
   var pref = null;
@@ -198,42 +197,6 @@
       }
     }
     return true;
-  }
-
-  function contentAreaMouseDown(event)
-  {
-    if (event.button == 1 && (event.target != event.currentTarget)
-        && !hrefAndLinkNodeForClickEvent(event)
-        && !isAutoscrollBlocker(event.originalTarget)) {
-      startScrolling(event);
-      return false;
-    }
-    return true;
-  }
-
-  function isAutoscrollBlocker(node)
-  {
-    if (!pref)
-      return false;
-    
-    if (pref.getBoolPref("middlemouse.contentLoadURL"))
-      return true;
-    
-    if (!pref.getBoolPref("middlemouse.paste"))
-      return false;
-    
-    if (node.ownerDocument.designMode == "on")
-      return true;
-    
-    while (node) {
-      if (node instanceof HTMLTextAreaElement ||
-          (node instanceof HTMLInputElement &&
-           (node.type == "text" || node.type == "password")))
-        return true;
-      
-      node = node.parentNode;
-    }
-    return false;
   }
 
   function openNewTabOrWindow(event, href, doc)
