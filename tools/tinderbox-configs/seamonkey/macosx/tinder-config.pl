16bf9daa53614f619434faa5ba02ba75b671d092
ccooper%deadsquid.com <ccooper%deadsquid.com>
2007-07-24 00:28:12 +0000

    - don't use anonymous CVS inside the firewall

--- tools/tinderbox-configs/seamonkey/macosx/tinder-config.pl
+++ tools/tinderbox-configs/seamonkey/macosx/tinder-config.pl
@@ -130,7 +130,7 @@ $VendorName               = 'mozilla.org';
 # :pserver:$ENV{USER}%netscape.com@cvs.mozilla.org:/cvsroot
 
 #$moz_cvsroot   = $ENV{CVSROOT};
-$moz_cvsroot   = ':pserver:anonymous@cvs.mozilla.org:/cvsroot';
+$moz_cvsroot   = ':ext:seabld@cvs.mozilla.org:/cvsroot';
 
 #- Set these proper values for your tinderbox server
 #$Tinderbox_server = 'tinderbox-daemon@tinderbox.mozilla.org';
