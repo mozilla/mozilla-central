073ec4ffce09b49de5592e4c28d76a23d359bdbd
daniel.boelzle%sun.com <daniel.boelzle%sun.com>
2007-07-31 08:18:30 +0000

    Bug 386925 - Sunbird Trunk fails to build (Error: Cannot open include file nsBuildID.h); r=mvl

--- calendar/sunbird/app/nsCalendarApp.cpp
+++ calendar/sunbird/app/nsCalendarApp.cpp
@@ -42,24 +42,52 @@
 #include <windows.h>
 #include <stdlib.h>
 #endif
-#include "nsBuildID.h"
-#include <string.h>
 
-static const nsXREAppData kAppData = {
-  offsetof(nsXREAppData, xreDirectory),
-  nsnull,
-  "Mozilla",
-  "Sunbird",
-  NS_STRINGIFY(APP_VERSION),
-  NS_STRINGIFY(BUILD_ID),
-  "{718e30fb-e89b-41dd-9da7-e25a45638b28}",
-  "Copyright (c) 2004-2007 mozilla.org",
-  NS_XRE_ENABLE_EXTENSION_MANAGER
-};
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "nsCOMPtr.h"
+#include "nsILocalFile.h"
+#include "nsStringGlue.h"
+
+static void Output(const char *fmt, ... )
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+#if defined(XP_WIN) && !MOZ_WINCONSOLE
+  char msg[2048];
+
+  _vsnprintf(msg, sizeof(msg), fmt, ap);
+
+  MessageBox(NULL, msg, "XULRunner", MB_OK | MB_ICONERROR);
+#else
+  vfprintf(stderr, fmt, ap);
+#endif
+
+  va_end(ap);
+}
 
 int main(int argc, char* argv[])
 {
-  return XRE_main(argc, argv, &kAppData);
+  nsCOMPtr<nsILocalFile> appini;
+  nsresult rv = XRE_GetBinaryPath(argv[0], getter_AddRefs(appini));
+  if (NS_FAILED(rv)) {
+    Output("Couldn't calculate the application directory.");
+    return 255;
+  }
+  appini->SetNativeLeafName(NS_LITERAL_CSTRING("application.ini"));
+
+  nsXREAppData *appData;
+  rv = XRE_CreateAppData(appini, &appData);
+  if (NS_FAILED(rv)) {
+    Output("Couldn't read application.ini");
+    return 255;
+  }
+
+  int result = XRE_main(argc, argv, appData);
+  XRE_FreeAppData(appData);
+  return result;
 }
 
 #if defined( XP_WIN ) && defined( WIN32 ) && !defined(__GNUC__)
