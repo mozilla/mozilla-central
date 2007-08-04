fa1185917526c048b144285539349c50046410bd
bienvenu%nventure.com <bienvenu%nventure.com>
2007-08-04 21:30:52 +0000

    fix bit-rotting of osx address book code, so that it compiles and works again, NPOB

--- mailnews/addrbook/public/nsAbBaseCID.h
+++ mailnews/addrbook/public/nsAbBaseCID.h
@@ -435,6 +435,47 @@
 #define NS_ABVIEW_CONTRACTID \
  "@mozilla.org/addressbook/abview;1"
 
+#ifdef XP_MACOSX
+//
+// nsAbOSXDirectory
+//
+#define NS_ABOSXDIRECTORY_PREFIX "moz-abosxdirectory"
+#define NS_ABOSXCARD_PREFIX "moz-abosxcard"
+
+#define NS_ABOSXDIRECTORY_CONTRACTID \
+  NS_RDF_RESOURCE_FACTORY_CONTRACTID_PREFIX NS_ABOSXDIRECTORY_PREFIX
+
+#define NS_ABOSXDIRECTORY_CID                            \
+{ /* {83781cc6-c682-11d6-bdeb-0005024967b8}*/            \
+        0x83781cc6, 0xc682, 0x11d6,                      \
+        {0xbd, 0xeb, 0x00, 0x05, 0x02, 0x49, 0x67, 0xb8} \
+}
+
+//
+// nsAbOSXCard
+//
+#define NS_ABOSXCARD_CONTRACTID \
+  NS_RDF_RESOURCE_FACTORY_CONTRACTID_PREFIX NS_ABOSXCARD_PREFIX
+
+#define NS_ABOSXCARD_CID                                 \
+{ /* {89bbf582-c682-11d6-bc9d-0005024967b8}*/            \
+        0x89bbf582, 0xc682, 0x11d6,                      \
+        {0xbc, 0x9d, 0x00, 0x05, 0x02, 0x49, 0x67, 0xb8} \
+}
+
+//
+// OS X directory factory
+//
+#define NS_ABOSXDIRFACTORY_CONTRACTID \
+  NS_AB_DIRECTORY_FACTORY_CONTRACTID_PREFIX NS_ABOSXDIRECTORY_PREFIX
+
+#define NS_ABOSXDIRFACTORY_CID                           \
+{ /* {90efe2fe-c682-11d6-9c83-0005024967b8}*/            \
+        0x90efe2fe, 0xc682, 0x11d6,                      \
+        {0x9c, 0x83, 0x00, 0x05, 0x02, 0x49, 0x67, 0xb8} \
+}
+#endif
+
 #define NS_MSGVCARDSERVICE_CID \
 { 0x3c4ac0da, 0x2cda, 0x4018, \
  { 0x95, 0x51, 0xe1, 0x58, 0xb2, 0xe1, 0x22, 0xd3 }}
