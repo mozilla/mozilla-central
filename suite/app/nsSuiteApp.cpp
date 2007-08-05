7737b70bfbab681f95fe2510e442a82298007a41
gavin%gavinsharp.com <gavin%gavinsharp.com>
2007-08-05 23:48:25 +0000

    Fix comment typo in patches for bug 389779 and bug 388833

--- suite/app/nsSuiteApp.cpp
+++ suite/app/nsSuiteApp.cpp
@@ -68,7 +68,7 @@ static void Output(const char *fmt, ... )
 }
 
 /**
- * A helper class which calls NS_LogTerm/NS_LogTerm in it's scope.
+ * A helper class which calls NS_LogInit/NS_LogTerm in it's scope.
  */
 class ScopedLogging
 {
