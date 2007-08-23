e2b68545482a9aa36800dbace8f1658a9e1041eb
philringnalda%gmail.com <philringnalda%gmail.com>
2007-08-23 04:03:28 +0000

    Bug 392670 - Add toolkit download manager pref for displayed history days, patch by Edward Lee (Mardak) <edilee@gmail.com>, r=mscott

--- mail/app/profile/all-thunderbird.js
+++ mail/app/profile/all-thunderbird.js
@@ -315,6 +315,7 @@ pref("browser.download.manager.closeWhenDone", true);
 pref("browser.download.manager.openDelay", 100);
 pref("browser.download.manager.focusWhenStarting", false);
 pref("browser.download.manager.flashCount", 0);
+pref("browser.download.manager.displayedHistoryDays", 7);
 
 pref("javascript.options.showInConsole",    true);
 
