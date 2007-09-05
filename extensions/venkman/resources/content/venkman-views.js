8fbc0dde3b5a6e6726bbec803455b84f9b8e5e47
gijskruitbosch%gmail.com <gijskruitbosch%gmail.com>
2007-09-05 09:52:08 +0000

    Backing out bug 295275 and bug 270298

--- extensions/venkman/resources/content/venkman-views.js
+++ extensions/venkman/resources/content/venkman-views.js
@@ -146,8 +146,10 @@ function syncTreeView (treeContent, treeView, cb)
             throw "tantrum";
         
         treeContent.treeBoxObject.view = treeView;
-        if (treeView && treeView.selection)
+        if (treeView.selection)
+        {
             treeView.selection.tree = treeContent.treeBoxObject;
+        }
     }
     catch (ex)
     {
