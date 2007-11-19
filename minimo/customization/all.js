f3ace2c53a1d9228de306db304d7e3025f41eeaf
masayuki%d-toybox.com <masayuki%d-toybox.com>
2007-11-19 10:17:19 +0000

    Bug 403484 The ellipsis for UI should be localizable r=roc+mconnor, sr=roc

--- minimo/customization/all.js
+++ minimo/customization/all.js
@@ -420,6 +420,7 @@ pref("intl.charsetmenu.browser.more5",  "chrome://global/locale/intl.properties"
 pref("intl.charsetmenu.browser.unicode",  "chrome://global/locale/intl.properties");
 pref("intl.charset.detector", "chrome://global/locale/intl.properties");
 pref("intl.charset.default",  "chrome://global-platform/locale/intl.properties");
+pref("intl.ellipsis",  "chrome://global-platform/locale/intl.properties");
 pref("font.language.group", "chrome://global/locale/intl.properties");
 pref("intl.menuitems.alwaysappendaccesskeys","chrome://global/locale/intl.properties");
 pref("intl.menuitems.insertseparatorbeforeaccesskeys","chrome://global/locale/intl.properties");
