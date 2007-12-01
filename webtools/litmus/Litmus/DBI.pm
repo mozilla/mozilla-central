776d88e91e17c01a09e31443c453012ee8035252
ccooper%deadsquid.com <ccooper%deadsquid.com>
2007-12-01 04:47:56 +0000

    - increase wait_timeout to 8 hours

--- webtools/litmus/Litmus/DBI.pm
+++ webtools/litmus/Litmus/DBI.pm
@@ -170,7 +170,8 @@ our $dsn = "dbi:mysql(RootClass=AuditDBI):database=$Litmus::Config::db_name;host
 Litmus::DBI->connection($dsn,
                         $Litmus::Config::db_user,
                         $Litmus::Config::db_pass,
-                        {mysql_enable_utf8 => 1}
+                        {mysql_enable_utf8 => 1,
+                         wait_timeout => 60*60*8}
 );
 
 our $readonly_dbh;
