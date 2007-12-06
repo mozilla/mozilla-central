a0632236e7a830bce5037a7563229d1c9db8e329
julien.pierre.boogz%sun.com <julien.pierre.boogz%sun.com>
2007-12-06 00:40:19 +0000

    Fix for bug 391771 . pk11_config_name and pk11_config_strings leaked on shutdown. Add new PK11_UnconfigurePKCS11 call to free the strings. r=nelson, relyea

--- security/nss/lib/nss/nssinit.c
+++ security/nss/lib/nss/nssinit.c
@@ -36,7 +36,7 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
-/* $Id: nssinit.c,v 1.85 2007-11-10 04:23:26 julien.pierre.boogz%sun.com Exp $ */
+/* $Id: nssinit.c,v 1.86 2007-12-06 00:40:19 julien.pierre.boogz%sun.com Exp $ */
 
 #include <ctype.h>
 #include "seccomon.h"
@@ -127,7 +127,7 @@ nss_makeFlags(PRBool readOnly, PRBool noCertDB,
 }
 
 /*
- * statics to remember the PKCS11_ConfigurePKCS11()
+ * statics to remember the PK11_ConfigurePKCS11()
  * info.
  */
 static char * pk11_config_strings = NULL;
@@ -226,6 +226,18 @@ PK11_ConfigurePKCS11(const char *man, const char *libdes, const char *tokdes,
     pk11_password_required = pwRequired;
 
     return;
+}
+
+void PK11_UnconfigurePKCS11(void)
+{
+    if (pk11_config_strings != NULL) {
+	PR_smprintf_free(pk11_config_strings);
+        pk11_config_strings = NULL;
+    }
+    if (pk11_config_name) {
+        PORT_Free(pk11_config_name);
+        pk11_config_name = NULL;
+    }
 }
 
 static char *
