6cafae79b5257b9aabd4ffc47156de01b53e49f0
bhearsum%mozilla.com <bhearsum%mozilla.com>
2008-01-03 16:22:30 +0000

    bug 393424: create package structure for custom buildbot steps - make tryserver buildbot use this. r=robcee, patch=me

---
+++ tools/buildbotcustom/env.py
@@ -0,0 +1,35 @@
+MozillaEnvironments = {}
+
+MozillaEnvironments['win32-ref-platform'] = {
+    "MOZ_TOOLS": 'd:\\mozilla-build\\moztools',
+    "VSINSTALLDIR": 'D:\\msvs8',
+    "VS80COMMTOOLS": 'D:\\msvs8\\Common7\\Tools\\',
+    "VCINSTALLDIR": 'D:\\msvs8\\VC',
+    "FrameworkDir": 'C:\\WINDOWS\\Microsoft.NET\\Framework',
+    "FrameworkSDKDir": 'D:\\msvs8\\SDK\\v2.0',
+    "DevEnvDir": "D:\\msvs8\\VC\\Common7\\IDE",
+    "MSVCDir": 'D:\\msvs8\\VC',
+    "PATH": 'D:\\msvs8\\Common7\\IDE;' + \
+            'D:\\msvs8\\VC\\bin;' + \
+            'C:\\msvs8\\SDK\\bin;' + \
+            'D:\\msvs8\\VC;' + \
+            'D:\\msvs8\\Common7\\Tools;' + \
+            'D:\\msvs8\\Common7\\Tools\\bin;' + \
+            'd:\\mercurial;' + \
+            'd:\\mozilla-build\\moztools\\bin;' + \
+            'd:\\mozilla-build\\msys\\bin;' + \
+            'd:\\mozilla-build\\msys\\local\\bin;' + \
+            'd:\\mozilla-build\\7zip;' + \
+            'd:\\mozilla-build\\upx203w;' + \
+            'd:\\mozilla-build\\python25;' + \
+            'd:\\mozilla-build\\blat261\\full;' + \
+            'd:\\mozilla-build\\info-zip;' + \
+            'd:\\mozilla-build\\wget;' + \
+            'd:\\mozilla-build\\nsis-2.22;',
+    "INCLUDE": 'D:\\msvs8\\VC\\ATLMFC\\INCLUDE;' + \
+               'D:\\msvs8\\VC\\INCLUDE;' + \
+               'D:\\msvs8\\VC\\PlatformSDK\\include',
+    "LIB": 'D:\\msvs8\\VC\\ATLMFC\\LIB;' + \
+           'D:\\msvs8\\VC\\LIB;' + \
+           'D:\\msvs8\\VC\\PlatformSDK\\lib'
+}
