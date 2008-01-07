f332974006119cec8f7c775fbe9dad0c601ae11b
rcampbell%mozilla.com <rcampbell%mozilla.com>
2008-01-07 15:12:22 +0000

    bug 400313 - Setup 10.5 unittest machine

--- tools/buildbot-configs/testing/unittest/auth.py
+++ tools/buildbot-configs/testing/unittest/auth.py
@@ -5,4 +5,5 @@ debugPassword = "swordfish"
 authList = [("win-vc8", "swordfish"),
             ("win-2k3", "swordfish"),
             ("linux","swordfish"),
-            ("mac-osx", "swordfish")]
+            ("mac-osx", "swordfish"),
+            ("qm-xserve02", "swordfish")]
