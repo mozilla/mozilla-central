dc6f6526b741ddc9a916c98d28212d4eaaa45a9d
nrthomas%gmail.com <nrthomas%gmail.com>
2008-01-08 13:24:59 +0000

    Convert Firefox trunk tinderboxes making nightlies to ffxbld key

--- tools/tinderbox-configs/firefox/macosx/tinder-config.pl
+++ tools/tinderbox-configs/firefox/macosx/tinder-config.pl
@@ -143,7 +143,7 @@ $DHTMLPerformanceTestTimeout      = 180;  # entire test, seconds
 # Note that win32 may not need \@, depends on ' or ".
 # :pserver:$ENV{USER}%netscape.com@cvs.mozilla.org:/cvsroot
 
-$moz_cvsroot   = ":ext:cltbld\@cvs.mozilla.org:/cvsroot";
+$moz_cvsroot   = ":ext:ffxbld\@cvs.mozilla.org:/cvsroot";
 
 #- Set these proper values for your tinderbox server
 #$Tinderbox_server = 'tinderbox-daemon@tinderbox.mozilla.org';
@@ -202,10 +202,10 @@ $package_creation_path = "/browser/installer";
 # needs setting for mac + talkback: $mac_bundle_path = "/browser/app";
 $mac_bundle_path = "/browser/app";
 $ssh_version   = "2";
-$ssh_user      = "cltbld";
-#$ssh_key       = "'$ENV{HOME}/.ssh/ffxbld_dsa'";
+$ssh_user      = "ffxbld";
+$ssh_key       = "'$ENV{HOME}/.ssh/ffxbld_dsa'";
 $ssh_server    = "stage.mozilla.org";
-#$ReleaseGroup  = "firefox";
+$ReleaseGroup  = "firefox";
 $ftp_path      = "/home/ftp/pub/firefox/nightly";
 $url_path      = "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly";
 $tbox_ftp_path = "/home/ftp/pub/firefox/tinderbox-builds";
