62e939f434a4c3c674c4b9fef5c3161a1d7a8961
ause%sun.com <ause%sun.com>
2008-01-17 14:23:37 +0000

    bug 408167 - change build_hour to have nightlies earlier

--- tools/tinderbox-configs/sunbird/macosx/tinder-config.pl
+++ tools/tinderbox-configs/sunbird/macosx/tinder-config.pl
@@ -213,7 +213,7 @@ $LocaleProduct = "calendar";
 $shiptalkback  = 0;
 $ReleaseToLatest = 1; # Push the release to latest-<milestone>?
 $ReleaseToDated = 1; # Push the release to YYYY-MM-DD-HH-<milestone>?
-$build_hour    = "3";
+$build_hour    = "18";
 $package_creation_path = "/calendar/installer";
 # needs setting for mac + talkback:
 $mac_bundle_path = "/calendar/sunbird/app";
