465e8ed5fe1ad0c3223276c19733c186a3c83eb0
christophe.ravel.bugs%sun.com <christophe.ravel.bugs%sun.com>
2008-01-31 22:29:17 +0000

    Prepare JSS 4.2.6 RTM. Uses NSPR 4.7 and NSS 3.11.9

--- security/jss/org/mozilla/jss/CryptoManager.java
+++ security/jss/org/mozilla/jss/CryptoManager.java
@@ -56,7 +56,7 @@ import org.mozilla.jss.provider.java.security.JSSMessageDigestSpi;
  * Initialization is done with static methods, and must be done before
  * an instance can be created.  All other operations are done with instance
  * methods.
- * @version $Revision: 1.49 $ $Date: 2007-05-31 20:10:43 $
+ * @version $Revision: 1.50 $ $Date: 2008-01-31 22:29:16 $
  */
 public final class CryptoManager implements TokenSupplier
 {
@@ -1322,11 +1322,11 @@ public final class CryptoManager implements TokenSupplier
     public static final String
     JAR_JDK_VERSION     = "JDK_VERSION = N/A";
     public static final String
-    JAR_NSS_VERSION     = "NSS_VERSION = NSS_3_11_7_RTM";
+    JAR_NSS_VERSION     = "NSS_VERSION = NSS_3_11_9_RTM";
     public static final String
     JAR_DBM_VERSION     = "DBM_VERSION = N/A";
     public static final String
-    JAR_NSPR_VERSION    = "NSPR_VERSION = NSPR_4_6_7_RTM";
+    JAR_NSPR_VERSION    = "NSPR_VERSION = NSPR_4_7_RTM";
 
     /**
      * Loads the JSS dynamic library if necessary.
