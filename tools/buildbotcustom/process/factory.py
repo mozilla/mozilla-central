908bd1e12f329ed9f6cfcd1555db9d6461f05b7e
rhelmer%mozilla.com <rhelmer%mozilla.com>
2008-03-12 22:01:02 +0000

    refactor and move bootstrap steps to custom factory b=417779 r=bhearsum

---
+++ tools/buildbotcustom/process/factory.py
@@ -0,0 +1,59 @@
+from buildbot.process.factory import BuildFactory
+from buildbot.steps.shell import ShellCommand
+from buildbot.steps.transfer import FileDownload
+
+class BootstrapFactory(BuildFactory):
+    def __init__(self, automation_tag, logdir, bootstrap_config, 
+                 cvsroot="pserver:anonymous@cvs-mirror.mozilla.org", 
+                 cvsmodule="mozilla"):
+        """
+    @type  cvsroot: string
+    @param cvsroot: The CVSROOT to use for checking out Bootstrap.
+
+    @type  cvsmodule: string
+    @param cvsmodule: The CVS module to use for checking out Bootstrap.
+
+    @type  automation_tag: string
+    @param automation_tag: The CVS Tag to use for checking out Bootstrap.
+
+    @type  logdir: string
+    @param logdir: The log directory for Bootstrap to use. Note - must already
+                   exist on the slave.
+
+    @type  bootstrap_config: string
+    @param bootstrap_config: The location of the bootstrap.cfg file on the 
+                             master. This will be uploaded to "bootstrap.cfg"
+                             in the builddir on the slave.
+        """
+        BuildFactory.__init__(self)
+        self.addStep(ShellCommand, 
+         description='clean checkout',
+         workdir='.', 
+         command=['rm', '-rfv', 'build'],
+         haltOnFailure=1)
+        self.addStep(ShellCommand, 
+         description='checkout', 
+         workdir='.',
+         command=['cvs', '-d', cvsroot, 'co', '-r', automation_tag,
+                  '-d', 'build', cvsmodule],
+         haltOnFailure=1,
+        )
+        self.addStep(FileDownload, 
+         mastersrc=bootstrap_config, 
+         slavedest="bootstrap.cfg", 
+         workdir="build",
+        )
+        self.addStep(ShellCommand, 
+         description='echo bootstrap.cfg',
+         command=['cat', 'bootstrap.cfg'],
+        )
+        self.addStep(ShellCommand, 
+         description='clean logs area',
+         command=['bash', '-c', 'rm -rf ' + logdir + '/*.log'], 
+         haltOnFailure=1,
+        )
+        self.addStep(ShellCommand, 
+         description='unit tests',
+         command=['make', 'test'], 
+         haltOnFailure=1,
+        )
