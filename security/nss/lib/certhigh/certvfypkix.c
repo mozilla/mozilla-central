bebe1c981a97e6191cae4d6aeee773cfbd17f49a
alexei.volkov.bugs%sun.com <alexei.volkov.bugs%sun.com>
2008-03-13 19:32:18 +0000

    Backout patch 390381 of bug 390381 since the code depends on another fix from the same bug.

--- security/nss/lib/certhigh/certvfypkix.c
+++ security/nss/lib/certhigh/certvfypkix.c
@@ -405,9 +405,11 @@ cert_ProcessingParamsSetKuAndEku(
                                                   plContext),
         PKIX_COMCERTSELPARAMSSETEXTKEYUSAGEFAILED);
 
-    PKIX_CHECK(
-        PKIX_PL_EkuChecker_Create(procParams, plContext),
-        PKIX_EKUCHECKERINITIALIZEFAILED);
+/*  Enable the following call when bug 390381 is fixed.
+ *   PKIX_CHECK(
+ *       PKIX_PL_EkuChecker_Create(procParams, plContext),
+ *       PKIX_EKUCHECKERINITIALIZEFAILED);
+ */
 
 cleanup:
     PKIX_DECREF(extKeyUsage);
