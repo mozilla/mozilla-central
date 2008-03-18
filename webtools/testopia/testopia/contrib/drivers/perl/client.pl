23b6690bf80df2cd1b0b52885ae7b88070861ff8
ghendricks%novell.com <ghendricks%novell.com>
2008-03-18 22:33:06 +0000

    Updated perl client

--- webtools/testopia/testopia/contrib/drivers/perl/client.pl
+++ webtools/testopia/testopia/contrib/drivers/perl/client.pl
@@ -137,165 +137,138 @@ if (defined($Bugzilla_login)) {
     }
 }
 
-#$soapresult = $proxy->call('TestPlan.list', {type_id=>4, pagesize=>1000});
-#$soapresult = $proxy->call('TestPlan.list', {page=>0, pagesize=>10});
-#$soapresult = $proxy->call('TestPlan.lookup_type_name_by_id', 1);
-#$soapresult = $proxy->call('TestPlan.lookup_type_id_by_name', 'unit2');
-#$soapresult = $proxy->call('TestPlan.get_test_runs', 122);
 
-#$soapresult = $proxy->call('User.lookup_login_by_id', 2);
-#$soapresult = $proxy->call('Product.lookup_name_by_id', 1);
-#$soapresult = $proxy->call('Product.lookup_id_by_name', 'TestProduct');
+#####################
+### Build Methods ###
+#####################
 
-#$soapresult = $proxy->call('TestCaseRun.update', 104, 183336, 1, { case_run_status_id => 1 });
-#$soapresult = $proxy->call('TestCaseRun.get', 3);
-#$soapresult = $proxy->call('TestCaseRun.update', 1, 1, 1, 1, { notes=>'WOW!! A note!', case_run_status_id => 1 });
-#$soapresult = $proxy->call('TestCaseRun.update', 1, 1, 2, 1, { build_id => 1 });
-#$soapresult = $proxy->call('TestCaseRun.update', 1, 1, 1, 1, { isCurrent => 1 });
-#$soapresult = $proxy->call('TestCaseRun.update', 1, 1, 1, 2, { environment_id => 1 });
-#$soapresult = $proxy->call('TestCaseRun.update', 1, 1, 1, 1, { assignee=> 1 });
-#$soapresult = $proxy->call('Build.get', 2);
-#$soapresult = $proxy->call('TestCaseRun.list', {});
-#$soapresult = $proxy->call('TestCaseRun.list', {build_id => 99});
-#$soapresult = $proxy->call('TestCaseRun.list', {case_id=>357222, isactive=>1});
+#$soapresult = $proxy->call('Build.check_build', 'Linux', 2);
+#$soapresult = $proxy->call('Build.check_build', 'Linux', 'Bugzilla');
+#$soapresult = $proxy->call('Build.create', {name=>'Build '. time(), product_id=>2, isactive=>0, description=> 'API Test Build - IGNORE'});
+#$soapresult = $proxy->call('Build.get', 1140);
+#$soapresult = $proxy->call('Build.update', 1140, { description=>'This is a description', milestone=>'3.0', isactive=>0});
 
-#$soapresult = $proxy->call('TestCase.list', {pagesize=>25, script_type=>'equals', script => 'novell.storage.p0.NetStorageTest.NetStorage - SIMPLE - test logout'});
-#$soapresult = $proxy->call('TestCase.list', {pagesize=>1500});
+###########################
+### Environment Methods ###
+###########################
 
-#$soapresult = $proxy->call('Build.lookup_id_by_name', 'First Build');
-#$soapresult = $proxy->call('TestPlan.list', {pagesize=>100, plan_id=>222});
-#$soapresult = $proxy->call('TestPlan.list', {pagesize=>100, name=>'Unit Test Plan', name_type=>'anyexact'});
-#$soapresult = $proxy->call('TestPlan.lookup_type_name_by_id', 0);
-#$soapresult = $proxy->call('TestPlan.get', 555);
-#$soapresult = $proxy->call('TestPlan.update', 1, {name => 'Restricted Plan Updated', isactive2=>1});
-#$soapresult = $proxy->call('TestPlan.list', {pagesize=>100});
-
-#$soapresult = $proxy->call('TestPlan.get_categories', 2);
-
-#$soapresult = $proxy->call('TestCaseRun.update',1,1,1,{notes=>'This is a new note to be appended.'});
-#$soapresult = $proxy->call('TestCaseRun.get',1);
-#$soapresult = $proxy->call('TestCaseRun.update', 1, 1, 1, { case_run_status_id => 1 });
-#$soapresult = $proxy->call('TestCase.add_component', 1, 1);
-#$soapresult = $proxy->call('TestCase.remove_component', 1, 1);
-#$soapresult = $proxy->call('TestCase.list_components', 1);
-#$soapresult = $proxy->call('Component.get', 9999);
-#$soapresult = $proxy->call('TestRun.get_test_cases', 1758);
-#$soapresult = $proxy->call('TestRun.get_test_case_runs', 1);
-#$soapresult = $proxy->call('TestPlan.get_builds', 2);
-$soapresult = $proxy->call('Build.get', 2);
-#$soapresult = $proxy->call('Build.create', {name=>'Another Build', product_id=>1});
-#$soapresult = $proxy->call('Build.update', 2, {name=>'Second Build', description=>'desc', milestone=>'hmm'});
-#$soapresult = $proxy->call('Build.update', 2, {milestone=>'hmm3'});
-
-#$soapresult = $proxy->call('TestRun.get_test_cases', 130);
-#$soapresult = $proxy->call('TestCase.list', {isautomated=>1});
-
-#$soapresult = $proxy->call('TestCase.list', {run_id=>130, isautomated=>1});
-
-#$soapresult = $proxy->call('TestPlan.list', {type_id => 1});
-#$soapresult = $proxy->call('TestPlan.list', {plan_id => 2, planidtype => "lessthan"});
-
-#$soapresult = $proxy->call('TestCase.list');
-#$soapresult = $proxy->call('TestCase.list', {default_tester_id => 1});
-#$soapresult = $proxy->call('TestCase.list', {case_id => 1});
-#$soapresult = $proxy->call('TestCase.list', $query);
-
-#$soapresult = $proxy->call('TestRun.list');
-
-#$soapresult = $proxy->call('TestCaseRun.list');
-#$soapresult = $proxy->call('TestCaseRun.list', {build_id => 1, case_id => 1, run_id => 1, case_run_status_id => 1});
-
-#$soapresult = $proxy->call('TestPlan.get', 1);
-
-#$soapresult = $proxy->call('TestCase.get', 1);
-
-#$soapresult = $proxy->call('TestRun.get', 2);
-
-#$soapresult = $proxy->call('TestCaseRun.get', 1);
-
-#$soapresult = $proxy->call('TestPlan.create', { name=>"Another Test Plan", 
-#	                                         product_id=>1, 
-#	                                         author_id=>1, 
-#	                                         editor_id=>1, 
-#	                                         type_id=>1,
-#	                                         default_product_version=>1
-#	                                       });
-#$soapresult = $proxy->call('TestPlan.get', 54);
-#$soapresult = $proxy->call('TestPlan.get_components', 2);
-#$soapresult = $proxy->call('TestPlan.add_tag', 1, 'This is a new tag');
-#$soapresult = $proxy->call('TestPlan.remove_tag', 1, 'This is a new tag');
-#$soapresult = $proxy->call('TestPlan.get_tags', 1);
-#$soapresult = $proxy->call('TestPlan.lookup_type_name_by_id', 1);
-#$soapresult = $proxy->call('TestPlan.lookup_type_id_by_name', 'Unit');
-#$soapresult = $proxy->call('TestPlan.get_test_cases', 1);
-#$soapresult = $proxy->call('TestPlan.get_test_runs', 1);
-
-
-#$soapresult = $proxy->call('TestCase.create', { isautomated=>0,
-#						 category_id=>1,
-#						 case_status_id=>2,
-#						 author_id=>1,
-#                                                plan_id=>2
-#                                              }); 
-#$soapresult = $proxy->call('TestCase.get', 3);
-#$soapresult = $proxy->call('TestCase.get_plans', 1);
-#$soapresult = $proxy->call('TestCase.link_plan', 1, 2);
-#$soapresult = $proxy->call('TestCase.unlink_plan', 1, 2);
-
-#$soapresult = $proxy->call('TestCase.get_components', 1);
-#$soapresult = $proxy->call('TestCase.add_tag', 1, 'This is a new tag');
-#$soapresult = $proxy->call('TestCase.remove_tag', 1, 'This is a new tag');
-#$soapresult = $proxy->call('TestCase.get_tags', 1);
-#$soapresult = $proxy->call('TestCase.lookup_category_name_by_id', 1);
-#$soapresult = $proxy->call('TestCase.lookup_category_id_by_name', 'Test Category 1');
-#$soapresult = $proxy->call('TestCase.lookup_status_name_by_id', 1);
-#$soapresult = $proxy->call('TestCase.lookup_status_id_by_name', 'PROPOSED');
-#$soapresult = $proxy->call('TestCase.lookup_priority_name_by_id', 1);
-#$soapresult = $proxy->call('TestCase.lookup_priority_id_by_name', 'P1');
-
-#$soapresult = $proxy->call('TestCaseRun.create', { assignee => 1,
-#                                                   build_id => 1,
-#                                                   case_id => 1,
-#                                                   case_text_version => 1,
-#                                                   environment_id => 1,
-#                                                   run_id => 1,
-#                                                 });
-#$soapresult = $proxy->call('TestCaseRun.get', 3);
-#$soapresult = $proxy->call('TestCaseRun.lookup_status_name_by_id', 1);
-#$soapresult = $proxy->call('TestCaseRun.lookup_status_id_by_name', 'IDLE');
-
-#$soapresult = $proxy->call('TestRun.create', { plan_id => 1,
-#                                               environment_id => 1,
-#                                               build_id => 1,
-#                                               plan_text_version => '1',
-#                                               manager_id => 1,
-#                                               summary => 'This is test run 2.',
-#                                             });
-#$soapresult = $proxy->call('TestRun.get', 3);
-#$soapresult = $proxy->call('TestRun.get_test_plan', 3);
-#$soapresult = $proxy->call('TestRun.get_test_cases', 1575);
-#$soapresult = $proxy->call('TestRun.get_test_case_runs', 1);
-#$soapresult = $proxy->call('TestRun.lookup_environment_name_by_id', 190);
-#$soapresult = $proxy->call('TestRun.lookup_environment_id_by_name', 'Linux');
-#$soapresult = $proxy->call('TestRun.add_tag', 1, 'This is a new tag');
-#$soapresult = $proxy->call('TestRun.remove_tag', 1, 'This is a new tag');
-#$soapresult = $proxy->call('TestRun.get_tags', 1);
-
-#$soapresult = $proxy->call('TestPlan.update', 1, {product_id => 1});
-#$soapresult = $proxy->call('TestCase.update', 1, {isautomated => 1});
-#$soapresult = $proxy->call('TestCase.update', 239137, {author_id => 1});
-#$soapresult = $proxy->call('TestCase.update', 1, {author_id => 2});
-#$soapresult = $proxy->call('TestRun.update', 2, { notes => 'These are new notes for test run 2' });
-#$soapresult = $proxy->call('TestCaseRun.update', 1, 1, 1, { case_run_status_id => 1 });
-
-#$soapresult = $proxy->call('TestCase.store_text', 2, 1, 'Another action for test case 2', 'Another effect for test case 2');
-#$soapresult = $proxy->call('TestCase.get_text', 2);
-
-#$soapresult = $proxy->call('Environment.create', {product_id=>275, name=>'linuxtesttest', isactive=>1});
-#$soapresult = $proxy->call('Environment.get', 1);
+#$soapresult = $proxy->call('Environment.check_environment', 'Linux', 2);
+#$soapresult = $proxy->call('Environment.check_environment', 'Linux', 'Bugzilla');
+#$soapresult = $proxy->call('Environment.create', {product_id=>2, name=>'Environment '.time() , isactive=>1});
+#$soapresult = $proxy->call('Environment.get', 1018);
 #$soapresult = $proxy->call('Environment.list', {environment_id=>330});
-#$soapresult = $proxy->call('Environment.list', {environment_name=>'t1'});
-#$soapresult = $proxy->call('Environment.update', 2, {name=>'Second Environment'});
+#$soapresult = $proxy->call('Environment.list', {name=>'Linux'});
+#$soapresult = $proxy->call('Environment.update', 1018, {name=>'Second Environment'});
 #$soapresult = $proxy->call('Environment.get_runs', 1);
+#$soapresult = $proxy->call('Environment.get_caseruns', 1);
+
+#######################
+### Product Methods ###
+#######################
+
+#$soapresult = $proxy->call('Product.get', 2);
+#$soapresult = $proxy->call('Product.check_product', 'Bugzilla');
+#$soapresult = $proxy->call('Product.check_category', 'CGI', 'Bugzilla');
+#$soapresult = $proxy->call('Product.get_builds', 2);
+#$soapresult = $proxy->call('Product.get_cases', 2);
+#$soapresult = $proxy->call('Product.get_categories', 2);
+#$soapresult = $proxy->call('Product.get_components', 2);
+#$soapresult = $proxy->call('Product.get_environments', 2);
+#$soapresult = $proxy->call('Product.get_milestones', 2);
+#$soapresult = $proxy->call('Product.get_plans', 2);
+#$soapresult = $proxy->call('Product.get_runs', 2);
+#$soapresult = $proxy->call('Product.get_tags', 2);
+#$soapresult = $proxy->call('Product.get_versions', 2);
+
+########################
+### TestCase Methods ###
+########################
+
+#$soapresult = $proxy->call('TestCase.add_component', [278,304],[2,3,4]);
+#$soapresult = $proxy->call('TestCase.add_tag', [278,304], ['Fred','Fish']);
+#$soapresult = $proxy->call('TestCase.add_to_run', [278,306], [1700,1701]);
+#$soapresult = $proxy->call('TestCase.attach_bug', [278,306], [33,44]);
+#$soapresult = $proxy->call('TestCase.calculate_average_time', 278);
+#$soapresult = $proxy->call('TestCase.create', {case_status_id => 'CONFIRMED', category_id => 'CGI', priority_id => 'P5 - None', summary => 'API TEST CASE', plans => [74]});
+#$soapresult = $proxy->call('TestCase.create', [{case_status_id => 'CONFIRMED', category_id => 'CGI', priority_id => 'P5 - None', summary => 'API TEST CASE', plans => [74]}]);
+#$soapresult = $proxy->call('TestCase.detach_bug', 278, 33);
+#$soapresult = $proxy->call('TestCase.get', 278);
+#$soapresult = $proxy->call('TestCase.get_bugs', 278);
+#$soapresult = $proxy->call('TestCase.get_case_run_history', 278);
+#$soapresult = $proxy->call('TestCase.get_change_history', 278);
+#$soapresult = $proxy->call('TestCase.get_components', 278);
+#$soapresult = $proxy->call('TestCase.get_plans', 278);
+#$soapresult = $proxy->call('TestCase.get_tags', 278);
+#$soapresult = $proxy->call('TestCase.get_text', 278,3);
+#$soapresult = $proxy->call('TestCase.link_plan', 278, [77,78]);
+#$soapresult = $proxy->call('TestCase.list', {default_tester => 'ghendricks@novell.com'});
+#$soapresult = $proxy->call('TestCase.lookup_category_id_by_name' );
+#$soapresult = $proxy->call('TestCase.lookup_category_name_by_id' );
+#$soapresult = $proxy->call('TestCase.lookup_priority_id_by_name', 'P5 - None');
+#$soapresult = $proxy->call('TestCase.lookup_priority_name_by_id', 3);
+#$soapresult = $proxy->call('TestCase.lookup_status_id_by_name', 'CONFIRMED');
+#$soapresult = $proxy->call('TestCase.lookup_status_name_by_id', 1);
+#$soapresult = $proxy->call('TestCase.remove_component', 278,2);
+#$soapresult = $proxy->call('TestCase.remove_tag', 278, 'fish');
+#$soapresult = $proxy->call('TestCase.store_text', 278, 'vrb@novell.com', 'FOO', 'FISH', 'FIGHT', 'FUN');
+#$soapresult = $proxy->call('TestCase.unlink_plan', 278, 78);
+#$soapresult = $proxy->call('TestCase.update', 278,{priority_id => 'P2 - High', case_status_id=>3 ,summary=>'This was Entering bugs'});
+
+###########################
+### TestCaseRun Methods ###
+###########################
+
+#$soapresult = $proxy->call('TestCaseRun.attach_bug', 65104, [33,44] );
+#$soapresult = $proxy->call('TestCaseRun.create', {case_id => 765, run_id => 501, build_id => 306, environment_id =>7});
+#$soapresult = $proxy->call('TestCaseRun.detach_bug', 65104, 33);
+#$soapresult = $proxy->call('TestCaseRun.get', 65104);
+#$soapresult = $proxy->call('TestCaseRun.get', 501, 765, 306, 1);
+#$soapresult = $proxy->call('TestCaseRun.get_bugs', 65104);
+#$soapresult = $proxy->call('TestCaseRun.get_completion_time', 65104);
+#$soapresult = $proxy->call('TestCaseRun.get_history',65104 );
+#$soapresult = $proxy->call('TestCaseRun.list', );
+#$soapresult = $proxy->call('TestCaseRun.lookup_status_id_by_name', 'PASSED');
+#$soapresult = $proxy->call('TestCaseRun.lookup_status_name_by_id', 3);
+#$soapresult = $proxy->call('TestCaseRun.update', 65104, {status=>3});
+#$soapresult = $proxy->call('TestCaseRun.update', [65104,6105,6106] , {status=>3});
+#$soapresult = $proxy->call('TestCaseRun.update', 501, 765, 306, 1, {status=>3});
+
+########################
+### TestPlan Methods ###
+########################
+
+#$soapresult = $proxy->call('TestPlan.add_tag', 74, 'Fish');
+#$soapresult = $proxy->call('TestPlan.create', {product_id => 'Bugzilla', name=>'API TEST PLAN', type_id=>'Integration', default_product_version=>'3.0'});
+#$soapresult = $proxy->call('TestPlan.get', 74);
+#$soapresult = $proxy->call('TestPlan.get_change_history', 74);
+#$soapresult = $proxy->call('TestPlan.get_product', 74);
+#$soapresult = $proxy->call('TestPlan.get_tags', 74);
+#$soapresult = $proxy->call('TestPlan.get_test_cases', 74);
+#$soapresult = $proxy->call('TestPlan.get_test_runs', 74);
+#$soapresult = $proxy->call('TestPlan.get_text', 74, 3);
+#$soapresult = $proxy->call('TestPlan.list', {product_id=>2, name=> 'selenium'});
+#$soapresult = $proxy->call('TestPlan.lookup_type_id_by_name', 'Integration');
+#$soapresult = $proxy->call('TestPlan.lookup_type_name_by_id', 11);
+#$soapresult = $proxy->call('TestPlan.remove_tag', 74, 'Fish');
+#$soapresult = $proxy->call('TestPlan.store_text', 74, 'THIS IS A TEST OF THE PLAN TEXT VIA API');
+#$soapresult = $proxy->call('TestPlan.update', 74, {name=>'API UPDATE', type_id=>3, default_product_version=> '2.22'});
+
+#######################
+### TestRun Methods ###
+#######################
+
+#$soapresult = $proxy->call('TestRun.add_tag', 501, "Fish");
+#$soapresult = $proxy->call('TestRun.create', {plan_id => 97, environment_id => 'test', build_id => 'linux', summary => 'API TEST RUN', manager_id => 'ghendricks@novell.com', product_version=>'1.2'});
+#$soapresult = $proxy->call('TestRun.get', 501);
+#$soapresult = $proxy->call('TestRun.get_change_history', 501);
+#$soapresult = $proxy->call('TestRun.get_completion_report', 501);
+#$soapresult = $proxy->call('TestRun.get_tags', 501);
+#$soapresult = $proxy->call('TestRun.get_test_case_runs', 501);
+#$soapresult = $proxy->call('TestRun.get_test_cases', 501);
+#$soapresult = $proxy->call('TestRun.get_test_plan', 501);
+#$soapresult = $proxy->call('TestRun.list', {plan => 97});
+#$soapresult = $proxy->call('TestRun.remove_tag', 501, 'fish' );
+#$soapresult = $proxy->call('TestRun.update', 501, {environment_id => 'test', build_id => 'linux', summary => 'API TEST RUN', manager_id => 'ghendricks@novell.com', product_version=>'1.2'});
 
 show_results('The results are: ', $soapresult);
+
