b138044f9627049ed9fcf8fb4ab5adb539ea7a2b
bugzilla%standard8.plus.com <bugzilla%standard8.plus.com>
2008-03-19 21:48:04 +0000

    Bug 422214 Enable Code Size tests on Thunderbird tinderboxes. r/sr=dmose

--- tools/tinderbox-configs/thunderbird/macosx/tinder-config.pl
+++ tools/tinderbox-configs/thunderbird/macosx/tinder-config.pl
@@ -67,8 +67,8 @@ $AliveTest                = 1;
 #$BloatTest2               = 0;  # dbaron memory bloat test, require tracemalloc
 #$DomToTextConversionTest  = 0;  
 #$XpcomGlueTest            = 0;
-#$CodesizeTest             = 0;  # Z,  require mozilla/tools/codesighs
-#$EmbedCodesizeTest        = 0;  # mZ, require mozilla/tools/codesigns
+$CodesizeTest             = 1;  # Z,  require mozilla/tools/codesighs
+$EmbedCodesizeTest        = 0;  # mZ, require mozilla/tools/codesigns
 #$MailBloatTest            = 0;
 #$EmbedTest                = 0;  # Assumes you wanted $BuildEmbed=1
 #$LayoutPerformanceTest    = 0;  # Tp
