70261614df157ef155c2464efa55f43f0e8810c2
christophe.ravel.bugs%sun.com <christophe.ravel.bugs%sun.com>
2008-04-04 20:37:01 +0000

    Prepare NSS 3.12 RC1.

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -36,7 +36,7 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
-/* $Id: nss.h,v 1.54 2008-03-16 13:07:52 kaie%kuix.de Exp $ */
+/* $Id: nss.h,v 1.55 2008-04-04 20:37:00 christophe.ravel.bugs%sun.com Exp $ */
 
 #ifndef __nss_h_
 #define __nss_h_
@@ -70,11 +70,11 @@ SEC_BEGIN_PROTOS
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>][ <ECC>][ <Beta>]"
  */
-#define NSS_VERSION  "3.12" _NSS_ECC_STRING " Beta" _NSS_CUSTOMIZED
+#define NSS_VERSION  "3.12.0.0" _NSS_ECC_STRING _NSS_CUSTOMIZED
 #define NSS_VMAJOR   3
 #define NSS_VMINOR   12
 #define NSS_VPATCH   0
-#define NSS_BETA     PR_TRUE
+#define NSS_BETA     PR_FALSE
 
 /*
  * Return a boolean that indicates whether the underlying library
