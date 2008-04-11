7454f3e3640f03d70f43a19c1fb47b3dbe61e05c
ccooper%deadsquid.com <ccooper%deadsquid.com>
2008-04-11 15:21:53 +0000

    b=419759 r=rcampbell - Add triadic coverage for Win2k3 on trunk, and turn back on mochichrome testing

--- tools/buildbot-configs/testing/unittest/auth.py
+++ tools/buildbot-configs/testing/unittest/auth.py
@@ -4,6 +4,8 @@ debugPassword = "swordfish"
 
 authList = [("win-vc8", "swordfish"),
             ("win-2k3", "swordfish"),
+            ("win-2k3-2", "swordfish"),
+            ("win-2k3-3", "swordfish"),
             ("linux","swordfish"),
             ("mac-osx", "swordfish"),
             ("qm-xserve02", "swordfish"),
