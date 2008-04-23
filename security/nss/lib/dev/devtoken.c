d1203dd0e813613ff8282ddcddec842387015eaa
rrelyea%redhat.com <rrelyea%redhat.com>
2008-04-23 16:37:19 +0000

    Back out patch to bug 391903.
    Windows tinderboxen are failing.

--- security/nss/lib/dev/devtoken.c
+++ security/nss/lib/dev/devtoken.c
@@ -35,7 +35,7 @@
  * ***** END LICENSE BLOCK ***** */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: devtoken.c,v $ $Revision: 1.44 $ $Date: 2008-04-21 23:54:49 $";
+static const char CVS_ID[] = "@(#) $RCSfile: devtoken.c,v $ $Revision: 1.45 $ $Date: 2008-04-23 16:37:19 $";
 #endif /* DEBUG */
 
 #ifndef NSSCKEPV_H
@@ -70,8 +70,6 @@ nssToken_Destroy (
 	if (PR_AtomicDecrement(&tok->base.refCount) == 0) {
 	    PZ_DestroyLock(tok->base.lock);
 	    nssTokenObjectCache_Destroy(tok->cache);
-	    /* in 3.4 the token manages the slot, not the other way around */
-	    nssSlot_Destroy(tok->slot);
 	    return nssArena_Destroy(tok->base.arena);
 	}
     }
