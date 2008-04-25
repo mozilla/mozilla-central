fea4cb96236e52c14390a359ef3032ff9238af67
ccooper%deadsquid.com <ccooper%deadsquid.com>
2008-04-25 16:00:07 +0000

    b=425791 r=rcampbell - Add triadic test slaves for Linux

--- tools/buildbot-configs/testing/unittest/auth.py
+++ tools/buildbot-configs/testing/unittest/auth.py
@@ -7,6 +7,8 @@ authList = [("win-vc8", "swordfish"),
             ("win-2k3-2", "swordfish"),
             ("win-2k3-3", "swordfish"),
             ("linux","swordfish"),
+            ("linux-2","swordfish"),
+            ("linux-3","swordfish"),
             ("mac-osx", "swordfish"),
             ("qm-xserve02", "swordfish"),
             ("qm-win2k3-pgo01", "swordfish")]
