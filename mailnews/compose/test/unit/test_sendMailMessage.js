d980e561d8885af7d8861b139349f5f1e0df85f9
bugzilla%standard8.plus.com <bugzilla%standard8.plus.com>
2008-06-05 09:42:05 +0000

    Second part of bug 436847 fix message file format to the interface requirement, add some more documentation, and a second test for basic auth. r/sr=bienvenu

--- mailnews/compose/test/unit/test_sendMailMessage.js
+++ mailnews/compose/test/unit/test_sendMailMessage.js
@@ -6,64 +6,27 @@
  * between mailnews and SMTP server. It does not check the data of the message
  * either side of the link, it will be extended later to do that.
  */
-var daemon = setup_daemon();
-
 var type = null;
 var test = null;
+var server;
 
-function do_check_transaction(real, expected) {
-  // real.them may have an extra QUIT on the end, where the stream is only
-  // closed after we have a chance to process it and not them. We therefore
-  // excise this from the list
-  if (real.them[real.them.length-1] == "QUIT")
-    real.them.pop();
-
-  do_check_eq(real.them.join(","), expected.join(","));
-  dump("Passed test " + test + "\n");
-}
-
-const SMTP_PORT = 1024+120;
 const kSender = "from@invalid.com";
 const kTo = "to@invalid.com";
 
 function test_RFC2822() {
   type = "RFC 2822";
 
-  // Get the servers
-  var acctMgr = Cc["@mozilla.org/messenger/account-manager;1"]
-                  .getService(Ci.nsIMsgAccountManager);
-
-  var smtpService = Cc["@mozilla.org/messengercompose/smtp;1"]
-                      .getService(Ci.nsISmtpService);
-
   // Test file
   var testFile = do_get_file("mailnews/compose/test/unit/data/message1.eml");
-
-  // Setup the daemon and server
-  var handler = new SMTP_RFC2822_handler(daemon);
-  var server = new nsMailServer(handler);
 
   // Ensure we have at least one mail account
   loadLocalMailAccount();
 
-  // Create an smtp server and fill in the details.
-  var smtpServer = smtpService.createSmtpServer();
+  var smtpServer = getBasicSmtpServer();
+  var identity = getSmtpIdentity(kSender, smtpServer);
 
-  smtpServer.hostname = "localhost";
-  smtpServer.port = SMTP_PORT;
-  // Set the authentication method to "none"
-  smtpServer.authMethod = 0;
-
-  // Override the default greeting so we get something predicitable
-  var prefSvc = Components.classes["@mozilla.org/preferences-service;1"]
-    .getService(Components.interfaces.nsIPrefBranch);
-
-  prefSvc.setCharPref("mail.smtpserver.default.hello_argument", "test");
-
-  // Set up the identity
-  var identity = acctMgr.createIdentity();
-  identity.email = kSender;
-  identity.smtpServerKey = smtpServer.key;
+  var smtpService = Cc["@mozilla.org/messengercompose/smtp;1"]
+                      .getService(Ci.nsISmtpService);
 
   // Handle the server in a try/catch/finally loop so that we always will stop
   // the server if something fails.
@@ -75,16 +38,43 @@ function test_RFC2822() {
     test = "Basic sendMailMessage";
 
     smtpService.sendMailMessage(testFile, kTo, identity,
-                                identity.password, null, null, null,
+                                null, null, null, null,
                                 false, {}, {});
 
     server.performTest();
 
     var transaction = server.playTransaction();
     do_check_transaction(transaction, ["EHLO test",
-                                       "MAIL FROM:<" + kSender + "> SIZE=80",
+                                       "MAIL FROM:<" + kSender + "> SIZE=155",
                                        "RCPT TO:<" + kTo + ">",
                                        "DATA"]);
+
+    server.resetTest();
+
+    // This time with auth
+    test = "Auth sendMailMessage";
+
+    smtpServer.authMethod = 1;
+    smtpServer.useSecAuth = false;
+    smtpServer.trySecAuth = false;
+    smtpServer.trySSL = false;
+    smtpServer.username = "test";
+    smtpServer.password = "password";
+
+    smtpService.sendMailMessage(testFile, kTo, identity,
+                                null, null, null, null,
+                                false, {}, {});
+
+    server.performTest();
+
+    var transaction = server.playTransaction();
+    do_check_transaction(transaction, ["EHLO test",
+                                       "AUTH PLAIN AHRlc3QAcGFzc3dvcmQ=",
+                                       "MAIL FROM:<" + kSender + "> SIZE=155",
+                                       "RCPT TO:<" + kTo + ">",
+                                       "DATA"]);
+
+
   } catch (e) {
     do_throw(e);
   } finally {
@@ -97,5 +87,7 @@ function test_RFC2822() {
 }
 
 function run_test() {
+  server = setupServerDaemon();
+
   test_RFC2822();
 }
--- mailnews/test/fakeserver/smtpd.js
+++ mailnews/test/fakeserver/smtpd.js
@@ -20,81 +20,65 @@ function SMTP_RFC2822_handler(daemon) {
 }
 SMTP_RFC2822_handler.prototype = {
   EHLO: function (args) {
-    dump("got ELHO\n");
-    return "250-foo.com greets bar.com\n250-8BITMIME\n250-SIZE\n250-AUTH PLAIN LOGIN EXTERNAL GSSAPI CRAM-MD5 MSN\n250 HELP";
+    return "250-foo.com greets bar.com\n250-8BITMIME\n250-SIZE\n250-AUTH PLAIN\n250 HELP";
   },
   AUTH: function (args) {
-    dump("AUTH " + args + "\n");
-    return "250 ok";
+    return "235 authentication successful";
   },
   MAIL: function (args) {
-    dump("mail\n");
     return "250 ok";
   },
   RCPT: function(args) {
-    dump(" RCPT\n");
     return "250 ok";
   },
   DATA: function(args) {
-    dump("DATA\n");
-    this.posting = true;
+    this.expectingData = true;
     return "354 ok\n";
   },
   RSET: function (args) {
-    dump("RSET\n");
     return "250 ok\n";
   },
   VRFY: function (args) {
-    dump("VRFY\n");
     return "250 ok\n";
   },
   EXPN: function (args) {
-    dump("EXPN\n");
     return "250 ok\n";
   },
   HELP: function (args) {
-    dump("HELP\n");
     return "211 ok\n";
   },
   NOOP: function (args) {
-    dump("NOOP\n");
     return "250 ok\n";
   },
   QUIT: function (args) {
-    dump(" got QUIT\n");
     this.closing = true;
-    dump(this.post + "\n");
     return "221 done";
   },
   onStartup: function () {
-    dump("started\n");
     this.closing = false;
     return "220 ok";
   },
   onError: function (command, args) {
-    dump("unrecognized " + command + " args " + args + "\n");
     return "500 not recognized\n";
   },
-  onMultiline : function(line) {
-    dump("line: " + line + "\n");
+  onMultiline: function(line) {
     if (line == ".") {
-      if (this.posting) {
-        this.posting = false;
+      if (this.expectingData) {
+        this.expectingData = false;
         return "250 Wonderful article, your style is gorgeous!";
 	    }
     }
 
-    if (this.posting) {
+    if (this.data) {
 	    if (line[0] == '.')
         line = line.substring(1);
 	    this.post += line+'\n';
     }
-    return "250 OK";
+    return undefined;
   },
-
-  postCommand: function (obj) {
+  postCommand: function(obj) {
     if (this.closing)
-	    obj.closeSocket();
-    obj.setMultiline(this.posting);
+      obj.closeSocket();
+    obj.setMultiline(this.expectingData);
   }
 }
