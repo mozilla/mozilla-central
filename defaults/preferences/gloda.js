a458d13f03d0ff94216a4632922791afe873d9eb
Andrew Sutherland <asutherland@asutherland.org>
2008-06-09 21:02:44 -0400

    initial import from Ted Mielczarek's extensionwiz, plus eclipse .project

---
+++ defaults/preferences/gloda.js
@@ -0,0 +1,5 @@
+pref("extensions.gloda.boolpref", false);
+pref("extensions.gloda.intpref", 0);
+pref("extensions.gloda.stringpref", "A string");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.gloda@mozillamessaging.com.description", "chrome://gloda/locale/gloda.properties");
