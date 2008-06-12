658ea91910758452f7b55d58f3d02e1c65b874a5
ted.mielczarek%gmail.com <ted.mielczarek%gmail.com>
2008-06-12 19:38:37 +0000

    get rid of objdir stuff I'm not using on the mozillabuild-builder vm

--- tools/build-environment/win32/make-mozillabuild.bat
+++ tools/build-environment/win32/make-mozillabuild.bat
@@ -9,10 +9,6 @@ set SRCDIR=%~dp0%
 call "%VC8DIR%\VC\bin\vcvars32.bat"
 set INCLUDE=%SDKDIR%\Include\atl;%INCLUDE%
 
-rmdir /S %SRCDIR%\_obj
-mkdir %SRCDIR%\_obj
-
-cd %SRCDIR%\_obj
 %PYTHONDIR%\python.exe ..\packageit.py --msys c:\msys --output c:\stage
 
 pause
