6a1f05a51137ccaec5719d7a5250e29251247b3e
bugzilla%standard8.plus.com <bugzilla%standard8.plus.com>
2008-06-18 14:20:24 +0000

    Bug 437098 Enable junk token limits. p=Kent James <kent@caspia.com>,r=Neil,sr=bienvenu

--- mailnews/mailnews.js
+++ mailnews/mailnews.js
@@ -508,6 +508,8 @@ pref("mail.spam.logging.enabled", false);
 pref("mail.spam.manualMark", false);
 pref("mail.spam.markAsReadOnSpam", false);
 pref("mail.spam.manualMarkMode", 0); // 0 == "move to junk folder", 1 == "delete"
+// the number of allowed bayes tokens before the database is shrunk
+pref("mailnews.bayesian_spam_filter.junk_maxtokens", 100000);
 
 pref("mail.autoComplete.highlightNonMatches", true);
 
