bdff1be44bf03a5d3c92a2c2a810f4300e094fc5
reed%reedloden.com <reed%reedloden.com>
2008-07-12 11:49:38 +0000

    Bug 71105 - "No new mail notification (biff) in browser until mail/news window has been opened at least once" ((Cv1) <mailnews.js>) [p=sgautherie.bz@free.fr (Serge Gautherie) r=Neil sr=bienvenu]

--- mailnews/mailnews.js
+++ mailnews/mailnews.js
@@ -613,8 +613,10 @@ pref("mail.biff.play_sound.url", "");
 pref("mail.biff.show_alert", true);
 pref("mail.biff.show_tray_icon", true); // currently Windows-only
 pref("mail.biff.animate_dock_icon", false);
+#ifdef MOZ_SUITE
 // if true, check for new mail even when opening non-mail windows
 pref("mail.biff.on_new_window", true);
+#endif
 
 pref("mail.content_disposition_type", 0);
 
