a6cd871547e111e9f10b459bff216708e2d8929b
mozilla%kewis.ch <mozilla%kewis.ch>
2008-07-17 13:29:24 +0000

    Fix bug 444551 - Select an event in a readonly calendar and use the delete key -> error message. r=berend

--- calendar/base/content/calendar-views.js
+++ calendar/base/content/calendar-views.js
@@ -192,8 +192,11 @@ var calendarViewController = {
 
         // Make sure we are modifying a copy of aOccurrences, otherwise we will
         // run into race conditions when the view's doDeleteItem removes the
-        // array elements while we are iterating through them.
-        var occurrences = aOccurrences.slice(0);
+        // array elements while we are iterating through them. While we are at
+        // it, filter out any items that have readonly calendars, so that
+        // checking for one total item below also works out if all but one item
+        // are readonly.
+        var occurrences = aOccurrences.filter(function(item) { return isCalendarWritable(item.calendar); });
 
         for each (var itemToDelete in occurrences) {
             if (aUseParentItems) {
