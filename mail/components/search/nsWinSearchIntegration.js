a1a22498522531ca239350f984be061448c9e00a
bugzilla%standard8.plus.com <bugzilla%standard8.plus.com>
2008-07-17 08:05:13 +0000

    Bug 430614 [GSoC] Thunderbird integration into Windows Vista/Windows Search indexer. p=Siddharth Agarwal <sid1337@gmail.com>,r=beckley,sr=bienvenu

--- mail/components/search/nsWinSearchIntegration.js
+++ mail/components/search/nsWinSearchIntegration.js
@@ -1,4 +1,4 @@
-# -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+# -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 # ***** BEGIN LICENSE BLOCK *****
 # Version: MPL 1.1/GPL 2.0/LGPL 2.1
 #
@@ -39,19 +39,21 @@
 #include content/searchCommon.js
 
 const MSG_DB_LARGE_COMMIT = 1;
+const MSG_FLAG_ATTACHMENT = 0x10000000;
+const CRLF="\r\n";
 
 // The property of the header that's used to check if a message is indexed
-const gHdrIndexedProperty = "indexed";
+const gHdrIndexedProperty = "wds_indexed";
 
 // The file extension that is used for support files of this component
-const gFileExt = ".mozeml";
+const gFileExt = ".wdseml";
 
 // The pref base
-const gPrefBase = "mail.spotlight";
+const gPrefBase = "mail.winsearch";
 
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
-function InitSpotlightIntegration()
+function InitWinSearchIntegration()
 {
   var enabled;
   try {
@@ -62,31 +64,26 @@ function InitSpotlightIntegration()
   if (!enabled)
     return;
 
-  SIDump("initializing spotlight integration\n");
+  SIDump("Initializing Windows Search integration\n");
   InitSupportIntegration();
 }
 
-function xmlEscapeString(s)
-{
- s = s.replace(/&/g, "&amp;");
- s = s.replace(/>/g, "&gt;");
- s = s.replace(/</g, "&lt;");
- return s;
-}
-
-var fileHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.\ncom/DTDs/PropertyList-1.0.dtd\">\n<plist version=\"1.0\">\n<dict>";
-
+// The stream listener to read messages
 var gStreamListener = {
 _buffer: "",
 outputFile: null,
 outputStream: null,
 unicodeConverter: null,
-subject: null,
+// subject: null,
 message: null,
-msgHdr:null,
+msgHdr: null,
+mimeHdrObj: null,
+mimeHdrParamObj: null,
 
 onDoneStreamingCurMessage: function(successful)
 {
+  if (this.outputStream)
+    this.outputStream.close();
   if (!successful && this.msgHdr)
   {
     var file = GetSupportFileForMsgHdr(this.msgHdr);
@@ -101,8 +98,7 @@ onDoneStreamingCurMessage: function(successful)
 },
 
 QueryInterface: function(aIId, instance) {
-  if (aIId.equals(Ci.nsIStreamListener) ||
-      aIId.equals(Ci.nsISupports))
+  if (aIId.equals(Ci.nsIStreamListener) || aIId.equals(Ci.nsISupports))
     return this;
 
   Components.returnCode = Components.results.NS_ERROR_NO_INTERFACE;
@@ -112,50 +108,12 @@ QueryInterface: function(aIId, instance) {
 onStartRequest: function(request, context) {
   try
   {
-
     var outputFileStream =  Cc["@mozilla.org/network/file-output-stream;1"]
       .createInstance(Ci.nsIFileOutputStream);
     outputFileStream.init(this.outputFile, -1, -1, 0);
-    this.outputStream = outputFileStream.QueryInterface(Ci.nsIOutputStream);
-    this.outputStream.write(fileHeader, fileHeader.length);
-    this.outputStream.write("<key>kMDItemLastUsedDate</key><string>", 38);
-    // need to write the date as a string
-    var curTimeStr = new Date().toLocaleString();
-    this.outputStream.write(curTimeStr, curTimeStr.length);
-    // need to write the subject in utf8 as the title
-    this.outputStream.write("</string>\n<key>kMDItemTitle</key>\n<string>", 42);
-
-    if (!this.unicodeConverter)
-    {
-      this.unicodeConverter = Cc["@mozilla.org/intl/scriptableunicodeconverter"]
-      .createInstance(Ci.nsIScriptableUnicodeConverter);
-      this.unicodeConverter.charset = "UTF-8";
-
-    }
-    var utf8Subject  = this.unicodeConverter.ConvertFromUnicode(this.subject);
-    utf8Subject += this.unicodeConverter.Finish();
-    utf8Subject = xmlEscapeString(utf8Subject);
-    this.outputStream.write(utf8Subject, utf8Subject.length);
-
-    // need to write the subject in utf8 as the title
-    this.outputStream.write("</string>\n<key>kMDItemDisplayName</key>\n<string>", 48);
-    this.outputStream.write(utf8Subject, utf8Subject.length);
-
-    this.outputStream.write("</string>\n<key>kMDItemTextContent</key>\n<string>", 48);
-    var author = this.msgHdr.mime2DecodedAuthor;
-    var recipients = this.msgHdr.mime2DecodedRecipients;
-
-    var utf8Author = this.unicodeConverter.ConvertFromUnicode(author);
-    utf8Author += this.unicodeConverter.Finish() + " ";
-    utf8Author = xmlEscapeString(utf8Author);
-    var utf8Recipients = this.unicodeConverter.ConvertFromUnicode(recipients);
-    utf8Recipients += this.unicodeConverter.Finish() + " ";
-    utf8Recipients = xmlEscapeString(utf8Recipients);
-    this.outputStream.write(utf8Author, utf8Author.length);
-    this.outputStream.write(utf8Recipients, utf8Recipients.length);
-
-    this.outputStream.write(utf8Subject, utf8Subject.length);
-    this.outputStream.write(" ", 1);
+    this.outputStream = Cc["@mozilla.org/intl/converter-output-stream;1"]
+      .createInstance(Ci.nsIConverterOutputStream);
+    this.outputStream.init(outputFileStream, "UTF-8", 0, 0x0000);
   }
   catch (ex)
   {
@@ -166,30 +124,56 @@ onStartRequest: function(request, context) {
 onStopRequest: function(request, context, status, errorMsg) {
   try
   {
-    // we want to write out the from, to, cc, and subject headers into the
-    // Text Content value, so they'll be indexed.
+    // If, for some reason, the first four characters are "null", then remove them
+    if (this.message.substring(0, 4) == "null")
+      this.message = this.message.substring(4);
 
-    var stringStream = Cc["@mozilla.org/io/string-input-stream;1"].
-      createInstance(Ci.nsIStringInputStream);
-    stringStream.setData(this.message, this.message.length);
-    var temp = this.msgHdr.folder.getMsgTextFromStream(this.msgHdr, stringStream, 20000, 20000, false);
-    temp = xmlEscapeString(temp);
-    SIDump("utf8 text = *****************\n"+ temp + "\n");
-    this.outputStream.write(temp, temp.length);
-    // close out the content, dict, and plist
-    this.outputStream.write("</string>\n</dict>\n</plist>\n", 26);
+    // First get all the headers (everything till a double CRLF)
+    var headerLength = this.message.indexOf(CRLF+CRLF)+1;
 
-    this.outputStream.close();
-   // this.outputFile.
+    // XXX the below has to be replaced with getMsgTextFromStream
+
+    // Filter out attachments. First locate the content-type header.
+    if (!this.mimeHdrObj)
+      this.mimeHdrObj = Cc["@mozilla.org/messenger/mimeheaders;1"].createInstance(Ci.nsIMimeHeaders);
+    this.mimeHdrObj.initialize(this.message.substring(0, headerLength), headerLength);
+    var contentTypeHeader = this.mimeHdrObj.extractHeader("Content-Type", false);
+
+    if (!this.mimeHdrParamObj)
+      this.mimeHdrParamObj = Cc["@mozilla.org/network/mime-hdrparam;1"].createInstance(Ci.nsIMIMEHeaderParam);
+    // If a multipart header, then strip out attachments
+    var contentType = this.mimeHdrParamObj.getParameter(contentTypeHeader, null, null, true, {});
+    if (contentType.substring(0,10) == "multipart/")
+    {
+      var boundary = this.mimeHdrParamObj.getParameter(contentTypeHeader, "boundary", null, true, {});
+
+      // Search for the boundary after the headers. Remove everything from the second occurrence.
+      boundary = CRLF + "--" + boundary;
+      var getRidFrom = this.message.indexOf(boundary, headerLength);
+      getRidFrom = this.message.indexOf(boundary, getRidFrom + boundary.length);
+
+      // In case we've got more than one boundary so far
+      if (getRidFrom != -1) {
+        this.message = this.message.substring(0, getRidFrom);
+
+        // Add RFC required stuff to the end
+        this.message += boundary + "--" + CRLF;
+      }
+    }
+
+    // We just write whatever we have in |message| out to the file
+    this.outputStream.writeString(this.message);
+
     this.msgHdr.setUint32Property(gHdrIndexedProperty, 1);
     var msgDB = this.msgHdr.folder.getMsgDatabase(null);
     msgDB.Commit(MSG_DB_LARGE_COMMIT);
 
     this.message = "";
+    SIDump("Successfully written file\n");
   }
   catch (ex)
   {
-    dump(ex);
+    SIDump(ex);
     this.onDoneStreamingCurMessage(false);
     return;
   }
@@ -199,35 +183,41 @@ onStopRequest: function(request, context, status, errorMsg) {
 onDataAvailable: function(request, context, inputStream, offset, count) {
   try
   {
-    var inStream = Cc["@mozilla.org/scriptableinputstream;1"]
-      .createInstance(Ci.nsIScriptableInputStream);
+    var inStream = Cc["@mozilla.org/scriptableinputstream;1"].createInstance(Ci.nsIScriptableInputStream);
     inStream.init(inputStream);
 
     // It is necessary to read in data from the input stream
     var inData = inStream.read(count);
 
-    // ignore stuff after the first 20K or so
+    // If we've already reached the attachments, safely ignore.
+    if (this.filteredAttachments)
+      return 0;
+
+    // Also ignore stuff after the first 20K or so
     if (this.message && this.message.length > 20000)
       return 0;
+    var inStream = Cc["@mozilla.org/scriptableinputstream;1"].
+      createInstance(Ci.nsIScriptableInputStream);
+
+    inStream.init(inputStream);
 
     this.message += inData;
     return 0;
   }
   catch (ex)
   {
-    dump(ex);
+    SIDump(ex);
     onDoneStreamingCurMessage(false);
   }
 }
-
-}
+};
 
 /* XPCOM boilerplate code */
-function SpotlightIntegration() { }
-SpotlightIntegration.prototype = {
-  classDescription: "Spotlight Integration",
-  classID: Components.ID("{cc9c2a34-567b-451a-a942-1a1c3ec26e07}"),
-  contractID: "@mozilla.org/spotlight-search-integration;1",
+function WinSearchIntegration() { }
+WinSearchIntegration.prototype = {
+  classDescription: "Windows Search Integration",
+  classID: Components.ID("{451a70f0-1b4f-11dd-bd0b-0800200c9a66}"),
+  contractID: "@mozilla.org/windows-search-integration;1",
 
   _xpcom_categories: [{
     category: "app-startup",
@@ -245,15 +235,15 @@ SpotlightIntegration.prototype = {
       obsSvc.addObserver(this, "profile-after-change", false);
     break;
     case "profile-after-change":
-      try { InitSpotlightIntegration(); }
-      catch(err) { SIDump("Could not initialize spotlight component"); }
+      try { InitWinSearchIntegration(); }
+      catch(err) { SIDump("Could not initialize winsearch component"); }
     break;
     default:
       throw Components.Exception("Unknown topic: " + aTopic);
     }
   }
 };
-var components = [SpotlightIntegration];
+var components = [WinSearchIntegration];
 function NSGetModule(aCompMgr, aFileSpec)
 {
   return XPCOMUtils.generateModule(components);
