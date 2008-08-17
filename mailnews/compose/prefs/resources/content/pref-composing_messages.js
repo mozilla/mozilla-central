43034694253358db83f95d1f8b7f311d2fb74cf8
Karsten Düsterloh <mnyromyr@tprac.de>
2008-08-17 16:55:36 +0200

    Bug 429143: Migrate Composition prefpane; p=Bruno 'Aqualon' Escherl <aqualon@aquachan.de>, r=me, sr=Neil

--- mailnews/compose/prefs/resources/content/pref-composing_messages.js
+++ mailnews/compose/prefs/resources/content/pref-composing_messages.js
@@ -1,4 +1,41 @@
-var gAutoSaveInterval;
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is SeaMonkey project code.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corp.
+ * Portions created by the Initial Developer are Copyright (C) 2006
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Ian Neal <iann_bugzilla@blueyonder.co.uk>.
+ *   Bruno Escherl <aqualon@aquachan.de>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 var gLastSelectedLang;
 var gDictCount = 0;
 
@@ -8,16 +45,13 @@ function Startup() {
   else
     document.getElementById("spellingGroup").hidden = true;
 
-  gAutoSaveInterval = document.getElementById("autoSaveInterval");
-  EnableTextbox(document.getElementById("autoSave"), gAutoSaveInterval, true);
+  let value = document.getElementById("mail.compose.autosave").value;
+  EnableElementById("autoSaveInterval", value, false);
 }
 
-function EnableTextbox(aCheckbox, aTextbox, aStartingUp) {
-  aTextbox.disabled = (!aCheckbox.checked ||
-                       parent.hPrefWindow.getPrefIsLocked(aTextbox.getAttribute("prefstring")));
-
-  if (!aTextbox.disabled && !aStartingUp)
-    aTextbox.focus();
+function EnableMailComposeAutosaveInterval(aValue) {
+  let focus = (document.getElementById("autoSave") == document.commandDispatcher.focusedElement);
+  EnableElementById("autoSaveInterval", aValue, focus);
 }
 
 function PopulateFonts() {
