944613e20a6ab953b0ead9df24754e055f1f77c2
David Bienvenu <bienvenu@nventure.com>
2008-09-02 07:11:08 -0700

    fix truncated message pop3 download for global inbox, r=standard8, sr=neil 450649

--- mailnews/local/src/nsPop3Service.cpp
+++ mailnews/local/src/nsPop3Service.cpp
@@ -333,9 +333,9 @@ NS_IMETHODIMP nsPop3Service::NewURI(const nsACString &aSpec,
       rv = localFolder->GetFolderScanState(&folderScanState);
       NS_ENSURE_SUCCESS(rv, rv);
       nsCOMPtr <nsIMsgDBHdr> msgHdr;
-      nsCOMPtr<nsIMsgMessageUrl> msgUrl = do_QueryInterface(mailboxUrl, &rv);
-      NS_ENSURE_SUCCESS(rv,rv);
-      msgUrl->GetMessageHeader(getter_AddRefs(msgHdr));
+      nsMsgKey msgKey;
+      mailboxUrl->GetMessageKey(&msgKey);
+      folder->GetMessageHeader(msgKey, getter_AddRefs(msgHdr));
       // we do this to get the account key
       if (msgHdr)
         localFolder->GetUidlFromFolder(&folderScanState, msgHdr);
