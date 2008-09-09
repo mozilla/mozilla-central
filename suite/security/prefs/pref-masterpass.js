4d554aa3e5e89ce78c82ff4929adfac9de2f301b
Manuel Reimer <Manuel.Spam@nurfuerspam.de>
2008-09-09 12:57:55 +0100

    Bug 444170 - "Migrate SeaMonkey's Master Password preferences to new pref window" [r+sr=neil]

--- suite/security/prefs/pref-masterpass.js
+++ suite/security/prefs/pref-masterpass.js
@@ -22,6 +22,7 @@
  *
  * Contributor(s):
  *   Javier Delgadillo <javi@netscape.com>
+ *   Manuel Reimer <Manuel.Reimer@gmx.de>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -38,84 +39,82 @@
  * ***** END LICENSE BLOCK ***** */
 
 const nsIPK11Token = Components.interfaces.nsIPK11Token;
-const nsPK11TokenDB = "@mozilla.org/security/pk11tokendb;1";
-const nsIPK11TokenDB = Components.interfaces.nsIPK11TokenDB;
-const nsIDialogParamBlock = Components.interfaces.nsIDialogParamBlock;
-const nsDialogParamBlock = "@mozilla.org/embedcomp/dialogparam;1";
+var gInternalToken;
 
-var internal_token;
-
-function onMasterPasswordLoad()
-{
-  var tokendb = Components.classes[nsPK11TokenDB].getService(nsIPK11TokenDB);
-  internal_token = tokendb.getInternalKeyToken();
-  var askTimes = internal_token.getAskPasswordTimes();
-  switch (askTimes) {
-  case nsIPK11Token.ASK_FIRST_TIME:  askTimes = 0; break;
-  case nsIPK11Token.ASK_EVERY_TIME:  askTimes = 1; break;
-  case nsIPK11Token.ASK_EXPIRE_TIME: askTimes = 2; break;
-  }
-  var radiogroup = document.getElementById("passwordAskTimes");
-  var radioitem;
-  switch (askTimes) {
-  case 0: radioitem = document.getElementById("askFirstTime"); break;
-  case 1: radioitem = document.getElementById("askEveryTime"); break;
-  case 2: radioitem = document.getElementById("askTimeout"); break;
-  }
-  radiogroup.selectedItem = radioitem;
-  var timeout = internal_token.getAskPasswordTimeout();
-  var timeoutField = document.getElementById("passwordTimeout");
-  timeoutField.setAttribute("value", timeout);
-  
-  changePasswordSettings(false);
+function Startup() {
+  var tokendb = Components.classes["@mozilla.org/security/pk11tokendb;1"]
+                          .getService(Components.interfaces.nsIPK11TokenDB);
+  gInternalToken = tokendb.getInternalKeyToken();
 }
 
-function changePasswordSettings(setFocus)
-{
-  var askTimes = 0;
-  var timeout = internal_token.getAskPasswordTimeout();
-  var timeoutField = document.getElementById("passwordTimeout");
-  var radiogroup = document.getElementById("passwordAskTimes");
-  switch ( radiogroup.value ) {
-  case "0": 
-    timeoutField.setAttribute("disabled", true);
+function ReadAskForPassword() {
+  var value;
+  switch (gInternalToken.getAskPasswordTimes()) {
+  case nsIPK11Token.ASK_FIRST_TIME:
+    value = 0;
+    break;
+  case nsIPK11Token.ASK_EVERY_TIME:
+    value = 1;
+    break;
+  case nsIPK11Token.ASK_EXPIRE_TIME:
+    value = 2;
+    break;
+  }
+
+  EnableLifetimeTextbox(value);
+  return value;
+}
+
+function WriteAskForPassword(field) {
+  var askTimes;
+  switch (field.value) {
+  case "0":
     askTimes = nsIPK11Token.ASK_FIRST_TIME;
     break;
-  case "1": 
-    timeoutField.setAttribute("disabled", true);
+  case "1":
     askTimes = nsIPK11Token.ASK_EVERY_TIME;
     break;
   case "2":
-    timeoutField.removeAttribute("disabled");
-    if ( setFocus ) {
-      timeoutField.focus();
-    }
-    timeout = timeoutField.value;
-    var re = new RegExp("^[0-9]+$");
-    if (!re.test(timeout)) {
-      timeout = "1";
-    }
     askTimes = nsIPK11Token.ASK_EXPIRE_TIME;
     break;
   }
-  internal_token.setAskPasswordDefaults(askTimes, timeout);
+  var timeout = gInternalToken.getAskPasswordTimeout();
+  gInternalToken.setAskPasswordDefaults(askTimes, timeout);
 
-  var askEveryTimeHidden = document.getElementById("askEveryTimeHidden");
-  askEveryTimeHidden.checked = (radiogroup.value == 1) ? true : false;
+  var prefExpire = document.getElementById("signon.expireMasterPassword");
+  prefExpire.value = (field.value == 1);
+
+  return field.value;
+}
+
+function ReadPasswordLifetime() {
+  return gInternalToken.getAskPasswordTimeout();
+}
+
+function WritePasswordLifetime(field) {
+  var askTimes = gInternalToken.getAskPasswordTimes();
+  gInternalToken.setAskPasswordDefaults(askTimes, field.value);
+  return field.value;
+}
+
+function EnableLifetimeTextbox(aPrefValue) {
+  EnableElementById("passwordTimeout", aPrefValue == 2, false);
 }
 
 function ChangePW()
 {
-  var params = Components.classes[nsDialogParamBlock].createInstance(nsIDialogParamBlock);
-  params.SetString(1,"");
-  window.openDialog("chrome://pippki/content/changepassword.xul","",
-                    "chrome,centerscreen,modal",params);
+  var p = Components.classes["@mozilla.org/embedcomp/dialogparam;1"]
+                    .createInstance(Components.interfaces.nsIDialogParamBlock);
+  p.SetString(1, "");
+  window.openDialog("chrome://pippki/content/changepassword.xul", "",
+                    "chrome,centerscreen,modal", p);
 }
 
 function ResetPW()
 {
-  var params = Components.classes[nsDialogParamBlock].createInstance(nsIDialogParamBlock);
-  params.SetString(1,internal_token.tokenName);
+  var p = Components.classes["@mozilla.org/embedcomp/dialogparam;1"]
+                    .createInstance(Components.interfaces.nsIDialogParamBlock);
+  p.SetString(1, gInternalToken.tokenName);
   window.openDialog("chrome://pippki/content/resetpassword.xul", "",
-                    "chrome,centerscreen,modal", params);
+                    "chrome,centerscreen,modal", p);
 }
