9f3d466ecc7ce57bcb0f895ab03a58be9a51414b
Caio Tiago Oliveira <asrail@gmail.com>
2008-09-11 14:06:51 +0200

    bug 403993 - Make python compare-locales script work with SeaMonkey, r=l10n@mozilla.com a=kairo

---
+++ suite/locales/filter.py
@@ -0,0 +1,24 @@
+def test(mod, path, entity = None):
+  import re
+  # ignore anyhting but SeaMonkey
+  if mod not in ("netwerk", "dom", "toolkit", "security/manager",
+                 "extensions/reporter", "editor/ui",
+                 "suite"):
+    return False
+  # ignore temporary files, hiden files and files from rejects
+  if (re.match(r".*?\/[.#].+", path) or
+      re.match(r".*~$", path) or
+      re.match(r".+\.(orig|rej)", path)):
+    return False
+  if mod not in ("suite"):
+    # we only have exceptions for suite
+    return True
+  if entity is None:
+    # missing and obsolete files
+    return not (re.match(r"searchplugins\/.+\.src", path) or
+                re.match(r"searchplugins\/.+\.png", path) or
+                re.match(r"chrome\/common\/help\/images\/[A-Za-z-_]+\.[a-z]+", path))
+  if path == "defines.inc":
+    return entity != "MOZ_LANGPACK_CONTRIBUTORS"
+  
+  return True
