fa68466530246597c7468118bb8a0eef2ede96d4
Mark Banner <bugzilla@standard8.plus.com>
2008-09-22 21:26:42 +0100

    Part of bug 451238 and bug 430762 - make Thunderbird start pages be hosted on mozillamessaging.com. r=dmose

--- mail/branding/nightly/thunderbird-branding.js
+++ mail/branding/nightly/thunderbird-branding.js
@@ -1,8 +1,8 @@
 // Default start page
-pref("mailnews.start_page.url","chrome://messenger/content/start.xhtml");
+pref("mailnews.start_page.url","http://live.mozillamessaging.com/%APP%/start?locale=%LOCALE%version=%VERSION%&os=%OS%&buildid=%APPBUILDID%");
 
 // first launch welcome page
-pref("mailnews.start_page.welcome_url","chrome://messenger/content/start.xhtml");
+pref("mailnews.start_page.welcome_url","http://live.mozillamessaging.com/%APP%/firstrun?locale=%LOCALE%version=%VERSION%&os=%OS%&buildid=%APPBUILDID%");
 
 // start page override to load after an update
-pref("mailnews.start_page.override_url","chrome://messenger/content/start.xhtml");
+pref("mailnews.start_page.override_url","http://live.mozillamessaging.com/%APP%/whatsnew?locale=%LOCALE%version=%VERSION%&os=%OS%&buildid=%APPBUILDID%");
