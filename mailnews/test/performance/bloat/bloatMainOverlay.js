adffa651331c8242b8eed61ad12a238bdccf6bb5
Mark Banner <bugzilla@standard8.plus.com>
2008-10-03 11:10:57 +0100

    Bug 427980 Re-implement the MailBloatTest on tinderbox - bloat test files. r=Neil. NPOTB

---
+++ mailnews/test/performance/bloat/bloatMainOverlay.js
@@ -0,0 +1,82 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla MailNews test code.
+ *   Mark Banner <bugzilla@standard8.plus.com>
+ *
+ * The Initial Developer of the Original Code is
+ * Mozilla Messaging.
+ * Portions created by the Initial Developer are Copyright (C) 2008
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Mark Banner <bugzilla@standard8.plus.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+// Milliseconds. Decided on MacBook 2.4GHz Intel (Dual Core).
+// 4s seemed a resonable time for the main window to be displayed.
+const kMailStartup = 4000;
+// The other two windows (compose + addrbook) are closed after 3 seconds,
+// well close after 8 to allow a little extra time - if they get delayed, then
+// well get a prompt for closing the compose window that we don't want.
+// See bug 321783.
+const kMailClose = 8000;
+
+const kMailWaitTotal = kMailStartup + kMailClose;
+
+var gCurrentTimeout;
+
+function startMainTest()
+{
+  removeEventListener("load", startMainTest, false);
+
+  gCurrentTimeout = setTimeout(shutdownMainWindow, kMailWaitTotal);
+
+  // First thing to do is to start the address book and compose windows
+  toOpenWindowByType("mail:addressbook",
+                     "chrome://messenger/content/addressbook/addressbook.xul");
+
+  startComposeWindow();
+}
+
+function shutdownMainWindow()
+{
+  goQuitApplication();
+}
+
+function startComposeWindow()
+{
+  var msgComposeService =
+    Components.classes["@mozilla.org/messengercompose;1"]
+              .getService(Components.interfaces.nsIMsgComposeService);
+
+  // Compose a new message, format HTML, default identity
+  msgComposeService.OpenComposeWindow(null, null, null, 0, 1, null, null);
+}
+
+// Add the startMainTest call to the load event for the window.
+addEventListener("load", startMainTest, false);
---
+++ mailnews/test/performance/common/mailnewsTestPrefs.js
@@ -0,0 +1,35 @@
+// This set of prefs is copyied over the defaults that build-seamonkey-util.pl
+// sets up.
+user_pref("browser.dom.window.dump.enabled", true);
+user_pref("extensions.palmsync.conduitRegistered", true);
+user_pref("mail.account.account1.server", "server1");
+user_pref("mail.account.account2.identities", "id1");
+user_pref("mail.account.account2.server", "server2");
+user_pref("mail.accountmanager.accounts", "account1,account2");
+user_pref("mail.accountmanager.defaultaccount", "account2");
+user_pref("mail.accountmanager.localfoldersserver", "server1");
+user_pref("mail.identity.id1.fullName", "Tinderbox");
+user_pref("mail.identity.id1.smtpServer", "smtp1");
+user_pref("mail.identity.id1.useremail", "tinderbox@invalid.com");
+user_pref("mail.identity.id1.valid", true);
+user_pref("mail.root.none-rel", "[ProfD]Mail");
+user_pref("mail.root.pop3-rel", "[ProfD]Mail");
+user_pref("mail.server.server1.directory-rel", "[ProfD]Mail/Local Folders");
+user_pref("mail.server.server1.hostname", "Local Folders");
+user_pref("mail.server.server1.name", "Local Folders");
+user_pref("mail.server.server1.type", "none");
+user_pref("mail.server.server1.userName", "nobody");
+user_pref("mail.server.server2.check_new_mail", false);
+user_pref("mail.server.server2.directory-rel", "[ProfD]Mail/tinderbox");
+user_pref("mail.server.server2.download_on_biff", true);
+user_pref("mail.server.server2.hostname", "tinderbox");
+user_pref("mail.server.server2.login_at_startup", false);
+user_pref("mail.server.server2.name", "tinderbox@invalid.com");
+user_pref("mail.server.server2.type", "pop3");
+user_pref("mail.server.server2.userName", "tinderbox");
+user_pref("mail.smtp.defaultserver", "smtp1");
+user_pref("mail.smtpserver.smtp1.hostname", "tinderbox");
+user_pref("mail.smtpserver.smtp1.username", "tinderbox");
+user_pref("mail.smtpservers", "smtp1");
+user_pref("mail.startup.enabledMailCheckOnce", true);
+user_pref("mailnews.start_page_override.mstone", "1.9pre");
