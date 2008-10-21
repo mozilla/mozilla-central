a8cd71e65180e8b4b8000bedb39bb569bb00f24c
Berend Cornelius [:berend] <berend.cornelius@sun.com>
2008-10-21 16:25:42 +0200

    Bug 348481 -  Unifinder: focus is lost when moving between events via up & down key;patch=Przemyslaw Bialik;r=berend

--- calendar/base/content/calendar-unifinder.js
+++ calendar/base/content/calendar-unifinder.js
@@ -375,6 +375,7 @@ function unifinderSelect(event) {
     currentView().setSelectedItems(selectedItems.length, selectedItems, true);
     currentView().centerSelectedItems();
     calendarController.onSelectionChanged({detail: selectedItems});
+    document.getElementById("unifinder-search-results-tree").focus();
 }
 
 function unifinderKeyPress(aEvent) {
