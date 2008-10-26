927289d76a450c520138bd9d4428d0100550cae3
Daniel Boelzle [:dbo] <daniel.boelzle@sun.com>
2008-10-26 16:18:17 +0100

    Bug 394902 - allow empty properties; r=philipp

--- calendar/libical/src/libical/icalparser.c
+++ calendar/libical/src/libical/icalparser.c
@@ -1118,28 +1118,15 @@ icalcomponent* icalparser_add_line(icalparser* parser,
 		}
 			
 	    if (vcount == 0){
-		char temp[200]; /* HACK */
-		
-		icalproperty_kind prop_kind = icalproperty_isa(prop);
 		icalcomponent *tail = pvl_data(pvl_tail(parser->components));
 		
-		snprintf(temp,sizeof(temp),"No value for %s property. Removing entire property",
-			icalproperty_kind_to_string(prop_kind));
-
-		insert_error(tail, str, temp,
-			     ICAL_XLICERRORTYPE_VALUEPARSEERROR);
-
-		/* Remove the troublesome property */
+		/* Remove the empty property */
 		icalcomponent_remove_property(tail,prop);
 		icalproperty_free(prop);
-		prop = 0;
-		tail = 0;
-		parser->state = ICALPARSER_ERROR;
-		return 0;
-	    } else {
-
-		break;
+		prop = NULL;
 	    }
+
+        break;
 	}
     }
 	
