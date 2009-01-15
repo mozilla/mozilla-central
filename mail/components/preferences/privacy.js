47c09343859d009f44bead9e7a74cc8c635e1951
Mark Banner <bugzilla@standard8.plus.com>
2009-01-15 17:55:32 +0000

    Bug 239131 Thunderbird should use the new password manager (UI changes). r=philor,ui-review=clarkbw

--- mail/components/preferences/privacy.js
+++ mail/components/preferences/privacy.js
@@ -47,8 +47,7 @@ var gPrivacyPane = {
     this.updateManualMarkMode(document.getElementById('manualMark').checked);
     this.updateJunkLogButton(document.getElementById('enableJunkLogging').checked);
 
-    // Update the MP buttons
-    this.updateMasterPasswordButton();
+    this._initMasterPasswordUI();
 
     // update the checkbox for downloading phishing url tables
     // this.updateDownloadedPhishingListState();
@@ -103,64 +102,112 @@ var gPrivacyPane = {
       junkmailPlugin.resetTrainingData();
   },
 
-  initReencryptCallback: function()
+
+  /**
+   * Initializes master password UI: the "use master password" checkbox, selects
+   * the master password button to show, and enables/disables it as necessary.
+   * The master password is controlled by various bits of NSS functionality,
+   * so the UI for it can't be controlled by the normal preference bindings.
+   */
+  _initMasterPasswordUI: function ()
   {
-    var wallet = Components.classes['@mozilla.org/wallet/wallet-service;1'];
-    wallet = wallet.getService().QueryInterface(Components.interfaces.nsIWalletService);
-    wallet.WALLET_InitReencryptCallback(window);
+    var noMP = !this._masterPasswordSet();
+
+    document.getElementById("changeMasterPassword").disabled = noMP;
+
+    document.getElementById("useMasterPassword").checked = !noMP;
   },
 
-  viewPasswords: function()
-  {
-    document.documentElement.openWindow("Toolkit:PasswordManager", "chrome://messenger/content/preferences/viewpasswords.xul",
-                                        "", null);
-  },
 
-  changeMasterPassword: function ()
+  /**
+   * Returns true if the user has a master password set and false otherwise.
+   */
+  _masterPasswordSet: function ()
   {
-    document.documentElement.openSubDialog("chrome://mozapps/content/preferences/changemp.xul",
-                                           "", null);
-    this.updateMasterPasswordButton();
-  },
-
-  updateMasterPasswordButton: function ()
-  {
-    // See if there's a master password and set the button label accordingly
-    var secmodDB = Components.classes["@mozilla.org/security/pkcs11moduledb;1"]
-                             .getService(Components.interfaces.nsIPKCS11ModuleDB);
+    const Cc = Components.classes, Ci = Components.interfaces;
+    var secmodDB = Cc["@mozilla.org/security/pkcs11moduledb;1"].
+                   getService(Ci.nsIPKCS11ModuleDB);
     var slot = secmodDB.findSlotByName("");
     if (slot) {
-      const nsIPKCS11Slot = Components.interfaces.nsIPKCS11Slot;
       var status = slot.status;
-      var noMP = status == nsIPKCS11Slot.SLOT_UNINITIALIZED ||
-                 status == nsIPKCS11Slot.SLOT_READY;
-
-      var bundle = document.getElementById("bundlePreferences");
-      document.getElementById("setMasterPassword").label =
-        bundle.getString(noMP ? "setMasterPassword" : "changeMasterPassword");
-
-      document.getElementById("removeMasterPassword").disabled = noMP;
+      var hasMP = status != Ci.nsIPKCS11Slot.SLOT_UNINITIALIZED &&
+                  status != Ci.nsIPKCS11Slot.SLOT_READY;
+      return hasMP;
+    } else {
+      // XXX I have no bloody idea what this means
+      return false;
     }
   },
 
-  removeMasterPassword: function ()
+
+  /**
+   * Enables/disables the master password button depending on the state of the
+   * "use master password" checkbox, and prompts for master password removal
+   * if one is set.
+   */
+  updateMasterPasswordButton: function ()
   {
-    var secmodDB = Components.classes["@mozilla.org/security/pkcs11moduledb;1"]
-                              .getService(Components.interfaces.nsIPKCS11ModuleDB);
-    if (secmodDB.isFIPSEnabled)
-    {
+    var checkbox = document.getElementById("useMasterPassword");
+    var button = document.getElementById("changeMasterPassword");
+    button.disabled = !checkbox.checked;
+
+    // unchecking the checkbox should try to immediately remove the master
+    // password, because it's impossible to non-destructively remove the master
+    // password used to encrypt all the passwords without providing it (by
+    // design), and it would be extremely odd to pop up that dialog when the
+    // user closes the prefwindow and saves his settings
+    if (!checkbox.checked)
+      this._removeMasterPassword();
+    else
+      this.changeMasterPassword();
+
+    this._initMasterPasswordUI();
+  },
+
+  /**
+   * Displays the "remove master password" dialog to allow the user to remove
+   * the current master password.  When the dialog is dismissed, master password
+   * UI is automatically updated.
+   */
+  _removeMasterPassword: function ()
+  {
+    const Cc = Components.classes, Ci = Components.interfaces;
+    var secmodDB = Cc["@mozilla.org/security/pkcs11moduledb;1"].
+                   getService(Ci.nsIPKCS11ModuleDB);
+    if (secmodDB.isFIPSEnabled) {
+      var promptService = Cc["@mozilla.org/embedcomp/prompt-service;1"].
+                          getService(Ci.nsIPromptService);
       var bundle = document.getElementById("bundlePreferences");
       promptService.alert(window,
                           bundle.getString("pw_change_failed_title"),
                           bundle.getString("pw_change2empty_in_fips_mode"));
     }
-    else
-    {
+    else {
       document.documentElement.openSubDialog("chrome://mozapps/content/preferences/removemp.xul",
                                              "", null);
-      this.updateMasterPasswordButton();
-      document.getElementById("setMasterPassword").focus();
     }
+    this._initMasterPasswordUI();
+  },
+
+  /**
+   * Displays a dialog in which the master password may be changed.
+   */
+  changeMasterPassword: function ()
+  {
+    document.documentElement.openSubDialog("chrome://mozapps/content/preferences/changemp.xul",
+                                           "", null);
+    this._initMasterPasswordUI();
+  },
+
+  /**
+   * Shows the sites where the user has saved passwords and the associated
+   * login information.
+   */
+  showPasswords: function ()
+  {
+    document.documentElement.openWindow("Toolkit:PasswordManager",
+                                        "chrome://passwordmgr/content/passwordManager.xul",
+                                        "", null);
   },
 
   updateDownloadedPhishingListState: function()
