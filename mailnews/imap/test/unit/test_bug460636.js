6672ffc86e558befca09a7c117da2ffd12179bcc
Kaspar Brand <mozbugzilla@velox.ch>
2009-02-06 12:11:40 +0000

    Bug 460636 - "nsMsgSaveAsListener sometimes inserts extra LF characters" [r=Standard8]

---
+++ mailnews/imap/test/unit/test_bug460636.js
@@ -0,0 +1,88 @@
+/*
+ * Test bug 460636 - nsMsgSaveAsListener sometimes inserts extra LF characters
+ */
+
+var gIMAPDaemon, gServer, gIMAPIncomingServer, gSavedMsgFile;
+
+const gIMAPService = Cc["@mozilla.org/messenger/messageservice;1?type=imap"]
+                       .getService(Ci.nsIMsgMessageService);
+
+const gFileName = "bug460636";
+const gMsgFile = do_get_file("../mailnews/test/data/" + gFileName);
+                     
+function run_test()
+{
+  /*
+   * Set up an IMAP server. The bug is only triggered when nsMsgSaveAsListener
+   * is used (i.e., for IMAP and NNTP).
+   */
+  gIMAPDaemon = new imapDaemon();
+  gServer = makeServer(gIMAPDaemon, "");
+  gIMAPIncomingServer = createLocalIMAPServer();
+
+  // pref tuning: one connection only, turn off notifications
+  var prefBranch = Cc["@mozilla.org/preferences-service;1"]
+                     .getService(Ci.nsIPrefBranch);
+  prefBranch.setIntPref("mail.server.server1.max_cached_connections", 1);
+  prefBranch.setBoolPref("mail.biff.play_sound", false);
+  prefBranch.setBoolPref("mail.biff.show_alert", false);
+  prefBranch.setBoolPref("mail.biff.show_tray_icon", false);
+  prefBranch.setBoolPref("mail.biff.animate_dock_icon", false);
+
+  var inbox = gIMAPDaemon.getMailbox("INBOX");
+
+  /*
+   * Ok, prelude done. Read the original message from disk
+   * (through a file URI), and add it to the Inbox.
+   */
+  var msgfileuri = Cc["@mozilla.org/network/io-service;1"]
+                     .getService(Ci.nsIIOService)
+                     .newFileURI(gMsgFile).QueryInterface(Ci.nsIFileURL);
+
+  inbox.addMessage(new imapMessage(msgfileuri.spec, inbox.uidnext++, []));
+
+  /*
+   * Save the message to a local file. IMapMD corresponds to
+   * mozilla/_test/mailtest/ImapMail in the build directory
+   * (where fakeserver puts the IMAP mailbox files). If we pass
+   * the test, we'll remove the file afterwards (cf. UrlListener),
+   * otherwise it's kept in IMapMD.
+   */
+  gSavedMsgFile = Cc["@mozilla.org/file/directory_service;1"]
+                  .getService(Ci.nsIProperties)
+                  .get("IMapMD", Ci.nsILocalFile);
+  gSavedMsgFile.append(gFileName + ".eml");
+
+  do_test_pending();
+  do_timeout(10000, "do_throw('SaveMessageToDisk did not complete within 10 seconds (incorrect messageURI?). ABORTING.');");
+
+  /*
+   * From nsIMsgMessageService.idl:
+   * void SaveMessageToDisk(in string aMessageURI, in nsIFile aFile,
+   *                        in boolean aGenerateDummyEnvelope,
+   *                        in nsIUrlListener aUrlListener, out nsIURI aURL,
+   *                        in boolean canonicalLineEnding,
+   *                        in nsIMsgWindow aMsgWindow);
+   * Enforcing canonicalLineEnding (i.e., CRLF) makes sure that the
+   * test also runs successfully on platforms not using CRLF by default.
+   */
+  gIMAPService.SaveMessageToDisk("imap-message://user@localhost/INBOX#"
+                                 + (inbox.uidnext-1), gSavedMsgFile,
+                                 false, UrlListener, {}, true, null);
+}
+
+var UrlListener = 
+{
+  OnStartRunningUrl: function(url) { },
+  OnStopRunningUrl: function(url, rc)
+  {
+    gIMAPIncomingServer.closeCachedConnections();
+    gServer.stop();
+    do_check_eq(loadFileToString(gMsgFile), loadFileToString(gSavedMsgFile));
+    gSavedMsgFile.remove(false);
+    var thread = gThreadManager.currentThread;
+    while (thread.hasPendingEvents())
+      thread.processNextEvent(true);
+    do_test_finished();
+  }
+};
