3ea987afda4258d34bb09b4c4159a0d92cef56fa
Philippe M. Chiasson <gozer@mozillamessaging.com>
2009-02-18 15:37:32 -0500

    adapt default tags of client.py to pull THUNDERBIRD_3_0b2_RELEASE, tracked by bug 477103
    --HG--
    branch : COMM_20090224_RELBRANCH

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-EXTENSION_CO_TAG = 'HEAD'
+EXTENSION_CO_TAG = 'THUNDERBIRD_3_0b2_RELEASE'
 LDAPCSDK_CO_TAG = 'LDAPCSDK_6_0_6_RTM'
 
 CHATZILLA_CO_TAG = 'HEAD'
@@ -11,10 +11,10 @@ LDAPCSDK_DIRS = ('directory/c-sdk',)
 CHATZILLA_DIRS = ('extensions/irc',)
 VENKMAN_DIRS = ('extensions/venkman',)
 
-DEFAULT_COMM_REV = "default"
+DEFAULT_COMM_REV = "THUNDERBIRD_3_0b2_RELEASE"
 # URL of the default hg repository to clone for Mozilla.
 DEFAULT_MOZILLA_REPO = 'http://hg.mozilla.org/releases/mozilla-1.9.1/'
-DEFAULT_MOZILLA_REV = "default"
+DEFAULT_MOZILLA_REV = "THUNDERBIRD_3_0b2_RELEASE"
 
 # REGEX to match against, $1 should refer to protocol scheme
 MOZILLA_TRUNK_REPO_REGEXP = "(ssh|http|https):\/\/hg\.mozilla\.org\/mozilla-central\/?$"
@@ -22,7 +22,7 @@ MOZILLA_BASE_REV = "GECKO_1_9_1_BASE"
 
 # URL of the default hg repository to clone for inspector.
 DEFAULT_INSPECTOR_REPO = 'http://hg.mozilla.org/dom-inspector/'
-DEFAULT_INSPECTOR_REV = "default"
+DEFAULT_INSPECTOR_REV = "THUNDERBIRD_3_0b2_RELEASE"
 
 import os
 import sys
