e052c538e69aefaf4eedecc1e2ecaf8cc6f4f44a
Andrew Sutherland <asutherland@asutherland.org>
2009-05-03 19:36:18 -0700

    Backed out changeset b67f484152e4, bug 474701.

--- mailnews/test/resources/asyncTestUtils.js
+++ mailnews/test/resources/asyncTestUtils.js
@@ -30,17 +30,11 @@ var asyncCopyListener = {
 };
 
 /**
- * Delete one or more synthetic messages.  If using SyntheticMessageSets, call
- *  once for each SyntheticMessageSet.  Otherwise, we take synthetic message
- *  instances that are assumed to live in gTestFolder.  The arguments can be one
+ * Delete one or synthetic messages from gTestFolder.  The arguments can be one
  *  or more synthetic message instances or a singe list of synthetic messages.
- *  (Synthetic messages are from messageGenerator.js, SyntheticMessageSets are
- *  from messageModifier.js)
+ *  (Synthetic messages are from messageGenerator.js)
  *
- * Usage example using a SyntheticMessageSet (which defines the folder):
- *  yield async_delete_messages(syntheticMessageSet);
- *  yield async_delete_messages(new SyntheticMessageSet(folder, [synMsg]);
- * Usage example assuming gTestFolder:
+ * Usage example:
  *  yield async_delete_messages(synMsg);
  *  yield async_delete_messages(synMsg1, synMsg2);
  *  yield async_delete_messages([synMsg1, synMsg2]);
@@ -54,35 +48,16 @@ function async_delete_messages() {
   else
     synMessages = [arguments[0]];
 
-  if (synMessages.length == 0)
-    do_throw("You need to tell us to delete at least one thing!");
+  let msgDatabase = gTestFolder.msgDatabase;
+  var array = Cc["@mozilla.org/array;1"].createInstance(Ci.nsIMutableArray);
+  synMessages.forEach(function (synMsg) {
+    let hdr = msgDatabase.getMsgHdrForMessageID(synMsg.messageId);
+    array.appendElement(hdr, false);
+  });
 
-  // SyntheticMessageSet case
-  if (synMessages[0].synMessages) {
-    let messageSet = synMessages[0];
-    // because synthetic message sets
-    return async_run({func: function () {
-        for (let [folder, xpcomHdrArray] in
-             messageSet.foldersWithXpcomHdrArrays) {
-          folder.deleteMessages(xpcomHdrArray, null, false, true,
-                                asyncCopyListener, true);
-          yield false;
-        }
-      },
-    });
-  }
-  // a list of synthetic messages case
-  else {
-    let msgDatabase = gTestFolder.msgDatabase;
-    let hdrArr = Cc["@mozilla.org/array;1"].createInstance(Ci.nsIMutableArray);
-    synMessages.forEach(function (synMsg) {
-      hdrArr.appendElement(msgDatabase.getMsgHdrForMessageID(synMsg.messageId),
-                           false);
-    });
-    gTestFolder.deleteMessages(hdrArr, null, false, true, asyncCopyListener,
-                               true);
-    return false;
-  }
+  gTestFolder.deleteMessages(array, null, false, true, asyncCopyListener, true);
+
+  return false;
 }
 
 
@@ -111,7 +86,7 @@ var asyncGeneratorStack = [];
 function async_run(aArgs) {
   let result = aArgs.func.apply(aArgs.dis || null, aArgs.args || []);
   if (result && result.next) {
-    asyncGeneratorStack.push([result, aArgs.func.name]);
+    asyncGeneratorStack.push(result);
     return _async_driver();
   }
   else {
@@ -127,11 +102,10 @@ function async_run(aArgs) {
  *  async_run that returns/yields false at any point is responsible for ensuring
  *  async_driver() is called again once the async operation completes.
  *
- * Note: This function actually schedules the real driver to run after a
- *  timeout. This is to ensure that if you call us from a notification event
- *  that all the other things getting notified get a chance to do their work
- *  before we actually continue execution.  It also keeps our stack traces
- *  cleaner.
+ * Note: This function actually schedules the real driver to run after a timeout.
+ *  This is to ensure that if you call us from a notification event that all the
+ *  other things getting notified get a chance to do their work before we actually
+ *  continue execution.  It also keeps our stack traces cleaner.
  */
 function async_driver() {
   do_timeout_function(0, _async_driver);
@@ -140,8 +114,7 @@ function async_driver() {
 // the real driver!
 function _async_driver() {
   let curGenerator;
-  while (asyncGeneratorStack.length) {
-    curGenerator = asyncGeneratorStack[asyncGeneratorStack.length-1][0];
+  while ((curGenerator = asyncGeneratorStack[asyncGeneratorStack.length-1])) {
     try {
       while (curGenerator.next()) {
       }
@@ -149,117 +122,10 @@ function _async_driver() {
     }
     catch (ex) {
       if (ex != StopIteration) {
-        dump("*******************************************\n");
-        dump("Generator explosion!\n");
-        dump("Unhappiness at: " + ex.fileName + ":" + ex.lineNumber + "\n");
-        dump("Because: " + ex + "\n");
-        dump("**** Async Generator Stack source functions:\n");
-        for (let i = asyncGeneratorStack.length - 1; i >= 0; i--) {
-          dump("  " + asyncGeneratorStack[i][1] + "\n");
-        }
         do_throw(ex);
       }
       asyncGeneratorStack.pop();
     }
   }
   return true;
-}
-
-var ASYNC_TEST_RUNNER_HELPERS = [];
-/**
- * If you are test helper code using asyncTestUtils, then you might want to
- *  provide a helper object.  We will let you do things after each test
- *  completes, and if a test times out, etc.
- *
- * @param aHelper A helper object which may have the following functions:
- * - postTest: This gets called after each test completes.
- * - onTimeout: This gets called if a test times out.
- */
-function async_test_runner_register_helper(aHelper) {
-  ASYNC_TEST_RUNNER_HELPERS.push(aHelper);
-}
-
-function _async_test_runner_postTest() {
-  for each (let [, helper] in Iterator(ASYNC_TEST_RUNNER_HELPERS)) {
-    if (helper.postTest)
-      helper.postTest();
-  }
-}
-
-function _async_test_runner_timeout() {
-  for each (let [, helper] in Iterator(ASYNC_TEST_RUNNER_HELPERS)) {
-    try {
-      if (helper.onTimeout)
-        helper.onTimeout();
-    }
-    catch (ex) {
-      dump("warning: helper failure: (" + ex.fileName + ":" + ex.lineNumber +
-           "): " + ex + "\n");
-    }
-  }
-  do_throw('Timeout running test, and we want you to have the log.');
-}
-
-/**
- * Purely decorative function to help explain to people reading lists of tests
- *  using async_run_tests what is going on.  We just return a tuple of our
- *  arguments and _async_test_runner understands what to do with this, namely
- *  to run the test once for each element in the aParameters list.  If the
- *  elements in the aParameters list have a 'name' attribute, it will get
- *  printed out to help figure out what is actually happening.
- */
-function parameterizeTest(aTestFunc, aParameters) {
-  return [aTestFunc, aParameters];
-}
-
-DEFAULT_LONGEST_TEST_RUN_CONCEIVABLE_SECS = 60;
-function async_run_tests(aTests, aLongestTestRunTimeConceivableInSecs) {
-  if (aLongestTestRunTimeConceivableInSecs == null)
-    aLongestTestRunTimeConceivableInSecs =
-        DEFAULT_LONGEST_TEST_RUN_CONCEIVABLE_SECS;
-  do_timeout(aLongestTestRunTimeConceivableInSecs * 1000,
-      "_async_test_runner_timeout();");
-
-  do_test_pending();
-
-  async_run({func: _async_test_runner, args: [aTests]});
-}
-
-function _async_test_runner(aTests) {
-  for each (let [, test] in Iterator(aTests)) {
-    // parameterized?
-    if (test.length) {
-      let [testFunc, parameters] = test;
-      for each (let [, parameter] in Iterator(parameters)) {
-        let paramDesc, args;
-        if (typeof(parameter) == "object") {
-          if (parameter.length) {
-            paramDesc = parameter.toString();
-            args = parameter;
-          }
-          else {
-            paramDesc = parameter.name;
-            args = [parameter];
-          }
-        }
-        else {
-          paramDesc = parameter.toString();
-          args = [parameter];
-        }
-        dump("=== Running test: " + testFunc.name + " Parameter: " +
-             paramDesc + "\n");
-        yield async_run({func: testFunc, args: args});
-        _async_test_runner_postTest();
-      }
-
-    }
-    else {
-      dump("=== Running test: " + test.name + "\n");
-      yield async_run({func: test});
-      _async_test_runner_postTest();
-    }
-  }
-
-  dump("=== (Done With Tests)\n");
-  do_test_finished();
 }
