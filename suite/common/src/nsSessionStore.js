55e23c0a42d4d8841ceb420823245a0debdcb7e8
Jens Hatlak <jh@junetz.de>
2009-08-19 01:25:15 +0200

    Bug 510652 - Port Bug 477564 - Session restore hangs/not responding with high CPU on large form with many checkboxes; r+sr=neil

--- suite/common/src/nsSessionStore.js
+++ suite/common/src/nsSessionStore.js
@@ -1291,8 +1291,15 @@ SessionStoreService.prototype = {
     if (!node)
       return null;
 
+    const MAX_GENERATED_XPATHS = 100;
+    let generatedCount = 0;
+
     let data = {};
     do {
+      // Only generate a limited number of XPath expressions for perf reasons (cf. bug 477564)
+      if (!node.id && ++generatedCount > MAX_GENERATED_XPATHS)
+        continue;
+
       let id = node.id ? "#" + node.id : XPathHelper.generate(node);
       if (node instanceof Components.interfaces.nsIDOMHTMLInputElement) {
         if (node.type != "file")
