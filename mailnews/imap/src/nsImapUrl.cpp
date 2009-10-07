ce9d26c1ad56f3775476ae86bc13f38bfc189157
David Bienvenu <bienvenu@nventure.com>
2009-10-07 07:18:22 -0700

    fix build bustage

--- mailnews/imap/src/nsImapUrl.cpp
+++ mailnews/imap/src/nsImapUrl.cpp
@@ -1251,7 +1251,7 @@ NS_IMETHODIMP nsImapUrl::GetUri(char** aURI)
 NS_IMPL_GETSET(nsImapUrl, AddDummyEnvelope, PRBool, m_addDummyEnvelope)
 NS_IMPL_GETSET(nsImapUrl, CanonicalLineEnding, PRBool, m_canonicalLineEnding)
 NS_IMPL_GETTER(nsImapUrl::GetMsgLoadingFromCache, PRBool, m_msgLoadingFromCache)
-NS_IMPL_GETSET(nsImapUrl, LocalFetchOnly, PRBool, m_localFetchOnly);
+NS_IMPL_GETSET(nsImapUrl, LocalFetchOnly, PRBool, m_localFetchOnly)
 NS_IMPL_GETSET(nsImapUrl, ExternalLinkUrl, PRBool, m_externalLinkUrl)
 NS_IMPL_GETSET(nsImapUrl, RerunningUrl, PRBool, m_rerunningUrl)
 NS_IMPL_GETSET(nsImapUrl, ValidUrl, PRBool, m_validUrl)
