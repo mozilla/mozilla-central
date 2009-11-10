bc195ea9af5c895c65fd867f7c9f6fc2add5b392
Mark Banner <bugzilla@standard8.plus.com>
2009-11-10 20:01:01 +0000

    Bug 526817 - Port bug 526668 - add option to 'unify' to allow files to match if their sorted contents match. r=gozer

--- build/macosx/universal/flight.mk
+++ build/macosx/universal/flight.mk
@@ -103,6 +103,8 @@ endif
 endif
 endif
 	$(TOPSRCDIR)/mozilla/build/macosx/universal/unify \
+	  --unify-with-sort "\.manifest$$" \
+	  --unify-with-sort "components\.list$$" \
 	  $(DIST_PPC)/$(MOZ_PKG_APPNAME)/$(APPNAME) \
 	  $(DIST_X86)/$(MOZ_PKG_APPNAME)/$(APPNAME) \
 	  $(DIST_UNI)/$(MOZ_PKG_APPNAME)/$(APPNAME)
