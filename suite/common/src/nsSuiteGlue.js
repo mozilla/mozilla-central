7962ebf0fc3bb106775f3bf67a0d5ccbf1644644
Tamas Hubai <htamas2@gmail.com>
2009-12-22 19:51:52 +0000

    Bug 534462 loadStartFolder() asks for master password even if already logged into the software security device (SeaMonkey part). r=Neil

--- suite/common/src/nsSuiteGlue.js
+++ suite/common/src/nsSuiteGlue.js
@@ -194,16 +194,16 @@ SuiteGlue.prototype = {
     if (!prefBranch.getBoolPref("signon.startup.prompt"))
       return;
 
-    // Look to see if a master password is set, if so prompt for it to try
-    // and avoid the multiple master password prompts on startup scenario.
+    // Try to avoid the multiple master password prompts on startup scenario
+    // by prompting for the master password upfront.
     let token = Components.classes["@mozilla.org/security/pk11tokendb;1"]
                           .getService(Components.interfaces.nsIPK11TokenDB)
                           .getInternalKeyToken();
 
-    // If an empty string is valid for the internal token, then we don't
-    // have a master password, else, if it does, then try to login.
+    // Only log in to the internal token if it is already initialized,
+    // otherwise we get a "Change Master Password" dialog.
     try {
-      if (!token.checkPassword(""))
+      if (!token.needsUserInit)
         token.login(false);
     } catch (ex) {
       // If user cancels an exception is expected.
