7bd77e30cdfb70f611918849016ee3ec3104e711
Serge Gautherie <sgautherie.bz@free.fr>
2010-01-04 08:53:13 +0100

    Bug 523820 - Remove old MOZILLA_1_9_1_BRANCH ifdefs from comm-central code; (Fv2a-SM) Remove last missed case. r=Callek.

--- suite/confvars.sh
+++ suite/confvars.sh
@@ -45,13 +45,7 @@ fi
 MOZ_STATIC_MAIL_BUILD=1
 MOZ_COMPOSER=1
 MOZ_SUITE=1
-# Needed for the mozilla-central build side of the system.
-MOZILLA_BRANCH_VERSION=`echo ${MOZILLA_VERSION} | sed -e 's/^\([0-9]\.[0-9]\.[0-9]\).*/\1/;'`
-if test "$MOZILLA_BRANCH_VERSION" = "1.9.1"; then
-  MOZ_APP_VERSION=`cat $topsrcdir/$MOZ_BUILD_APP/config/version-191.txt`
-else
-  MOZ_APP_VERSION=`cat $topsrcdir/$MOZ_BUILD_APP/config/version.txt`
-fi
+MOZ_APP_VERSION=`cat $topsrcdir/$MOZ_BUILD_APP/config/version.txt`
 SEAMONKEY_VERSION=$MOZ_APP_VERSION
 MOZ_NO_XPCOM_OBSOLETE=1
 MOZ_EXTENSIONS_DEFAULT=" venkman inspector irc gnomevfs reporter"
