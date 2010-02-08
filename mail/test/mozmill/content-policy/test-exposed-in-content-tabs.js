e3d2e5b614d3b2c6bb1287eeb37d0949c4d25a2c
Mark Banner <bugzilla@standard8.plus.com>
2010-02-08 21:28:13 +0000

    Bug 491921 Part 4 - Content Policy test additions for checking messages aren't exposed to remote content. r=bienvenu

---
+++ mail/test/mozmill/content-policy/test-exposed-in-content-tabs.js
@@ -0,0 +1,176 @@
+/**
+ * The purpose of this test is to ensure that remote content can't gain access
+ * to messages by loading their URIs.
+ */
+
+//
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2001
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Dan Mosedale <dmose@mozilla.org>
+ *   Joey Minta <jminta@gmail.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+var MODULE_NAME = 'test-exposed-in-content-tabs';
+
+var RELATIVE_ROOT = '../shared-modules';
+var MODULE_REQUIRES = ['folder-display-helpers', 'compose-helpers'];
+var jumlib = {};
+Components.utils.import("resource://mozmill/modules/jum.js", jumlib);
+var elib = {};
+Components.utils.import('resource://mozmill/modules/elementslib.js', elib);
+
+var folder = null;
+var composeHelper = null;
+var gMsgNo = 0;
+
+// RELATIVE_ROOT messes with the collector, so we have to bring the path back
+// so we get the right path for the resources.
+var url = collector.addHttpResource('../content-policy/html', 'content');
+
+// These two constants are used to build the message body.
+const msgBody = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">\n' +
+'<html>\n' +
+'<head>\n' +
+'\n' +
+'<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">\n' +
+'</head>\n' +
+'<body bgcolor="#ffffff" text="#000000">\n' +
+'<img id="testelement" src="' + url + 'pass.png"/>\n' +
+'</body>\n</html>\n';
+
+var setupModule = function (module) {
+  let fdh = collector.getModule('folder-display-helpers');
+  fdh.installInto(module);
+  composeHelper = collector.getModule('compose-helpers');
+  composeHelper.installInto(module);
+
+  folder = create_folder("exposedInContent");
+};
+
+function addToFolder(aSubject, aBody, aFolder) {
+
+  let msgId = Components.classes["@mozilla.org/uuid-generator;1"]
+                          .getService(Components.interfaces.nsIUUIDGenerator)
+                          .generateUUID() +"@mozillamessaging.invalid";
+
+  let source = "From - Sat Nov  1 12:39:54 2008\n" +
+               "X-Mozilla-Status: 0001\n" +
+               "X-Mozilla-Status2: 00000000\n" +
+               "Message-ID: <" + msgId + ">\n" +
+               "Date: Wed, 11 Jun 2008 20:32:02 -0400\n" +
+               "From: Tester <tests@mozillamessaging.invalid>\n" +
+               "User-Agent: Thunderbird 3.0a2pre (Macintosh/2008052122)\n" +
+               "MIME-Version: 1.0\n" +
+               "To: recipient@mozillamessaging.invalid\n" +
+               "Subject: " + aSubject + "\n" +
+               "Content-Type: text/html; charset=ISO-8859-1\n" +
+               "Content-Transfer-Encoding: 7bit\n" +
+               "\n" + aBody + "\n";
+
+  aFolder.QueryInterface(Components.interfaces.nsIMsgLocalMailFolder);
+  aFolder.gettingNewMessages = true;
+
+  aFolder.addMessage(source);
+  aFolder.gettingNewMessages = false;
+
+  return aFolder.msgDatabase.getMsgHdrForMessageID(msgId);
+}
+
+function addMsgToFolder(folder) {
+  let msgDbHdr = addToFolder("exposed test message " + gMsgNo,
+                             msgBody, folder);
+
+  // select the newly created message
+  let msgHdr = select_click_row(gMsgNo);
+
+  if (msgDbHdr != msgHdr)
+    throw new Error("Selected Message Header is not the same as generated header");
+
+  assert_selected_and_displayed(gMsgNo);
+
+  ++gMsgNo;
+
+  // We also want to return the url of the message, so save that here.
+  let msgSimpleURL = msgHdr.folder.getUriForMsg(msgHdr);
+
+  let msgService = Cc["@mozilla.org/messenger;1"]
+                     .createInstance(Ci.nsIMessenger)
+                     .messageServiceFromURI(msgSimpleURL);
+
+  var neckoURL = {};
+  msgService.GetUrlForUri(msgSimpleURL, neckoURL, null);
+
+  // This is the full url to the message that we want (i.e. passing this to
+  // a browser element or iframe will display it).
+  return neckoURL.value.spec;
+}
+
+function checkContentTab(msgURL) {
+  // To open a tab we're going to have to cheat and use tabmail so we can load
+  // in the data of what we want.
+  let preCount = mc.tabmail.tabContainer.childNodes.length;
+
+  let dataurl = 'data:text/html,<html><head><title>test exposed</title>' +
+    '</head><body><iframe id="msgIframe" src="' + msgURL + '"/></body></html>';
+
+  let newTab = mc.tabmail.openTab("contentTab", { contentPage: dataurl });
+
+  mc.waitForEval("subject.busy == false", 5000, 100, newTab);
+
+  if (mc.tabmail.tabContainer.childNodes.length != preCount + 1)
+    throw new Error("The content tab didn't open");
+
+  if (mc.window.content.document.getElementById("msgIframe")
+        .contentDocument.URL != "about:blank")
+    throw new Error("Message display/access has not been blocked from remote content!");
+
+  mc.tabmail.closeTab(newTab);
+
+  if (mc.tabmail.tabContainer.childNodes.length != preCount)
+    throw new Error("The content tab didn't close");
+}
+
+function test_exposedInContentTabs() {
+  let folderTab = mc.tabmail.currentTabInfo;
+  be_in_folder(folder);
+
+  assert_nothing_selected();
+
+  // Check for denied in mail
+  let msgURL = addMsgToFolder(folder);
+
+  // Check allowed in content tab
+  checkContentTab(msgURL);
+}
