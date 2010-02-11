a18dacbfde338c3040d13380457080c06c4788d7
Serge Gautherie <sgautherie.bz@free.fr>
2010-02-11 02:15:39 +0100

    Bug 543294 - Port |Bug 473411 - the rule for DIST_FILES should create dist/bin| to c-c; (Av1) Just copy it. r=Callek.

--- config/rules.mk
+++ config/rules.mk
@@ -1844,7 +1844,10 @@ endif
 endif
 
 ifneq ($(DIST_FILES),)
-libs:: $(DIST_FILES)
+$(DIST)/bin:
+	$(NSINSTALL) -D $@
+
+libs:: $(DIST_FILES) $(DIST)/bin
 	@$(EXIT_ON_ERROR) \
 	for f in $(DIST_FILES); do \
 	  dest=$(FINAL_TARGET)/`basename $$f`; \
