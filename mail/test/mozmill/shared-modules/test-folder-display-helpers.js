3fc1035b0c1272f259058ca6b91885818c08feb9
Andrew Sutherland <asutherland@asutherland.org>
2010-02-23 21:05:49 -0800

    bug 548188 - comment change to trigger new mozmill run

--- mail/test/mozmill/shared-modules/test-folder-display-helpers.js
+++ mail/test/mozmill/shared-modules/test-folder-display-helpers.js
@@ -13,8 +13,7 @@
  *
  * The Original Code is Thunderbird Mail Client.
  *
- * The Initial Developer of the Original Code is
- * Mozilla Messaging, Inc.
+ * The Initial Developer of the Original Code is the Mozilla Foundation.
  * Portions created by the Initial Developer are Copyright (C) 2009
  * the Initial Developer. All Rights Reserved.
  *
