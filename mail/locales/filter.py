bba17f395103b24cea7bfb5305d9fb185e4271bb
Axel Hecht <l10n@mozilla.com>
2010-03-23 04:28:09 +0100

    bug 554046, update filter.py to ignore the lorentz toolkit strings on 1.9.2, bustage fix for tb31x

--- mail/locales/filter.py
+++ mail/locales/filter.py
@@ -5,6 +5,12 @@ def test(mod, path, entity = None):
                  "mail", "editor/ui", "extensions/spellcheck",
                  "other-licenses/branding/thunderbird"):
     return False
+
+  # Ignore Lorentz strings, at least temporarily
+  if mod == "toolkit" and path == "chrome/mozapps/plugins/plugins.dtd":
+    if entity.startswith('reloadPlugin.'): return False
+    if entity.startswith('report.'): return False
+
   # ignore MOZ_LANGPACK_CONTRIBUTORS
   if mod == "mail" and path == "defines.inc" and \
      entity == "MOZ_LANGPACK_CONTRIBUTORS":
