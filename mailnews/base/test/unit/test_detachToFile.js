22b343980aaa3d97ed37810e60f923f53e0baddc
Kent James <kent@caspia.com>
2010-04-07 07:41:10 -0700

    Bug 531757 - Extend nsIMessenger.idl to allow detachment to folders, r/sr=bienvenu

---
+++ mailnews/base/test/unit/test_detachToFile.js
@@ -0,0 +1,168 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * Kent James <kent@caspia.com>.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+/*
+ * Tests nsIMessenger's detachAttachmentsWOPrompts
+ */
+
+load("../../mailnews/resources/logHelper.js");
+load("../../mailnews/resources/mailTestUtils.js");
+load("../../mailnews/resources/asyncTestUtils.js");
+
+// javascript mime emitter functions
+mimeMsg = {};
+Components.utils.import("resource:///modules/gloda/mimemsg.js", mimeMsg);
+
+const copyService = Cc["@mozilla.org/messenger/messagecopyservice;1"]
+                      .getService(Ci.nsIMsgCopyService);
+
+var tests = [
+  startCopy,
+  startMime,
+  startDetach,
+  testDetach,
+]
+
+function startCopy()
+{
+  // Get a message into the local filestore.
+  var mailFile = do_get_file("../../mailnews/data/external-attach-test");
+  copyService.CopyFileMessage(mailFile, gLocalInboxFolder, null, false, 0,
+                              "", asyncCopyListener, null);
+  yield false;
+}
+
+// process the message through mime
+function startMime()
+{
+  let msgHdr = firstMsgHdr(gLocalInboxFolder);
+
+  mimeMsg.MsgHdrToMimeMessage(msgHdr, gCallbackObject, gCallbackObject.callback,
+                              true /* allowDownload */);
+  yield false;
+}
+
+// detach any found attachments
+function startDetach()
+{
+  let msgHdr = firstMsgHdr(gLocalInboxFolder);
+  let msgURI = msgHdr.folder.generateMessageURI(msgHdr.messageKey);
+
+  let messenger = Cc["@mozilla.org/messenger;1"].createInstance(Ci.nsIMessenger);
+  let attachment = gCallbackObject.attachments[0];
+
+  messenger.detachAttachmentsWOPrompts(gProfileDir, 1,
+                                       [attachment.contentType], [attachment.url],
+                                       [attachment.name], [msgURI], asyncUrlListener);
+  yield false;
+}
+
+// test that the detachment was successful
+function testDetach()
+{
+  // This test seems to fail on Linux without the following delay.
+  do_timeout(200, async_driver());
+  yield false;
+  // The message contained a file "check.pdf" which should
+  //  now exist in the profile directory.
+  let checkFile = gProfileDir.clone();
+  checkFile.append("check.pdf");
+  do_check_true(checkFile.exists());
+
+  // The message should now have a detached attachment. Read the message,
+  //  and search for "AttachmentDetached" which is added on detachment.
+
+  // Get the message header
+  let msgHdr = firstMsgHdr(gLocalInboxFolder);
+
+  let messageContent = getContentFromMessage(msgHdr);
+  do_check_true(messageContent.indexOf("AttachmentDetached") != -1);
+}
+
+function SaveAttachmentCallback() {
+  this.attachments = null;
+}
+
+SaveAttachmentCallback.prototype = {
+  callback: function saveAttachmentCallback_callback(aMsgHdr, aMimeMessage) {
+    this.attachments = aMimeMessage.allAttachments;
+    async_driver();
+  }
+}
+let gCallbackObject = new SaveAttachmentCallback();
+
+function run_test()
+{
+  if (!gLocalInboxFolder)
+    loadLocalMailAccount();
+  async_run_tests(tests);
+}
+
+/*
+ * Get the full message content.
+ *
+ * aMsgHdr: nsIMsgDBHdr object whose text body will be read
+ *          returns: string with full message contents
+ */
+function getContentFromMessage(aMsgHdr) {
+  const MAX_MESSAGE_LENGTH = 65536;
+  let msgFolder = aMsgHdr.folder;
+  let msgUri = msgFolder.getUriForMsg(aMsgHdr);
+
+  let messenger = Cc["@mozilla.org/messenger;1"]
+                    .createInstance(Ci.nsIMessenger);
+  let streamListener = Cc["@mozilla.org/network/sync-stream-listener;1"]
+                         .createInstance(Ci.nsISyncStreamListener);
+  messenger.messageServiceFromURI(msgUri).streamMessage(msgUri,
+                                                        streamListener,
+                                                        null,
+                                                        null,
+                                                        false,
+                                                        "",
+                                                        false);
+  let sis = Cc["@mozilla.org/scriptableinputstream;1"]
+              .createInstance(Ci.nsIScriptableInputStream);
+  sis.init(streamListener.inputStream);
+  return sis.read(MAX_MESSAGE_LENGTH);
+}
+
+// get the first message header found in a folder
+function firstMsgHdr(folder) {
+  let enumerator = folder.msgDatabase.EnumerateMessages();
+  if (enumerator.hasMoreElements())
+    return enumerator.getNext().QueryInterface(Ci.nsIMsgDBHdr);
+  return null;
+}
