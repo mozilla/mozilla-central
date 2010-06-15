976886156408b2d8ed5d36ea4403fedf4839b5cb
Mark Banner <bugzilla@standard8.plus.com>
2010-06-15 20:40:04 +0100

    Backout the code part of bug 538750 to see if it fixes the random crashes we're seeing in mozmill tests

--- mail/base/content/messageDisplay.js
+++ mail/base/content/messageDisplay.js
@@ -338,15 +338,10 @@ MessageDisplayWidget.prototype = {
 
     // figure out if we're looking at one thread or more than one thread
     let selectedMessages = this.folderDisplay.selectedMessages;
-    let selectedIndices = this.folderDisplay.selectedIndices;
-    let dbView = this.folderDisplay.view.dbView;
-    let firstThreadId = dbView.getThreadContainingIndex(selectedIndices[0])
-      .getChildHdrAt(0).messageKey;
+    let firstThreadId = selectedMessages[0].threadId;
     let oneThread = true;
-    for (let i = 0; i < selectedIndices.length; i++) {
-      let threadId = dbView.getThreadContainingIndex(selectedIndices[i])
-        .getChildHdrAt(0).messageKey;
-      if (threadId != firstThreadId) {
+    for (let i = 0; i < selectedMessages.length; i++) {
+      if (selectedMessages[i].threadId != firstThreadId) {
         oneThread = false;
         break;
       }
