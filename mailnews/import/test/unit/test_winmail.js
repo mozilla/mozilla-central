50b3389cd8141f8f3cd78d07c0044f238b3df7ab
Phil Lacy <philbaseless-firefox@yahoo.com>
2010-06-17 14:09:22 +0100

    Bug 561422 Test fails for Importing settings for windows live mail. r=Standard8

--- mailnews/import/test/unit/test_winmail.js
+++ mailnews/import/test/unit/test_winmail.js
@@ -1,11 +1,26 @@
 const IMPORTNAME = 'Windows Live Mail';
 const IMPORTTYPE = 'settings';
 
+// XXX These prefs are excluded from being checked because they contain
+// relative directories, and hence make the results dependent on where
+// the tests are run. The prefs do have relative versions which are checked;
+// Or because they have assigned numbers which are not absolute;
+// Or mail.accountmanager the data is not relevant
+// At the end of this file is detailed information and prefs
+//
+function excludedPrefs(name) {
+  return !/\-rel/i.test(name) &&
+     (/mail\.account/i.test(name) ||
+      /smtpserver$/i.test(name) ||
+      /newsrc_root$/i.test(name) ||
+      /mail\.root/i.test(name) ||
+      /deferred_to_account/i.test(name) ||
+      /mail\.server\.server\d+\.(directory|newsrc)/i.test(name) ||
+      /mail\.smtpservers/i.test(name));
+}
+
 function run_test()
 {
-  // XXX disabled for now as test is broken. Bug 561422 will fix it.
-  return;
-
   if (!("nsIWindowsRegKey" in Ci))
     return;
 
@@ -73,10 +88,12 @@ function run_test()
     },
     observe: function observe(aSubj, aTopic, aData) {
       let prefSetting = {};
+      if (excludedPrefs(aData))
+        return;
       prefSetting.name = aData;
       switch (prefS.getPrefType(aData)) {
         case prefS.PREF_BOOL:
-          prefSetting.data = prefS.getBoolPref(aData);
+          prefSetting.data = String(prefS.getBoolPref(aData));
           break;
         case prefS.PREF_INT:
           prefSetting.data = prefS.getIntPref(adata);
@@ -122,88 +139,50 @@ function run_test()
     function(a,b){
       return (a.name < b.name ? -1 : (a.name > b.name ? 1 : 0));
     });
-var test = "";
+
 // testing data--if new accounts are added, replace this with the dump 'test'.
 // change all '\' to '\\' and replace this data. This test data needs to go
 // to a file and probably be binary since we may have other invalid string
 // characters in future checks.
+var testdata = [
+"popdisplayname","popdisplayname","testpop@invalid.invalid",
+"testpop@invalid.invalid","test","test","mozillanews@invalid.invalid",
+"mozillanews@invalid.invalid","Don Hall","Wingtip Toys","don@wingtiptoys.com",
+"don@wingtiptoys.com","Don Hall","Wingtip Toys","don@wingtiptoys.com",
+"don@wingtiptoys.com","[ProfD]News","[ProfD]ImapMail","[ProfD]News","[ProfD]Mail",
+"[ProfD]Mail","true","false","true","true","[ProfD]Mail/pop3.test.test",
+"pop3.test.test","true","testpopaccountname","pop3","testpopusername",
+"[ProfD]Mail/Local Folders","Local Folders","Local Folders","none","nobody",
+"[ProfD]News/testnews.mozilla.org","testnews.mozilla.org","accountnamemozillanews",
+"[ProfD]News/testnews.mozilla.org.rc","nntp","[ProfD]News/testmsnews.microsoft.com",
+"testmsnews.microsoft.com","Microsoft Communities Test",
+"[ProfD]News/testmsnews.microsoft.com.rc","nntp","true",
+"[ProfD]News/news.wingtiptoys.com","news.wingtiptoys.com","donhallnntp",
+"[ProfD]News/news.wingtiptoys.com.rc","nntp","don","true",
+"[ProfD]ImapMail/mail.wingtiptoys.com","mail.wingtiptoys.com","donhallimap",
+"Inbox","imap","don","smtp.pop.test","smtp.wingtiptoys.com","don"
+];
 
-var testdata = "mail.account.account1.serverserver2mail.account.account2.ide" +
-"ntitiesid1mail.account.account2.serverserver1mail.account.account3.identiti" +
-"esid2mail.account.account3.serverserver3mail.account.account4.identitiesid3" +
-"mail.account.account4.serverserver4mail.account.account5.identitiesid4mail." +
-"account.account5.serverserver5mail.account.account6.identitiesid5mail.accou" +
-"nt.account6.serverserver6mail.accountmanager.accountsaccount1mail.accountma" +
-"nager.accountsaccount1,account2mail.accountmanager.accountsaccount1,account" +
-"2,account3mail.accountmanager.accountsaccount1,account2,account3,account4ma" +
-"il.accountmanager.accountsaccount1,account2,account3,account4,account5mail." +
-"accountmanager.accountsaccount1,account2,account3,account4,account5,account" +
-"6mail.accountmanager.defaultaccountaccount1mail.accountmanager.localfolders" +
-"serverserver2mail.identity.id1.fullNamepopdisplaynamemail.identity.id1.orga" +
-"nizationpopdisplaynamemail.identity.id1.reply_totestpop@invalid.invalidmail" +
-".identity.id1.smtpServersmtp1mail.identity.id1.useremailtestpop@invalid.inv" +
-"alidmail.identity.id2.fullNametestmail.identity.id2.organizationtestmail.id" +
-"entity.id2.reply_tomozillanews@invalid.invalidmail.identity.id2.useremailmo" +
-"zillanews@invalid.invalidmail.identity.id4.fullNameDon Hallmail.identity.id" +
-"4.organizationWingtip Toysmail.identity.id4.reply_todon@wingtiptoys.commail" +
-".identity.id4.useremaildon@wingtiptoys.commail.identity.id5.fullNameDon Hal" +
-"lmail.identity.id5.organizationWingtip Toysmail.identity.id5.reply_todon@wi" +
-"ngtiptoys.commail.identity.id5.smtpServersmtp2mail.identity.id5.useremaildo" +
-"n@wingtiptoys.commail.newsrc_root-rel[ProfD]Newsmail.root.imap-rel[Pr" +
-"ofD]ImapMailmail.root.nntp-rel[ProfD]Newsmail.root.none-rel[ProfD]Mailmail." +
-"root.pop3-rel[ProfD]Mailmail.server.server1.check_new_mailtruemail.server.s" +
-"erver1.defer_get_new_mailfalsemail.server.server1.deferred_to_accountaccoun" +
-"t1mail.server.server1.delete_by_age_from_servertruemail.server.server1.dele" +
-"te_mail_left_on_servertruemail.server.server" +
-"1.directory-rel[ProfD]Mail/pop3.test.testmail.server.server1.hostnamepop3.t" +
-"est.testmail.server.server1.leave_on_servertruemail.server.server1.nametest" +
-"popaccountnamemail.server.server1.typepop3mail.server.server1.userNametestp" +
-"opusernamemail.server.server2.directory-rel[P" +
-"rofD]Mail/Local Foldersmail.server.server2.hostnameLocal Foldersmail.server" +
-".server2.nameLocal Foldersmail.server.server2.typenonemail.server.server2.u" +
-"serNamenobodymail.server.server3.direc" +
-"tory-rel[ProfD]News/testnews.mozilla.orgmail.server.server3.hostnametestnew" +
-"s.mozilla.orgmail.server.server3.nameaccountnamemozillanewsmail.server.serv" +
-"er3.newsrc.file-rel[ProfD]News/te" +
-"stnews.mozilla.org.rcmail.server.server3.typenntpmail.server.server4.always" +
-"_authenticatetruemail.server.server4.directory-rel[ProfD]News/news.wingtipt" +
-"oys.commail.server.server4.hostnamenews.wingtiptoys.commail.server.server4." +
-"namedonhallnntpmail.server.server4.newsrc.file-rel[ProfD]News/news.wingtipt" +
-"oys.com.rcmail.server.server4.typenntpmail.server.server4.userNamedonmail.s" +
-"erver.server5.check_new_mailtruemail.server.server5.directory-rel[ProfD]Ima" +
-"pMail/mail.wingtiptoys.commail.server.server5.hostnamemail.wingtiptoys.comm" +
-"ail.server.server5.namedonhallimapmail.server.server5.server_sub_directoryI" +
-"nboxmail.server.server5.typeimapmail.server.server5.userNamedonmail.server." +
-"server6.directory-rel[ProfD]News/testmsnews.microsoft.commail.server.server" +
-"6.hostnametestmsnews.microsoft.commail.server.server6.nameMicrosoft Communi" +
-"ties Testmail.server.server6.newsrc.file-rel[ProfD]News/testmsnews.microsof" +
-"t.com.rcmail.server.server6.typenntpmail.smtpserver.smtp1.hostnamesmtp.pop." +
-"testmail.smtpserver.smtp2.hostnamesmtp.wingtiptoys.commail.smtpserver.smtp2" +
-".usernamedonmail.smtpserverssmtp1mail.smtpserverssmtp1,smtp2";
-
-  // XXX These prefs are excluded from being checked because they contain
-  // relative directories, and hence make the results dependent on where
-  // the tests are run. The prefs do have relative versions which are checked.
-  const excludedPrefs = [
-    "mail.newsrc_root", "mail.root.imap", "mail.root.nntp", "mail.root.none",
-    "mail.root.pop3", "mail.server.server1.directory",
-    "mail.server.server2.directory",
-    "mail.server.server3.directory", "mail.server.server3.newsrc.file",
-    "mail.server.server4.directory", "mail.server.server4.newsrc.file",
-    "mail.server.server5.directory", "mail.server.server5.newsrc.file",
-    "mail.server.server6.directory", "mail.server.server6.newsrc.file"
-  ];
-  dump(excludedPrefs + "\n");
+  var test = [];
   for ( let i = 0; i < importedPrefs.length ; i++){
-    if (excludedPrefs.indexOf(importedPrefs[i].name) != -1)
-      continue;
     dump(importedPrefs[i].name+"--"+importedPrefs[i].data+"\n");
-    test += importedPrefs[i].name + importedPrefs[i].data;
+    test.push(importedPrefs[i].data);
   }
-// uncomment following to get valid test data when changing test data files.
-  dump("\nStart test dump\n" + test + "\nend test dump\n");
 
-  do_check_true(test == testdata);
+// Following is to create valid test data when changing test data files.
+  dump("\nStart test dump\n")
+  test.forEach(function(elem){
+    dump("\"" + elem + "\",")
+    }
+  )
+  dump("\nend test dump\n")
+
+  if (test.length != testdata.length)
+    do_throw("failed in test/testdata length--" + test.length + "/" + testdata.length)
+  for (let i = 0; i < test.length; i++) {
+    if (test[i] != testdata[i])
+      do_throw("failed to match item " + i + ": " + test[i] + " ||| " + testdata[i]);
+  }
 
 //////////////// -- Modified nsIWindowsRegKey implementation ---
   Components.manager
@@ -212,3 +191,127 @@ var testdata = "mail.account.account1.serverserver2mail.account.account2.ide" +
 //////////////// -- Modified nsIWindowsRegKey implementation ---
 }
 
+/////////////// Following are the rules for including data in test
+/*
+Rules and samples for test data
+
+Include /\-rel/
+mail.newsrc_root-rel--[ProfD]News
+mail.root.imap-rel--[ProfD]ImapMail
+mail.root.nntp-rel--[ProfD]News
+mail.root.none-rel--[ProfD]Mail
+mail.root.pop3-rel--[ProfD]Mail
+mail.server.server2.directory-rel--[ProfD]Mail/Local Folders
+mail.server.server3.directory-rel--[ProfD]News/testnews.mozilla.org
+mail.server.server3.newsrc.file-rel--[ProfD]News/testnews.mozilla.org.rc
+mail.server.server4.directory-rel--[ProfD]News/testmsnews.microsoft.com
+mail.server.server4.newsrc.file-rel--[ProfD]News/testmsnews.microsoft.com.rc
+mail.server.server5.directory-rel--[ProfD]News/news.wingtiptoys.com
+mail.server.server5.newsrc.file-rel--[ProfD]News/news.wingtiptoys.com.rc
+mail.server.server6.directory-rel--[ProfD]ImapMail/mail.wingtiptoys.com
+mail.server.server1.directory-rel--[ProfD]Mail/pop3.test.test
+
+remove /mail\.account/
+-mail.account.account1.server--server2
+-mail.account.account2.identities--id1
+-mail.account.account2.server--server1
+-mail.account.account3.identities--id2
+-mail.account.account3.server--server3
+-mail.account.account4.identities--id3
+-mail.account.account4.server--server4
+-mail.account.account5.identities--id4
+-mail.account.account5.server--server5
+-mail.account.account6.identities--id5
+-mail.account.account6.server--server6
+-mail.accountmanager.accounts--account1,account2,account3,account4
+-mail.accountmanager.accounts--account1,account2,account3,account4,account5,account6
+-mail.accountmanager.accounts--account1,account2,account3,account4,account5
+-mail.accountmanager.accounts--account1,account2,account3
+-mail.accountmanager.accounts--account1
+-mail.accountmanager.accounts--account1,account2
+-mail.accountmanager.defaultaccount--account1
+-mail.accountmanager.localfoldersserver--server2
+
+remove /smtpserver$/
+-mail.identity.id1.smtpServer--smtp1
+-mail.identity.id5.smtpServer--smtp2
+
+remove /newsrc_root$/
+-mail.newsrc_root--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\News
+
+remove after including '-rel' /mail\.root/
+-mail.root.imap--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\ImapMail
+-mail.root.nntp--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\News
+-mail.root.none--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\Mail
+-mail.root.pop3--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\Mail
+
+remove /deferred_to_account/
+-mail.server.server1.deferred_to_account--account1
+
+remove after incl '-rel' /mail\.server\.server\d+\.(directory|newsrc)/
+-mail.server.server1.directory--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\Mail\pop3.test.test
+-mail.server.server2.directory--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\Mail\Local Folders
+-mail.server.server3.directory--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\News\testnews.mozilla.org
+-mail.server.server3.newsrc.file--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\News\testnews.mozilla.org.rc
+-mail.server.server4.directory--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\News\testmsnews.microsoft.com
+-mail.server.server4.newsrc.file--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\News\testmsnews.microsoft.com.rc
+-mail.server.server5.directory--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\News\news.wingtiptoys.com
+-mail.server.server5.newsrc.file--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\News\news.wingtiptoys.com.rc
+-mail.server.server6.directory--f:\mozilla\objdir-debug_tb\mozilla\_tests\mailtest\ImapMail\mail.wingtiptoys.com
+
+remove /mail\.smtpservers/
+-mail.smtpservers--smtp1,smtp2
+-mail.smtpservers--smtp1
+
+following should be included
+mail.identity.id1.fullName--popdisplayname
+mail.identity.id1.organization--popdisplayname
+mail.identity.id1.reply_to--testpop@invalid.invalid
+mail.identity.id1.useremail--testpop@invalid.invalid
+mail.identity.id2.fullName--test
+mail.identity.id2.organization--test
+mail.identity.id2.reply_to--mozillanews@invalid.invalid
+mail.identity.id2.useremail--mozillanews@invalid.invalid
+mail.identity.id4.fullName--Don Hall
+mail.identity.id4.organization--Wingtip Toys
+mail.identity.id4.reply_to--don@wingtiptoys.com
+mail.identity.id4.useremail--don@wingtiptoys.com
+mail.identity.id5.fullName--Don Hall
+mail.identity.id5.organization--Wingtip Toys
+mail.identity.id5.reply_to--don@wingtiptoys.com
+mail.identity.id5.useremail--don@wingtiptoys.com
+
+mail.server.server1.check_new_mail--true
+mail.server.server1.defer_get_new_mail--false
+mail.server.server1.delete_by_age_from_server--true
+mail.server.server1.delete_mail_left_on_server--true
+mail.server.server1.hostname--pop3.test.test
+mail.server.server1.leave_on_server--true
+mail.server.server1.name--testpopaccountname
+mail.server.server1.type--pop3
+mail.server.server1.userName--testpopusername
+mail.server.server2.hostname--Local Folders
+mail.server.server2.name--Local Folders
+mail.server.server2.type--none
+mail.server.server2.userName--nobody
+mail.server.server3.hostname--testnews.mozilla.org
+mail.server.server3.name--accountnamemozillanews
+mail.server.server3.type--nntp
+mail.server.server4.hostname--testmsnews.microsoft.com
+mail.server.server4.name--Microsoft Communities Test
+mail.server.server4.type--nntp
+mail.server.server5.always_authenticate--true
+mail.server.server5.hostname--news.wingtiptoys.com
+mail.server.server5.name--donhallnntp
+mail.server.server5.type--nntp
+mail.server.server5.userName--don
+mail.server.server6.check_new_mail--true
+mail.server.server6.hostname--mail.wingtiptoys.com
+mail.server.server6.name--donhallimap
+mail.server.server6.server_sub_directory--Inbox
+mail.server.server6.type--imap
+mail.server.server6.userName--don
+mail.smtpserver.smtp1.hostname--smtp.pop.test
+mail.smtpserver.smtp2.hostname--smtp.wingtiptoys.com
+mail.smtpserver.smtp2.username--don
+*/
