0cfa77a723dd14871e838bd8e128b7a2e1ac6157
Mark Banner <bugzilla@standard8.plus.com>
2010-07-04 13:33:14 +0100

    mail changes for component manager changes, part of bug 575740. r=bienvenu. Part of bustage fix for CLOSED TREE

--- mail/components/build/nsModule.cpp
+++ mail/components/build/nsModule.cpp
@@ -37,7 +37,7 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include "nsIGenericFactory.h"
+#include "mozilla/ModuleUtils.h"
 #include "nsMailMigrationCID.h"
 #include "nsProfileMigrator.h"
 #include "nsSeamonkeyProfileMigrator.h"
@@ -93,67 +93,100 @@ NS_GENERIC_FACTORY_CONSTRUCTOR(nsMailMacIntegration)
 NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsMailWinSearchHelper, Init)
 #endif
 
-static const nsModuleComponentInfo components[] = {
-  { "Profile Importer",
-    NS_THUNDERBIRD_PROFILEIMPORT_CID,
-    NS_PROFILEMIGRATOR_CONTRACTID,
-    nsProfileMigratorConstructor },
-  { "Seamonkey Profile Migrator",
-    NS_SEAMONKEYPROFILEMIGRATOR_CID,
-    NS_MAILPROFILEMIGRATOR_CONTRACTID_PREFIX "seamonkey",
-    nsSeamonkeyProfileMigratorConstructor },
-#if !defined(XP_BEOS)
-  { "Netscape Communicator 4.x",
-    NS_DOGBERTPROFILEMIGRATOR_CID,
-    NS_MAILPROFILEMIGRATOR_CONTRACTID_PREFIX "dogbert",
-    nsDogbertProfileMigratorConstructor },
+NS_DEFINE_NAMED_CID(NS_THUNDERBIRD_PROFILEIMPORT_CID);
+NS_DEFINE_NAMED_CID(NS_SEAMONKEYPROFILEMIGRATOR_CID);
+#ifndef XP_BEOS
+NS_DEFINE_NAMED_CID(NS_DOGBERTPROFILEMIGRATOR_CID);
 #endif
+
 #ifdef XP_WIN32
-  { "Outlook Express Profile Migrator",
-    NS_OEXPRESSPROFILEMIGRATOR_CID,
-    NS_MAILPROFILEMIGRATOR_CONTRACTID_PREFIX "oexpress",
-    nsOEProfileMigratorConstructor },
-  { "Outlook Profile Migrator",
-    NS_OUTLOOKPROFILEMIGRATOR_CID,
-    NS_MAILPROFILEMIGRATOR_CONTRACTID_PREFIX "outlook",
-    nsOutlookProfileMigratorConstructor },
-#endif
+NS_DEFINE_NAMED_CID(NS_OEXPRESSPROFILEMIGRATOR_CID);
+NS_DEFINE_NAMED_CID(NS_OUTLOOKPROFILEMIGRATOR_CID);
+NS_DEFINE_NAMED_CID(NS_MAILWININTEGRATION_CID);
+#ifndef MOZ_DISABLE_VISTA_SDK_REQUIREMENTS
+NS_DEFINE_NAMED_CID(NS_MAILWINSEARCHHELPER_CID);
+#endif // !MOZ_DISABLE_VISTA_SDK_REQUIREMENTS
+#endif // !XP_WIN32
+
 #if defined (XP_WIN32) || defined (XP_MACOSX)
-  { "Eudora Profile Migrator",
-    NS_EUDORAPROFILEMIGRATOR_CID,
-    NS_MAILPROFILEMIGRATOR_CONTRACTID_PREFIX "eudora",
-    nsEudoraProfileMigratorConstructor },
+NS_DEFINE_NAMED_CID(NS_EUDORAPROFILEMIGRATOR_CID);
+#endif
+
+#ifdef MOZ_WIDGET_GTK2
+NS_DEFINE_NAMED_CID(NS_MAILGNOMEINTEGRATION_CID);
+#endif
+
+#ifdef XP_MACOSX
+NS_DEFINE_NAMED_CID(NS_MAILMACINTEGRATION_CID);
+#endif
+
+#ifndef MOZ_PLACES
+NS_DEFINE_NAMED_CID(NS_HISTORYSERVICE_CID);
+#endif
+
+const mozilla::Module::CIDEntry kMailCIDs[] = {
+  { &kNS_THUNDERBIRD_PROFILEIMPORT_CID, false, NULL, nsProfileMigratorConstructor },
+  { &kNS_SEAMONKEYPROFILEMIGRATOR_CID, false, NULL, nsSeamonkeyProfileMigratorConstructor },
+#if !defined(XP_BEOS)
+  { &kNS_DOGBERTPROFILEMIGRATOR_CID, false, NULL, nsDogbertProfileMigratorConstructor },
 #endif
 #ifdef XP_WIN32
-  { "Mail Windows Integration",
-    NS_MAILWININTEGRATION_CID,
-    "@mozilla.org/mail/shell-service;1",
-    nsWindowsShellServiceConstructor},
+  { &kNS_OEXPRESSPROFILEMIGRATOR_CID, false, NULL, nsOEProfileMigratorConstructor },
+  { &kNS_OUTLOOKPROFILEMIGRATOR_CID, false, NULL, nsOutlookProfileMigratorConstructor },
+  { &kNS_MAILWININTEGRATION_CID, false, NULL, nsWindowsShellServiceConstructor },
+#ifndef MOZ_DISABLE_VISTA_SDK_REQUIREMENTS
+  { &kNS_MAILWINSEARCHHELPER_CID, false, NULL, nsMailWinSearchHelperConstructor },
+#endif // !MOZ_DISABLE_VISTA_SDK_REQUIREMENTS
+#endif // !XP_WIN32
+#if defined (XP_WIN32) || defined (XP_MACOSX)
+  { &kNS_EUDORAPROFILEMIGRATOR_CID, false, NULL, nsEudoraProfileMigratorConstructor },
 #endif
 #ifdef MOZ_WIDGET_GTK2
-  { "Mail GNOME Integration",
-    NS_MAILGNOMEINTEGRATION_CID,
-    "@mozilla.org/mail/shell-service;1",
-    nsMailGNOMEIntegrationConstructor },
+  { &kNS_MAILGNOMEINTEGRATION_CID, false, NULL, nsMailGNOMEIntegrationConstructor },
 #endif
 #ifdef XP_MACOSX
-  { "Mail Mac Integration",
-    NS_MAILMACINTEGRATION_CID,
-    "@mozilla.org/mail/shell-service;1",
-    nsMailMacIntegrationConstructor },
-#endif
-#if defined(XP_WIN32) && !defined(MOZ_DISABLE_VISTA_SDK_REQUIREMENTS)
-  { "Mail Windows Search Integration Helper",
-    NS_MAILWINSEARCHHELPER_CID,
-    "@mozilla.org/mail/windows-search-helper;1",
-    nsMailWinSearchHelperConstructor },
+  { &kNS_MAILMACINTEGRATION_CID, false, NULL, nsMailMacIntegrationConstructor },
 #endif
 #ifndef MOZ_PLACES
-  { "Mail History Empty implementation",
-    NS_HISTORYSERVICE_CID,
-    NS_IHISTORY_CONTRACTID,
-    HistoryConstructor },
+  { &kNS_HISTORYSERVICE_CID, false, NULL, HistoryConstructor },
 #endif
+  { NULL }
 };
 
-NS_IMPL_NSGETMODULE(nsMailCompsModule, components)
+const mozilla::Module::ContractIDEntry kMailContracts[] = {
+  { NS_PROFILEMIGRATOR_CONTRACTID, &kNS_THUNDERBIRD_PROFILEIMPORT_CID },
+  { NS_MAILPROFILEMIGRATOR_CONTRACTID_PREFIX "seamonkey", &kNS_SEAMONKEYPROFILEMIGRATOR_CID },
+#if !defined(XP_BEOS)
+  { NS_MAILPROFILEMIGRATOR_CONTRACTID_PREFIX "dogbert", &kNS_DOGBERTPROFILEMIGRATOR_CID },
+#endif
+#ifdef XP_WIN32
+  { NS_MAILPROFILEMIGRATOR_CONTRACTID_PREFIX "oexpress", &kNS_OEXPRESSPROFILEMIGRATOR_CID },
+  { NS_MAILPROFILEMIGRATOR_CONTRACTID_PREFIX "outlook", &kNS_OUTLOOKPROFILEMIGRATOR_CID },
+  { "@mozilla.org/mail/shell-service;1", &kNS_MAILWININTEGRATION_CID },
+#ifndef MOZ_DISABLE_VISTA_SDK_REQUIREMENTS
+  { "@mozilla.org/mail/windows-search-helper;1", &kNS_MAILWINSEARCHHELPER_CID },
+#endif // !MOZ_DISABLE_VISTA_SDK_REQUIREMENTS
+#endif // !XP_WIN32
+#if defined (XP_WIN32) || defined (XP_MACOSX)
+  { NS_MAILPROFILEMIGRATOR_CONTRACTID_PREFIX "eudora", &kNS_EUDORAPROFILEMIGRATOR_CID },
+#endif
+#ifdef MOZ_WIDGET_GTK2
+  { "@mozilla.org/mail/shell-service;1", &kNS_MAILGNOMEINTEGRATION_CID },
+#endif
+#ifdef XP_MACOSX
+  { "@mozilla.org/mail/shell-service;1", &kNS_MAILMACINTEGRATION_CID },
+#endif
+#ifndef MOZ_PLACES
+  { NS_IHISTORY_CONTRACTID, &kNS_HISTORYSERVICE_CID },
+#endif
+  { NULL }
+};
+
+static const mozilla::Module kMailCompsModule = {
+  mozilla::Module::kVersion,
+  kMailCIDs,
+  kMailContracts
+};
+
+NSMODULE_DEFN(nsMailCompsModule) = &kMailCompsModule;
+
