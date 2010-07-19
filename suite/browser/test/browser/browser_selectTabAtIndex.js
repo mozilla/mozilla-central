56cc6e6c8146563a00adac3ecddd10db610216b3
Misak Khachatryan <misak.bugzilla@gmail.com>
2010-07-19 20:21:47 +0500

    Bug 579845 - Implement Firefox TabBrowser API: selectTabAtIndex() to get feature parity. r=Neil, sr=Neil

---
+++ suite/browser/test/browser/browser_selectTabAtIndex.js
@@ -0,0 +1,22 @@
+function test() {
+  for (let i = 0; i < 9; i++)
+    gBrowser.addTab();
+
+/* This part tests accel keys, which are not implemented in Seamonkey yet.
+ * Commenting out for now ...
+ * var isLinux = navigator.platform.indexOf("Linux") == 0;
+ * for (let i = 9; i >= 1; i--) {
+ *   EventUtils.synthesizeKey(i.toString(), { altKey: isLinux, accelKey: !isLinux });
+ *
+ *   is(gBrowser.tabContainer.selectedIndex, (i == 9 ? gBrowser.tabs.length : i) - 1,
+ *      (isLinux ? "Alt" : "Accel") + "+" + i + " selects expected tab");
+ * }
+ */
+
+  gBrowser.selectTabAtIndex(-3);
+  is(gBrowser.tabContainer.selectedIndex, gBrowser.tabs.length - 3,
+     "gBrowser.selectTabAtIndex(-3) selects expected tab");
+
+  for (let i = 0; i < 9; i++)
+    gBrowser.removeCurrentTab();
+}
