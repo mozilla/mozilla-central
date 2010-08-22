9df63b948fcbf51423e3dc66a5c17bd76122c8e9
Robert Kaiser <kairo@kairo.at>
2010-08-22 16:47:08 +0200

    bug 557583 - Update SeaMonkey to newer AUS URL scheme, r=Callek

--- suite/browser/browser-prefs.js
+++ suite/browser/browser-prefs.js
@@ -428,7 +428,7 @@ pref("app.update.mode", 1);
 pref("app.update.silent", false);
 
 // Update service URL:
-pref("app.update.url", "https://aus2-community.mozilla.org/update/1/%PRODUCT%/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/update.xml");
+pref("app.update.url", "https://aus2-community.mozilla.org/update/3/%PRODUCT%/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml");
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
 pref("app.update.url.manual", "http://www.seamonkey-project.org/");
