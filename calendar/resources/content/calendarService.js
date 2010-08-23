ee5fff3d62782084fb5d4bece6149d50e748d147
Philipp Kewisch <mozilla@kewis.ch>
2010-08-23 13:31:14 +0200

    Fix bug 576746 - Command Line Service - Make calendar XPCOM components use new manifests and data tables. r=mschroeder
    --HG--
    extra : rebase_source : 84115a006a1b52d3418b8ff154215a6bda39a8f7

--- calendar/resources/content/calendarService.js
+++ calendar/resources/content/calendarService.js
@@ -1,5 +1,4 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * ***** BEGIN LICENSE BLOCK *****
+/* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
  * The contents of this file are subject to the Mozilla Public License Version
@@ -22,6 +21,7 @@
  * Contributor(s):
  *   Seth Spitzer <sspitzer@netscape.com>
  *   Robert Ginda <rginda@netscape.com>
+ *   Philipp Kewisch <mozilla@kewis.ch>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -49,82 +49,45 @@
  *    that webcal:// links have the content type text/calendar (BogusChannel)
  */
 
-/* components defined in this file */
-const CLINE_SERVICE_CONTRACTID =
-    "@mozilla.org/commandlinehandler/general-startup;1?type=calendar";
-const CLINE_SERVICE_CID =
-    Components.ID("{65ef4b0b-d116-4b93-bf8a-84525992bf27}");
-const ICALCNT_HANDLER_CONTRACTID =
-    "@mozilla.org/uriloader/content-handler;1?type=text/calendar";
-const ICALCNT_HANDLER_CID =
-    Components.ID("{9ebf4c8a-7770-40a6-aeed-e1738129535a}");
-const ICALPROT_HANDLER_CID =
-    Components.ID("{d320ba05-88cf-44a6-b718-87a72ef05918}");
-
-/* components used in this file */
-const MEDIATOR_CONTRACTID =
-    "@mozilla.org/appshell/window-mediator;1";
-const STANDARDURL_CONTRACTID =
-    "@mozilla.org/network/standard-url;1";
-const ASS_CONTRACTID =
-    "@mozilla.org/appshell/appShellService;1";
-const WINDOWWATCHER_CONTRACTID =
-    "@mozilla.org/embedcomp/window-watcher;1";
-
-/* interafces used in this file */
-const nsIWindowMediator  = Components.interfaces.nsIWindowMediator;
-const nsICmdLineHandler  = Components.interfaces.nsICmdLineHandler;
-const nsICategoryManager = Components.interfaces.nsICategoryManager;
-const nsIContentHandler  = Components.interfaces.nsIContentHandler;
-const nsIFactory         = Components.interfaces.nsIFactory;
-const nsIProtocolHandler = Components.interfaces.nsIProtocolHandler;
-const nsIURI             = Components.interfaces.nsIURI;
-const nsIStandardURL     = Components.interfaces.nsIStandardURL;
-const nsIChannel         = Components.interfaces.nsIChannel;
-const nsIRequest         = Components.interfaces.nsIRequest;
-const nsIAppShellService = Components.interfaces.nsIAppShellService;
-const nsISupports        = Components.interfaces.nsISupports;
-const nsIWindowWatcher   = Components.interfaces.nsIWindowWatcher;
-
-if ("nsICommandLineHandler" in Components.interfaces)
-     const nsICommandLineHandler = Components.interfaces.nsICommandLineHandler;
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("resource://calendar/modules/calUtils.jsm");
 
 /* Command Line handler service */
-function CLineService()
-{}
+function CLineService() {
+}
 
 CLineService.prototype = {
-    /* nsISupports */
-    QueryInterface : function service_qi(iid) {
-        if (iid.equals(nsISupports))
-            return this;
+    getInterfaces: function getInterfaces(aCount) {
+        const ifaces = [
+            Components.interfaces.nsISupports,
+            Components.interfaces.nsIClassInfo,
+            Components.interfaces.nsICommandLineHandler
+        ];
 
-        if (nsICmdLineHandler && iid.equals(nsICmdLineHandler))
-            return this;
-
-        if (nsICommandLineHandler && iid.equals(nsICommandLineHandler))
-            return this;
-
-        throw Components.results.NS_ERROR_NO_INTERFACE;
+        aCount.value = ifaces.length;
+        return ifaces;
     },
 
-    /* nsICmdLineHandler */
+    getHelperForLanguage: function (language) {
+        return null;
+    },
 
-    commandLineArgument : "-calendar",
-    prefNameForStartup  : "general.startup.calendar",
-    chromeUrlForTask    : "chrome://calendar/content",
-    helpText            : "Start with calendar",
-    handlesArgs         : false,
-    defaultArgs         : "",
-    openWindowWithArgs  : true,
+    contractID: "@mozilla.org/commandlinehandler/general-startup;1?type=calendar",
+    classDescription: "Calendar Commandline Handler",
+    classID: Components.ID("{65ef4b0b-d116-4b93-bf8a-84525992bf27}"),
+    implementationLanguage: Components.interfaces.nsIProgrammingLanguage.JAVASCRIPT,
+    flags: Components.interfaces.nsIClassInfo.SINGLETON,
+
+    QueryInterface: function QueryInterface(aIID) {
+        return cal.doQueryInterface(this, CLineService.prototype, aIID, null, this);
+    },
 
     /* nsICommandLineHandler */
-
     handle : function service_handle(cmdLine) {
         if (!cmdLine.preventDefault) {
             // just pass all arguments on to the Sunbird window
-            wwatch = Components.classes[WINDOWWATCHER_CONTRACTID]
-                               .getService(nsIWindowWatcher);
+            let wwatch = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
+                                   .getService(Components.interfaces.nsIWindowWatcher);
             wwatch.openWindow(null, "chrome://sunbird/content/calendar.xul",
                                   "_blank", "chrome,dialog=no,all", cmdLine);
             cmdLine.preventDefault = true;
@@ -137,221 +100,143 @@ CLineService.prototype = {
                "                       to show this date on startup.\n"
 };
 
-/* factory for command line handler service (CLineService) */
-var CLineFactory = {
-    createInstance : function (outer, iid) {
-        if (outer != null)
-            throw Components.results.NS_ERROR_NO_AGGREGATION;
-
-        return new CLineService().QueryInterface(iid);
-    }
-}
-
 /* text/calendar content handler */
-function ICALContentHandler ()
-{}
+function ICALContentHandler() {}
 
-ICALContentHandler.prototype.QueryInterface =
-function (iid) {
+ICALContentHandler.prototype = {
+    getInterfaces: function getInterfaces(aCount) {
+        const ifaces = [
+            Components.interfaces.nsISupports,
+            Components.interfaces.nsIClassInfo,
+            Components.interfaces.nsIContentHandler
+        ];
 
-    if (iid.equals(nsIContentHandler) ||
-        iid.equals(nsISupports))
-        return this;
+        aCount.value = ifaces.length;
+        return ifaces;
+    },
 
-    throw Components.results.NS_ERROR_NO_INTERFACE;
-}
+    getHelperForLanguage: function (language) {
+        return null;
+    },
 
-ICALContentHandler.prototype.handleContent =
-function(aContentType, aWindowTarget, aRequest)
-{
-    var channel = aRequest.QueryInterface(nsIChannel);
+    contractID: "@mozilla.org/uriloader/content-handler;1?type=text/calendar",
+    classDescription: "text/calendar Content Handler",
+    classID: Components.ID("{9ebf4c8a-7770-40a6-aeed-e1738129535a}"),
+    implementationLanguage: Components.interfaces.nsIProgrammingLanguage.JAVASCRIPT,
 
-    // Cancel the request ...
-    var uri = channel.URI;
-    const NS_BINDING_ABORTED = 0x804b0002 // from nsNetError.h
-    aRequest.cancel(NS_BINDING_ABORTED);
+    QueryInterface: function QueryInterface(aIID) {
+        return cal.doQueryInterface(this, ICALContentHandler.prototype, aIID, null, this);
+    },
 
-    // ... Subscribe to the uri ...
-    var calendarManager = Components.classes["@mozilla.org/calendar/manager;1"]
-                                    .getService(Components.interfaces.calICalendarManager);
+    handleContent: function handleContent(aContentType, aWindowTarget, aRequest) {
+        let channel = aRequest.QueryInterface(Components.interfaces.nsIChannel);
 
-    var newCalendar = calendarManager.createCalendar('ics', uri);
+        // Cancel the request ...
+        let uri = channel.URI;
+        const NS_BINDING_ABORTED = 0x804b0002 // from nsNetError.h
+        aRequest.cancel(NS_BINDING_ABORTED);
 
-    // XXX Come up with a better name, like the filename or X-WR-CALNAME
-    // XXX Also, make up a color
-    newCalendar.name = "temp";
+        // ... Subscribe to the uri ...
+        let calendarManager = cal.getCalendarManager();
+        let newCalendar = calendarManager.createCalendar('ics', uri);
 
-    calendarManager.registerCalendar(newCalendar);
+        // XXX Come up with a better name, like the filename or X-WR-CALNAME
+        // XXX Also, make up a color
+        newCalendar.name = "temp";
 
-    // ... and open or focus a calendar window.
-    var windowManager = Components.classes[MEDIATOR_CONTRACTID]
-                                  .getService(nsIWindowMediator);
+        calendarManager.registerCalendar(newCalendar);
 
-    var w = windowManager.getMostRecentWindow("calendarMainWindow");
+        // ... and open or focus a calendar window.
+        let w = cal.getCalendarWindow();
 
-    if (w) {
-        w.focus();
-    } else {
-        var ass = Components.classes[ASS_CONTRACTID]
-                            .getService(nsIAppShellService);
-        w = ass.hiddenDOMWindow;
+        if (w) {
+            w.focus();
+        } else {
+            let ass = Components.classes["@mozilla.org/appshell/appShellService;1"]
+                                .getService(Components.interfaces.nsIAppShellService);
+            w = ass.hiddenDOMWindow;
 
-        var args = new Object ();
-        args.channel = channel;
-        w.openDialog("chrome://sunbird/content/calendar.xul", "calendar", "chrome,menubar,resizable,scrollbars,status,toolbar,dialog=no", args);
+            let args = {};
+            args.channel = channel;
+            w.openDialog("chrome://sunbird/content/calendar.xul",
+                         "calendar",
+                         "chrome,menubar,resizable,scrollbars,status,toolbar,dialog=no",
+                         args);
+        }
     }
+};
+
+function NYI() {
+    throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
 }
-
-/* content handler factory object (ICALContentHandler) */
-var ICALContentHandlerFactory = new Object();
-
-ICALContentHandlerFactory.createInstance =
-function (outer, iid) {
-    if (outer != null)
-        throw Components.results.NS_ERROR_NO_AGGREGATION;
-
-    if (!iid.equals(nsIContentHandler) && !iid.equals(nsISupports))
-        throw Components.results.NS_ERROR_INVALID_ARG;
-
-    return new ICALContentHandler();
-}
-
 
 /* bogus webcal channel used by the ICALProtocolHandler */
-function BogusChannel (aURI)
-{
+function BogusChannel(aURI) {
     this.URI = aURI;
     this.originalURI = aURI;
 }
 
-BogusChannel.prototype.QueryInterface =
-function (iid) {
+BogusChannel.prototype = {
+    getInterfaces: function getInterfaces(aCount) {
+        const ifaces = [
+            Components.interfaces.nsISupports,
+            Components.interfaces.nsIClassInfo,
+            Components.interfaces.nsIRequest,
+            Components.interfaces.nsIChannel
+        ];
 
-    if (iid.equals(nsIChannel) ||
-        iid.equals(nsIRequest) ||
-        iid.equals(nsISupports))
-        return this;
+        aCount.value = ifaces.length;
+        return ifaces;
+    },
 
-    throw Components.results.NS_ERROR_NO_INTERFACE;
-}
+    getHelperForLanguage: function (language) {
+        return null;
+    },
 
-/* nsIChannel */
-BogusChannel.prototype.loadAttributes = null;
-BogusChannel.prototype.contentType = "text/calendar";
-BogusChannel.prototype.contentLength = 0;
-BogusChannel.prototype.owner = null;
-BogusChannel.prototype.loadGroup = null;
-BogusChannel.prototype.notificationCallbacks = null;
-BogusChannel.prototype.securityInfo = null;
+    contractID: "@mozilla.org/calendar/webcal-channel;1",
+    classDescription: "webcal bogus channel",
 
-BogusChannel.prototype.open =
-BogusChannel.prototype.asyncOpen =
-function ()
-{
-    throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
-}
+    classID: Components.ID("{1e36ef5d-92d9-488f-ada6-9f15a1b57acf}"),
+    implementationLanguage: Components.interfaces.nsIProgrammingLanguage.JAVASCRIPT,
 
-BogusChannel.prototype.asyncOpen =
-function (observer, ctxt)
-{
-    observer.onStartRequest (this, ctxt);
-}
+    URI: null,
+    originalURI: null,
 
-BogusChannel.prototype.asyncRead =
-function (listener, ctxt)
-{
-    return listener.onStartRequest (this, ctxt);
-}
+    QueryInterface: function QueryInterface(aIID) {
+        return cal.doQueryInterface(this, BogusChannel.prototype, aIID, null, this);
+    },
 
-/* nsIRequest */
-BogusChannel.prototype.isPending =
-function ()
-{
-    return true;
-}
+    /* nsIChannel */
+    loadAttributes: null,
+    contentType: "text/calendar",
+    contentLength: 0,
+    owner: null,
+    loadGroup: null,
+    notificationCallbacks: null,
+    securityInfo: null,
 
-BogusChannel.prototype.status = Components.results.NS_OK;
+    open: NYI,
+    asyncOpen: NYI,
 
-BogusChannel.prototype.cancel =
-function (aStatus)
-{
-    this.status = aStatus;
-}
+    asyncOpen: function asyncOpen(observer, ctxt) {
+        observer.onStartRequest(this, ctxt);
+    },
 
-BogusChannel.prototype.suspend =
-BogusChannel.prototype.resume =
-function ()
-{
-    throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
-}
+    asyncRead: function asyncRead(listener, ctxt) {
+        return listener.onStartRequest(this, ctxt);
+    },
 
-var CalendarModule = new Object();
+    /* nsIRequest */
+    isPending: function() true,
+    status: Components.results.NS_OK,
 
-CalendarModule.registerSelf =
-function (compMgr, fileSpec, location, type)
-{
-    // dump("*** Registering -calendar handler.\n");
+    cancel: function cancel(aStatus) {
+        this.status = aStatus;
+    },
 
-    compMgr = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+    suspend: NYI,
+    resume: NYI
+};
 
-    compMgr.registerFactoryLocation(CLINE_SERVICE_CID,
-                                    "Calendar CommandLine Service",
-                                    CLINE_SERVICE_CONTRACTID,
-                                    fileSpec,
-                                    location,
-                                    type);
-
-    var catman = Components.classes["@mozilla.org/categorymanager;1"]
-                           .getService(nsICategoryManager);
-    catman.addCategoryEntry("command-line-argument-handlers",
-                            "calendar command line handler",
-                            CLINE_SERVICE_CONTRACTID, true, true);
-    catman.addCategoryEntry("command-line-handler", "x-default",
-                            CLINE_SERVICE_CONTRACTID, true, true);
-
-    // dump("*** Registering text/calendar handler.\n");
-    compMgr.registerFactoryLocation(ICALCNT_HANDLER_CID,
-                                    "Webcal Content Handler",
-                                    ICALCNT_HANDLER_CONTRACTID,
-                                    fileSpec,
-                                    location,
-                                    type);
-}
-
-CalendarModule.unregisterSelf =
-function(compMgr, fileSpec, location)
-{
-    compMgr = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar);
-
-    compMgr.unregisterFactoryLocation(CLINE_SERVICE_CID, fileSpec);
-    compMgr.unregisterFactoryLocation(ICALCNT_HANDLER_CID, fileSpec);
-
-    catman = Components.classes["@mozilla.org/categorymanager;1"]
-                       .getService(nsICategoryManager);
-    catman.deleteCategoryEntry("command-line-argument-handlers",
-                               "calendar command line handler", true);
-    catman.deleteCategoryEntry("command-line-handler",
-                               "x-default", true);
-}
-
-CalendarModule.getClassObject =
-function (compMgr, cid, iid) {
-    if (cid.equals(CLINE_SERVICE_CID))
-        return CLineFactory;
-
-    if (cid.equals(ICALCNT_HANDLER_CID))
-        return ICALContentHandlerFactory;
-
-    throw Components.results.NS_ERROR_NO_INTERFACE;
-}
-
-CalendarModule.canUnload =
-function(compMgr)
-{
-    return true;
-}
-
-/* entrypoint */
-function NSGetModule(compMgr, fileSpec) {
-    return CalendarModule;
-}
+var components = [CLineService, ICALContentHandler, BogusChannel];
+var NSGetFactory = XPCOMUtils.generateNSGetFactory(components);
