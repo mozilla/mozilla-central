f1574486a8cf6dbbb9774b4c8897c7da4d76f744
Robert Kaiser <kairo@kairo.at>
2010-08-24 17:47:07 +0200

    Test for bug 584630 (Find / FAYT is not focusing on correct tab), r=mcsmurf

---
+++ suite/browser/test/browser/browser_fayt.js
@@ -0,0 +1,25 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+function test() {
+  var tab1 = gBrowser.addTab("data:text/html,<p>this is some dummy text</p>");
+  var tab2 = gBrowser.addTab("data:text/html,<p>this is some random text</p>");
+
+  gBrowser.getBrowserForTab(tab2).addEventListener("load", runTest, true);
+  waitForExplicitFinish();
+
+  function runTest() {
+    gBrowser.getBrowserForTab(tab2).removeEventListener("load", runTest, true);
+
+    gBrowser.selectedTab = tab2;
+    is(gBrowser.fastFind.find("random", false), Components.interfaces.nsITypeAheadFind.FIND_FOUND, "FAYT found the random text");
+    gBrowser.selectedTab = tab1;
+    is(gBrowser.fastFind.find("dummy", false), Components.interfaces.nsITypeAheadFind.FIND_FOUND, "FAYT found the dummy text");
+
+    gBrowser.removeTab(tab2);
+    gBrowser.removeTab(tab1);
+    finish();
+  }
+}
+
