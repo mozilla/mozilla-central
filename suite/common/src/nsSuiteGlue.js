cf4dec783d4dbc943d67d4f132b7f67b19f02f9b
Jens Hatlak (:InvisibleSmiley) <jh@junetz.de>
2010-09-18 23:58:53 +0100

    Bug 588651 - Privacy options are not cleared on closing SeaMonkey and instead cleared when SeaMonkey is reopened r=callek

--- suite/common/src/nsSuiteGlue.js
+++ suite/common/src/nsSuiteGlue.js
@@ -143,8 +143,7 @@ SuiteGlue.prototype = {
           Services.obs.removeObserver(this, "places-shutdown");
           this._isPlacesShutdownObserver = false;
         }
-        // places-shutdown is fired on profile-before-change, but before
-        // Places executes the last flush and closes connection.
+        // places-shutdown is fired when the profile is about to disappear.
         this._onProfileShutdown();
         break;
       case "idle":
