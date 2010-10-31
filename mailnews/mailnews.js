fc82f974b7c35589add42e40b950dd190d94e01e
Phil Ringnalda <philringnalda@gmail.com>
2010-10-31 01:24:57 -0700

    Bug 180798 - Remove prematurely added pref for auto-unzipping attachments, r=bienvenu
    --HG--
    extra : rebase_source : 68abfae1154452d75f1eae43e6a20786feab8724

--- mailnews/mailnews.js
+++ mailnews/mailnews.js
@@ -64,7 +64,6 @@ pref("mail.wrap_long_lines",                true);
 pref("news.wrap_long_lines",                true);
 pref("mail.inline_attachments",             true);
 pref("mail.reply_quote_inline",             false);
-pref("mailnews.auto_unzip_saved_attachments", false);
 
 // hidden pref for controlling if the user agent string
 // is displayed in the message pane or not...
