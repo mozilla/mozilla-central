f022a50f34df3c878d8382de73988aa54ef27d50
Mark Banner <bugzilla@standard8.plus.com>
2010-11-30 09:59:06 +0000

    Bug 576264 Update urls for nightly builds. r=gozer

--- mail/branding/nightly/thunderbird-branding.js
+++ mail/branding/nightly/thunderbird-branding.js
@@ -1,11 +1,11 @@
 // Default start page
-pref("mailnews.start_page.url","http://live.mozillamessaging.com/%APP%/start?locale=%LOCALE%&version=%VERSION%&os=%OS%&buildid=%APPBUILDID%");
+pref("mailnews.start_page.url","https://live.mozillamessaging.com/%APP%/start?locale=%LOCALE%&version=%VERSION%&os=%OS%&buildid=%APPBUILDID%");
 
 // first launch welcome page
-pref("mailnews.start_page.welcome_url","http://live.mozillamessaging.com/%APP%/firstrun?locale=%LOCALE%&version=%VERSION%&os=%OS%&buildid=%APPBUILDID%");
+pref("mailnews.start_page.welcome_url","https://live.mozillamessaging.com/%APP%/firstrun?locale=%LOCALE%&version=%VERSION%&os=%OS%&buildid=%APPBUILDID%");
 
 // start page override to load after an update
-pref("mailnews.start_page.override_url","http://live.mozillamessaging.com/%APP%/whatsnew?locale=%LOCALE%&version=%VERSION%&os=%OS%&buildid=%APPBUILDID%");
+pref("mailnews.start_page.override_url","https://live.mozillamessaging.com/%APP%/whatsnew?locale=%LOCALE%&version=%VERSION%&os=%OS%&buildid=%APPBUILDID%");
 
 // Interval: Time between checks for a new version (in seconds)
 // nightly=8 hours, official=24 hours
