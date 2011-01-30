e67d10f4a366a73699c1e31950fade9e7c49d797
Serge Gautherie <sgautherie.bz@free.fr>
2011-01-30 06:22:14 +0100

    Bug 625936 - Port |Bug 620837 - Modify blocklist pingCount parameter to track pingCount for profile and for version| to SeaMonkey; (Av1) Just copy it. r=Callek.

--- suite/browser/browser-prefs.js
+++ suite/browser/browser-prefs.js
@@ -533,7 +533,7 @@ pref("extensions.logging.enabled", false);
 // Blocklist preferences
 pref("extensions.blocklist.enabled", true);
 pref("extensions.blocklist.interval", 86400);
-pref("extensions.blocklist.url", "https://addons.mozilla.org/blocklist/3/%APP_ID%/%APP_VERSION%/%PRODUCT%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/%PING_COUNT%/");
+pref("extensions.blocklist.url", "https://addons.mozilla.org/blocklist/3/%APP_ID%/%APP_VERSION%/%PRODUCT%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/%PING_COUNT%/%TOTAL_PING_COUNT%/%DAYS_SINCE_LAST_PING%/");
 pref("extensions.blocklist.detailsURL", "https://www.mozilla.com/%LOCALE%/blocklist/");
 
 // Symmetric (can be overridden by individual extensions) update preferences.
