7e97ff6094beb47b8c8b067c90374069f1f7a252
Serge Gautherie <sgautherie.bz@free.fr>
2011-03-08 16:10:32 +0100

    Bug 630140 - Port |Bug 595652 - fix hit testing for border-radius| to SeaMonkey; (Ev1-SM) Increase delay to 250 ms from 100. rs=iann. DONTBUILD

--- suite/browser/test/browser/browser_bug462289.js
+++ suite/browser/test/browser/browser_bug462289.js
@@ -1,6 +1,6 @@
 // Wanted delay (in ms) to let UI fully update.
-// 100: hopefully enough (on slow test environments).
-var gDelay = 100;
+// 250: hopefully enough (on slow test environments).
+var gDelay = 250;
 
 var tab1, tab2;
 
