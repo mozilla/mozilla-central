1dd0044980c37a6bb0aaf77911761ea1613576e4
Misak Khachatryan <misak.bugzilla@gmail.com>
2011-03-21 16:51:34 +0400

    Bug 639836 - Port Bug 581937 [Recently closed tabs list keep blank tabs], also add observer for browser.sessionstore.max_tabs_undo. Part 1 - tabbrowser observer. r=Neil

--- suite/common/src/nsSessionStore.js
+++ suite/common/src/nsSessionStore.js
@@ -243,8 +243,9 @@ SessionStoreService.prototype = {
     this._resume_from_crash = this._prefBranch.getBoolPref("sessionstore.resume_from_crash");
     this._prefBranch.addObserver("sessionstore.resume_from_crash", this, true);
 
-     // observe prefs changes so we can modify stored data to match
-     this._prefBranch.addObserver("sessionstore.max_windows_undo", this, true);
+    // observe prefs changes so we can modify stored data to match
+    this._prefBranch.addObserver("sessionstore.max_tabs_undo", this, true);
+    this._prefBranch.addObserver("sessionstore.max_windows_undo", this, true);
 
     // this pref is only read at startup, so no need to observe it
     this._sessionhistory_max_entries =
@@ -471,6 +472,13 @@ SessionStoreService.prototype = {
       break;
     case "nsPref:changed": // catch pref changes
       switch (aData) {
+      // if the user decreases the max number of closed tabs they want
+      // preserved update our internal states to match that max
+      case "sessionstore.max_tabs_undo":
+        for (let ix in this._windows) {
+          this._windows[ix]._closedTabs.splice(this._prefBranch.getIntPref("sessionstore.max_tabs_undo"));
+        }
+        break;
       case "sessionstore.max_windows_undo":
         this._capClosedWindows();
         break;
@@ -741,9 +749,9 @@ SessionStoreService.prototype = {
         this._updateCookies([winData]);
       }
 
-      // save the window if it has multiple tabs or a single tab with entries
+      // save the window if it has multiple tabs or a single saveable tab
       if (winData.tabs.length > 1 ||
-          (winData.tabs.length == 1 && winData.tabs[0].entries.length > 0)) {
+          (winData.tabs.length == 1 && this._shouldSaveTabState(winData.tabs[0]))) {
         this._closedWindows.unshift(winData);
         this._capClosedWindows();
       }
@@ -848,7 +856,7 @@ SessionStoreService.prototype = {
     this._updateTextAndScrollDataForTab(aWindow, aTab.linkedBrowser, tabState);
 
     // store closed-tab data for undo
-    if (tabState.entries.length > 0) {
+    if (this._shouldSaveTabState(tabState)) {
       aTab.tabData = { state: tabState };
       var closedTabs = this._windows[aWindow.__SSi]._closedTabs;
       closedTabs.unshift(aTab.tabData);
@@ -3323,6 +3331,24 @@ SessionStoreService.prototype = {
     return max_resumed_crashes != -1 &&
            (aRecentCrashes > max_resumed_crashes ||
             sessionAge && sessionAge >= SIX_HOURS_IN_MS);
+  },
+
+  /**
+   * Determine if the tab state we're passed is something we should save. This
+   * is used when closing a tab or closing a window with a single tab
+   *
+   * @param aTabState
+   *        The current tab state
+   * @returns boolean
+   */
+  _shouldSaveTabState: function sss__shouldSaveTabState(aTabState) {
+    // If the tab has only the transient about:blank history entry, no other
+    // session history, and no userTypedValue, then we don't actually want to
+    // store this tab's data.
+    return aTabState.entries.length &&
+           !(aTabState.entries.length == 1 &&
+             aTabState.entries[0].url == "about:blank" &&
+             !aTabState.userTypedValue);
   },
 
   /**
