1b1ebe4fcfbb59b3f1f36fa199f25b01c4f9c5ae
Siddharth Agarwal <sid.bugzilla@gmail.com>
2011-03-31 01:50:46 +0530

    Bug 641781 - Port the safe mode startup window to Thunderbird. r=Standard8, sr=bienvenu, ui-r=clarkbw

---
+++ mail/components/mailGlue.js
@@ -0,0 +1,95 @@
+/* -*- Mode: JavaScript; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * the Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Siddharth Agarwal <sid.bugzilla@gmail.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+const Ci = Components.interfaces;
+const Cc = Components.classes;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+/**
+ * Glue code that should be executed before any windows are opened. Any
+ * window-independent helper methods (a la nsBrowserGlue.js) should go in
+ * MailUtils.js instead.
+ */
+
+function MailGlue() {
+  this._init();
+}
+
+MailGlue.prototype = {
+  // init (called at app startup)
+  _init: function MailGlue__init() {
+    Services.obs.addObserver(this, "xpcom-shutdown", false);
+    Services.obs.addObserver(this, "final-ui-startup", false);
+  },
+
+  // cleanup (called at shutdown)
+  _dispose: function MailGlue__dispose() {
+    Services.obs.removeObserver(this, "xpcom-shutdown");
+    Services.obs.removeObserver(this, "final-ui-startup");
+  },
+
+  // nsIObserver implementation
+  observe: function MailGlue_observe(aSubject, aTopic, aData) {
+    switch (aTopic) {
+    case "xpcom-shutdown":
+      this._dispose();
+      break;
+    case "final-ui-startup":
+      this._onProfileStartup();
+      break;
+    }
+  },
+
+  _onProfileStartup: function MailGlue__onProfileStartup() {
+    // check if we're in safe mode
+    if (Services.appinfo.inSafeMode) {
+      Services.ww.openWindow(null, "chrome://messenger/content/safeMode.xul", 
+                             "_blank", "chrome,centerscreen,modal,resizable=no", null);
+    }
+  },
+
+  // for XPCOM
+  classID: Components.ID("{eb239c82-fac9-431e-98d7-11cacd0f71b8}"),
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver]),
+};
+
+var components = [MailGlue];
+var NSGetFactory = XPCOMUtils.generateNSGetFactory(components);
