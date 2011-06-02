fd08b85c73aa95a682274a1a67038c5da425c8db
Stefan Plewako <splewako@aviary.pl>
2011-06-02 10:56:33 +0100

    Bug 649989 - display account names in favorite folders pane view only when there are duplicated folder names; r=bienvenu ui-r=bwinton

--- mail/base/content/folderPane.js
+++ mail/base/content/folderPane.js
@@ -1323,6 +1323,7 @@ let gFolderTreeView = {
 
       generateMap: function ftv_favorite_generateMap(ftv) {
         let faves = [];
+        let uniqueNames = [];
         for each (let folder in ftv._enumerateFolders) {
           if (folder.flags & nsMsgFolderFlags.Favorite)
             faves.push(new ftvItem(folder));
@@ -1330,10 +1331,15 @@ let gFolderTreeView = {
 
         // There are no children in this view!
         // And we want to display the account name to distinguish folders w/
-        // the same name.
+        // the same name. (only for folders with duplicated names)
         for each (let folder in faves) {
           folder.__defineGetter__("children", function() []);
-          folder.addServerName = true;
+          if (isNaN(uniqueNames[folder.text]))
+            uniqueNames[folder.text] = 0;
+          uniqueNames[folder.text]++;
+        }
+        for each (let folder in faves) {
+          folder.addServerName = (uniqueNames[folder.text] > 1) ? true : false;
         }
         sortFolderItems(faves);
         return faves;
