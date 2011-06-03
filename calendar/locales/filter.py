d779a2da6d86e5d0459095bfb5c0d01855478074
Pavel Franc <p.franc@gmail.com>
2011-06-03 19:15:09 +0200

    Fix bug 577695 - Filter out the Nounclass strings in compare-locales. r=philipp

---
+++ calendar/locales/filter.py
@@ -0,0 +1,15 @@
+import re
+
+def test(mod, path, entity = None):
+
+  # ignore anyhting but calendar stuff
+  if mod not in ("netwerk", "dom", "toolkit", "security/manager",
+                 "calendar", "other-licenses/branding/sunbird"):
+    return False
+
+  # Noun class entries do not have to be translated
+  if path == "chrome/calendar/calendar-event-dialog.properties":
+    return not re.match(r".*Nounclass[1-9]", entity)
+
+  # Everything else should be taken into account
+  return True
