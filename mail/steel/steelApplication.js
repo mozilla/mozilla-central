e4e2ef4cdec1870a57f84a1fd0241d3059ef55bf
Mark Banner <bugzilla@standard8.plus.com>
2011-06-10 10:08:00 +0100

    Fix mozmill unit test bustage by fixing steelApplication.js which appears to be broken in some instances, by porting bug 658632 from mozilla-central. r=protz

--- mail/steel/steelApplication.js
+++ mail/steel/steelApplication.js
@@ -39,6 +39,9 @@ const Ci = Components.interfaces;
 
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
+const APPLICATION_CID = Components.ID("f265021a-7f1d-4b4b-bdc6-9aedca4d8f13");
+const APPLICATION_CONTRACTID = "@mozilla.org/steel/application;1";
+
 //=================================================
 // Factory - Treat Application as a singleton
 // XXX This is required, because we're registered for the 'JavaScript global
@@ -62,20 +65,22 @@ function Application() {
   this.initToolkitHelpers();
 }
 
+
 Application.prototype = {
-  classID:          Components.ID("f265021a-7f1d-4b4b-bdc6-9aedca4d8f13"),
+  classID: APPLICATION_CID,
 
   // redefine the default factory for XPCOMUtils
   _xpcom_factory: ApplicationFactory,
 
   // for nsISupports
-  QueryInterface : XPCOMUtils.generateQI([Ci.steelIApplication, Ci.extIApplication, Ci.nsIObserver, Ci.nsIClassInfo]),
+  QueryInterface : XPCOMUtils.generateQI([Ci.steelIApplication, Ci.extIApplication, Ci.nsIObserver]),
 
-  getInterfaces : function app_gi(aCount) {
-    let interfaces = [Ci.steelIApplication, Ci.extIApplication, Ci.nsIObserver, Ci.nsIClassInfo];
-    aCount.value = interfaces.length;
-    return interfaces;
-  },
+  classInfo: XPCOMUtils.generateCI({classID: APPLICATION_CID,
+				    contractID: APPLICATION_CONTRACTID,
+				    interfaces: [Ci.steelIApplication,
+						 Ci.extIApplication,
+						 Ci.nsIObserver],
+				    flags: Ci.nsIClassInfo.SINGLETON}),
 
   // for steelIApplication
   platformIsMac: "nsILocalFileMac" in Components.interfaces
