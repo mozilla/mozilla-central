6aea681e621a2266201b89e0ff4ec0b2c9bddc35
Richard Marti <richard.marti@gmail.com>
2011-06-10 12:39:00 +0200

    Fix bug 663503 - Icon missing in Alarms > 'Play a sound' filefield. r=philipp

--- calendar/base/content/preferences/alarms.js
+++ calendar/base/content/preferences/alarms.js
@@ -125,6 +125,7 @@ var gAlarmsPane = {
         soundUrl.value = document.getElementById("calendar.alarms.soundURL").value;
         if (soundUrl.value.indexOf("file://") == 0) {
             soundUrl.label = this.convertURLToLocalFile(soundUrl.value).leafName;
+            soundUrl.image = "moz-icon://" + soundUrl.label + "?size=16";
         } else {
             soundUrl.label = soundUrl.value;
         }
