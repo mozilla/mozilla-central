3c064dd0b4f9d578810d70105dfb52f9c4a841c3
Mark Banner <bugzilla@standard8.plus.com>
2011-06-23 11:57:49 +0100

    Fix xpcshell-test bustage from bug 664441 - add a QI to the test so that the interface can be found. a=bustage-fix

--- mailnews/local/test/unit/test_fileName.js
+++ mailnews/local/test/unit/test_fileName.js
@@ -59,7 +59,8 @@ function run_test() {
   let defaultAccount = acctMgr.defaultAccount;
 
   let pop3Server = acctMgr.FindServer("user", "poptest", "pop3");
-  var rootFolder = gLocalIncomingServer.rootMsgFolder;
+  var rootFolder = gLocalIncomingServer.rootMsgFolder
+    .QueryInterface(Ci.nsIMsgLocalMailFolder);
   let pop3Root = pop3Server.rootMsgFolder;
   
   // Note: Inbox is not created automatically when there is no deferred server,
