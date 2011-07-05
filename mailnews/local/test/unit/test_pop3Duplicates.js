a0dfcfc5c5b5716a51c4a504fc9d1f00a7e4cd09
David Bienvenu <bienvenu@nventure.com>
2011-07-05 08:33:43 -0700

    add simple unit test for deleting incoming pop3 duplicates, r=standard8, bug 668563

---
+++ mailnews/local/test/unit/test_pop3Duplicates.js
@@ -0,0 +1,38 @@
+/**
+ * The intent of this file is to test duplicate handling options
+ * in the pop3 download code.
+ */
+load("../../../resources/POP3pump.js");
+Components.utils.import("resource://gre/modules/Services.jsm");
+
+var testSubjects = ["[Bug 397009] A filter will let me tag, but not untag",
+                    "Hello, did you receive my bugmail?"];
+
+function run_test()
+{
+  // Set duplicate action to be delete duplicates.
+  Services.prefs.setIntPref("mail.server.default.dup_action",
+                            Ci.nsIMsgIncomingServer.deleteDups);
+  // add 3 messages, 2 of which are duplicates.
+  gPOP3Pump.files = ["../../../data/bugmail1",
+                     "../../../data/draft1",
+                     "../../../data/bugmail1"];
+  gPOP3Pump.onDone = continueTest;
+  do_test_pending();
+  gPOP3Pump.run();
+}
+
+function continueTest()
+{
+  // get message headers for the inbox folder
+  let enumerator = gLocalInboxFolder.msgDatabase.EnumerateMessages();
+  var msgCount = 0;
+  while (enumerator.hasMoreElements())
+  {
+    let hdr = enumerator.getNext().QueryInterface(Ci.nsIMsgDBHdr);
+    do_check_eq(hdr.subject, testSubjects[msgCount++]);
+  }
+  do_check_eq(msgCount, 2);
+  gPOP3Pump = null;
+  do_test_finished();
+}
