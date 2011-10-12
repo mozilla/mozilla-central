b7f26c4ae05a3149a685b71e937b510f77e72c73
Mark Banner <bugzilla@standard8.plus.com>
2011-10-12 07:52:18 +0100

    Fix unit test bustage from bug 679513 by specifying prefs to disable the test pilot information tab

--- mail/test/mozmill/runtest.py
+++ mail/test/mozmill/runtest.py
@@ -139,6 +139,8 @@ class ThunderTestProfile(mozrunner.ThunderbirdProfile):
         'mail.startup.enabledMailCheckOnce' :  True,
         # Disable compatibility checking
         'extensions.checkCompatibility.nightly': False,
+        # Disable test pilot new tab (this can be set to anything currently, just needs to be set).
+        'extensions.testpilot.lastversion': '1.0',
         # In case a developer is working on a laptop without a network
         # connection, don't detect offline mode; hence we'll still startup
         # online which is what mozmill currently requires. It'll also protect us
