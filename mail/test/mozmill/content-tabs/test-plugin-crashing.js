20623c42a90f4d48a164012f1a21af4749c4592a
Mark Banner <bugzilla@standard8.plus.com>
2011-10-28 22:18:32 +0100

    Bug 697964 - Port bug 665196 (submit crash report & reload plugin in one step) and bug 688083 (sync submit crash report checkbox between instances) to Thunderbird. r=mconley

--- mail/test/mozmill/content-tabs/test-plugin-crashing.js
+++ mail/test/mozmill/content-tabs/test-plugin-crashing.js
@@ -82,6 +82,8 @@ function setupModule(module) {
 
   let crashReporter = Cc["@mozilla.org/toolkit/crash-reporter;1"]
                         .getService(Ci.nsICrashReporter);
+  crashReporter.enabled = true;
+  crashReporter.submitReports = true;
 
   /* Bug 689580 - these crash tests fail randomly on 64-bit OSX.  We'll
    * disable them for now, until we can figure out what's going on.
