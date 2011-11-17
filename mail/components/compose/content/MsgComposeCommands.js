cd928ef54f25aaa6a9a65f193913d9fd08a7988c
Jim Porter <squibblyflabbetydoo@gmail.com>
2011-11-16 21:14:19 -0600

    Bug 702094 - Dragging attachments from the compose window doesn't work anymore; r=bwinton

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -3641,7 +3641,7 @@ var attachmentBucketDNDObserver = {
   {
     var target = aEvent.target;
 
-    if (target.localName == "listitem")
+    if (target.localName == "attachmentitem")
       aAttachmentData.data = CreateAttachmentTransferData(target.attachment);
   }
 };
