136d1b7252699a70084c7951d902cf2b670ec80c
Jens Hatlak <jh@junetz.de>
2011-11-26 01:14:58 +0100

    Bug 702792 - Port |Bug 700201 - Addons that haven't been updated for an extremely long time should not be compatible by default|. Part 2: Enable compatible-by-default (port bug 698653). r=IanN

--- suite/browser/browser-prefs.js
+++ suite/browser/browser-prefs.js
@@ -548,9 +548,8 @@ pref("app.update.incompatible.mode", 0);
 // Enables some extra Extension System Logging (can reduce performance)
 pref("extensions.logging.enabled", false);
 
-// Enables strict compatibility. To be toggled in bug 698653, to make addons
-// compatible by default.
-pref("extensions.strictCompatibility", true);
+// Disables strict compatibility, making addons compatible-by-default.
+pref("extensions.strictCompatibility", false);
 
 // Specifies a minimum maxVersion an addon needs to say it's compatible with
 // for it to be compatible by default.
