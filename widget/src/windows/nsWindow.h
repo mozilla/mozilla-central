7e3a996e2c3d4199359a14cb06afeb8a6cf196f7
Jim Mathies <jmathies@mozilla.com>
2011-12-07 13:01:48 -0600

    Bug 684555, r=bbondy, a=lsblakk

--- widget/src/windows/nsWindow.h
+++ widget/src/windows/nsWindow.h
@@ -304,6 +304,11 @@ public:
 #endif
 
   NS_IMETHOD              ReparentNativeWidget(nsIWidget* aNewParent);
+
+  // Open file picker tracking
+  void                    PickerOpen();
+  void                    PickerClosed();
+
 protected:
 
   // A magic number to identify the FAKETRACKPOINTSCROLLABLE window created
@@ -529,6 +534,7 @@ protected:
   bool                  mIsRTL;
   bool                  mFullscreenMode;
   bool                  mMousePresent;
+  bool                  mDestroyCalled;
   PRUint32              mBlurSuppressLevel;
   DWORD_PTR             mOldStyle;
   DWORD_PTR             mOldExStyle;
@@ -539,6 +545,7 @@ protected:
   nsSizeMode            mOldSizeMode;
   WindowHook            mWindowHook;
   DWORD                 mAssumeWheelIsZoomUntil;
+  PRUint32              mPickerDisplayCount;
   static bool           sDropShadowEnabled;
   static PRUint32       sInstanceCount;
   static TriStateBool   sCanQuit;
