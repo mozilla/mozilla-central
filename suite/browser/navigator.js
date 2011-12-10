4ee68b4a0eddf1c15aa241c2173b21cb94a3198c
Philip Chee <philip.chee@gmail.com>
2011-12-10 18:27:25 +0800

    Bug 708640 Should be able to open a new tab by middle-clicking the "Go" button r=Neil.

--- suite/browser/navigator.js
+++ suite/browser/navigator.js
@@ -1608,7 +1608,8 @@ function handleURLBarCommand(aUserAction, aTriggeringEvent)
     // Accept both Control and Meta (=Command) as New-Window-Modifiers
     if (aTriggeringEvent &&
         (('ctrlKey' in aTriggeringEvent && aTriggeringEvent.ctrlKey) ||
-         ('metaKey' in aTriggeringEvent && aTriggeringEvent.metaKey))) {
+         ('metaKey' in aTriggeringEvent && aTriggeringEvent.metaKey) ||
+         ('button'  in aTriggeringEvent && aTriggeringEvent.button == 1))) {
       // Check if user requests Tabs instead of windows
       if (GetBoolPref("browser.tabs.opentabfor.urlbar", false)) {
         // Open link in new tab
