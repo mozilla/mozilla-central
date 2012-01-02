ecfffbb979769608d2ec5648ef20d0be1e444eae
Philip Chee <philip.chee@gmail.com>
2012-01-02 17:28:32 +0800

    Bug 713671 addressingWidgetOverlay.js uses non-existent gPromptService r=IanN.

--- suite/mailnews/compose/addressingWidgetOverlay.js
+++ suite/mailnews/compose/addressingWidgetOverlay.js
@@ -50,20 +50,10 @@ var gMimeHeaderParser = null;
 /**
  * global variable inherited from MsgComposeCommands.js
  *
- 
- var sPrefs;
- var gPromptService;
  var gMsgCompose;
- 
  */
 
-var test_addresses_sequence = false;
-
-try {
-  if (sPrefs)
-    test_addresses_sequence = sPrefs.getBoolPref("mail.debug.test_addresses_sequence");
-}
-catch (ex) {}
+var test_addresses_sequence = getPref("mail.debug.test_addresses_sequence");
 
 function awGetMaxRecipients()
 {
@@ -828,7 +818,7 @@ function awRecipientErrorCommand(errItem, element)
 	specificErrString = "Internal error";
     }
 
-    gPromptService.alert(window, generalErrString, specificErrString);
+    Services.prompt.alert(window, generalErrString, specificErrString);
 }
 
 function awRecipientKeyPress(event, element)
