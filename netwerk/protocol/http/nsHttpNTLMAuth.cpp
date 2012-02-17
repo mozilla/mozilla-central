0949935d6934ddb04d4c5de02988247cd08af6bd
Richard van den Berg <richard@vdberg.org>
2012-02-17 16:24:31 +0100

    Bug 452781 - Allow specifying wildcard that matches all simple netbiosnames in network.automatic-ntlm-auth.trusted-uris, r=honzab, a=lsblakk

--- netwerk/protocol/http/nsHttpNTLMAuth.cpp
+++ netwerk/protocol/http/nsHttpNTLMAuth.cpp
@@ -44,6 +44,7 @@
 #include "nsIAuthModule.h"
 #include "nsCOMPtr.h"
 #include "plbase64.h"
+#include "prnetdb.h"
 
 //-----------------------------------------------------------------------------
 
@@ -54,6 +55,7 @@
 #include "nsIURI.h"
 
 static const char kAllowProxies[] = "network.automatic-ntlm-auth.allow-proxies";
+static const char kAllowNonFqdn[] = "network.automatic-ntlm-auth.allow-non-fqdn";
 static const char kTrustedURIs[]  = "network.automatic-ntlm-auth.trusted-uris";
 static const char kForceGeneric[] = "network.auth.force-generic-ntlm";
 
@@ -115,6 +117,20 @@ MatchesBaseURI(const nsCSubstring &matchScheme,
     }
 
     return false;
+}
+
+static bool
+IsNonFqdn(nsIURI *uri)
+{
+    nsCAutoString host;
+    PRNetAddr addr;
+
+    if (NS_FAILED(uri->GetAsciiHost(host)))
+        return false;
+
+    // return true if host does not contain a dot and is not an ip address
+    return !host.IsEmpty() && host.FindChar('.') == kNotFound &&
+           PR_StringToNetAddr(host.BeginReading(), &addr) != PR_SUCCESS;
 }
 
 static bool
@@ -206,6 +222,15 @@ CanUseDefaultCredentials(nsIHttpAuthenticableChannel *channel,
 
     nsCOMPtr<nsIURI> uri;
     channel->GetURI(getter_AddRefs(uri));
+
+    bool allowNonFqdn;
+    if (NS_FAILED(prefs->GetBoolPref(kAllowNonFqdn, &allowNonFqdn)))
+        allowNonFqdn = false;
+    if (allowNonFqdn && uri && IsNonFqdn(uri)) {
+        LOG(("Host is non-fqdn, default credentials are allowed\n"));
+        return true;
+    }
+
     bool isTrustedHost = (uri && TestPref(uri, kTrustedURIs));
     LOG(("Default credentials allowed for host: %d\n", isTrustedHost));
     return isTrustedHost;
