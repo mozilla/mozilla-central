ba6922d3f5272e032c82de08a31f018d3dd116e0
Gary Kwong <gary@rumblingedge.com>
2012-03-02 11:28:42 -0800

    Backed out 6dd1c5192f09. a=wrongFix

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -3962,11 +3962,6 @@ JSObject::TradeGuts(JSContext *cx, JSObject *a, JSObject *b, TradeGutsReserved &
 
     a->newType = newTypeA;
     b->newType = newTypeB;
-
-    if (a->inDictionaryMode())
-        a->isNative() && a->inDictionaryMode();
-    if (b->inDictionaryMode())
-        b->isNative() && b->inDictionaryMode();
 }
 
 /*
