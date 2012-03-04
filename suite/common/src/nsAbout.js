b4292c8e24c302ac9cc5552b3f442c3101bf9348
Neil Rashbrook <neil@parkwaycc.co.uk>
2012-03-04 15:10:03 +0000

    Bug 728478 Consolidate about redirectors r=IanN

--- suite/common/src/nsAbout.js
+++ suite/common/src/nsAbout.js
@@ -35,21 +35,44 @@
  * ***** END LICENSE BLOCK ***** */
 
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("resource://gre/modules/Services.jsm");
+
+const SCRIPT = Components.interfaces.nsIAboutModule.ALLOW_SCRIPT;
+const UNTRUSTED = Components.interfaces.nsIAboutModule.URI_SAFE_FOR_UNTRUSTED_CONTENT;
+const HIDE = Components.interfaces.nsIAboutModule.HIDE_FROM_ABOUTABOUT;
 
 function About() { }
 About.prototype = {
+  Flags: SCRIPT,
+  URI: "chrome://communicator/content/about.xhtml",
+  certerrorFlags: SCRIPT | UNTRUSTED | HIDE,
+  certerrorURI: "chrome://communicator/content/certError.xhtml",
+  dataFlags: SCRIPT,
+  dataURI: "chrome://communicator/content/dataman/dataman.xul",
+  feedsFlags: SCRIPT | UNTRUSTED | HIDE,
+  feedsURI: "chrome://communicator/content/feeds/subscribe.xhtml",
+  lifeFlags: SCRIPT | HIDE,
+  lifeURI: "chrome://communicator/content/aboutLife.xhtml",
+  rightsFlags: SCRIPT | UNTRUSTED,
+  rightsURI: "chrome://branding/content/aboutRights.xhtml",
+  sessionrestoreFlags: SCRIPT | HIDE,
+  sessionrestoreURI: "chrome://communicator/content/aboutSessionRestore.xhtml",
+  synctabsFlags: SCRIPT,
+  synctabsURI: "chrome://communicator/content/aboutSyncTabs.xul",
+
   classID: Components.ID("{d54f2c89-8fd6-4eeb-a7a4-51d4dcdf460f}"),
   QueryInterface: XPCOMUtils.generateQI([Components.interfaces.nsIAboutModule]),
 
   getURIFlags: function(aURI) {
-    return Components.interfaces.nsIAboutModule.ALLOW_SCRIPT;
+    return this[aURI.path.replace(/-|\W.*$/g, "").toLowerCase() + "Flags"];
   },
 
   newChannel: function(aURI) {
-    let ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
-    let channel = ios.newChannel("chrome://communicator/content/about.xhtml",
-                                 null, null);
+    var module = aURI.path.replace(/-|\W.*$/g, "").toLowerCase();
+    var channel = Services.io.newChannel(this[module + "URI"], null, null);
     channel.originalURI = aURI;
+    if (this[module + "Flags"] & UNTRUSTED)
+      channel.owner = Services.scriptSecurityManager.getCodebasePrincipal(aURI);
     return channel;
   }
 };
