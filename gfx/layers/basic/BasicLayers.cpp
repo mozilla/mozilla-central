4a2caf9cc5c4cfded1f21b12575ee039eb6616ba
Robert O'Callahan <robert@ocallahan.org>
2012-03-19 12:36:22 +1300

    Bug 664764. Part 1: Change assertion to warning since it can happen due to resource exhaustion. r=mattwoodrow,a=akeybl

--- gfx/layers/basic/BasicLayers.cpp
+++ gfx/layers/basic/BasicLayers.cpp
@@ -744,9 +744,10 @@ BasicThebesLayer::PaintThebes(gfxContext* aContext,
       Mutated();
     } else {
       // It's possible that state.mRegionToInvalidate is nonempty here,
-      // if we are shrinking the valid region to nothing.
-      NS_ASSERTION(state.mRegionToDraw.IsEmpty(),
-                   "If we need to draw, we should have a context");
+      // if we are shrinking the valid region to nothing. So use mRegionToDraw
+      // instead.
+      NS_WARN_IF_FALSE(state.mRegionToDraw.IsEmpty(),
+                       "No context when we have something to draw; resource exhaustion?");
     }
   }
 
