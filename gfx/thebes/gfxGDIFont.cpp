48fbd4dfb7fef628eb8c6d8b0b9585ecd1d6c652
Jonathan Kew <jfkthame@gmail.com>
2012-04-13 17:39:39 +0100

    bug 744480 - check for failure to set up the cairo font. r=bas a=lsblakk

--- gfx/thebes/gfxGDIFont.cpp
+++ gfx/thebes/gfxGDIFont.cpp
@@ -155,9 +155,13 @@ gfxGDIFont::InitTextRun(gfxContext *aContext,
 
     bool ok = false;
 
-    // ensure the cairo font is set up, so there's no risk it'll fall back to
-    // creating a "toy" font internally (see bug 544617)
-    SetupCairoFont(aContext);
+    // Ensure the cairo font is set up, so there's no risk it'll fall back to
+    // creating a "toy" font internally (see bug 544617).
+    // We must check that this succeeded, otherwise we risk cairo creating the
+    // wrong kind of font internally as a fallback (bug 744480).
+    if (!SetupCairoFont(aContext)) {
+        return false;
+    }
 
     if (mHarfBuzzShaper) {
         if (gfxPlatform::GetPlatform()->UseHarfBuzzForScript(aRunScript)) {
