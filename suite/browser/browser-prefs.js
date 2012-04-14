919fc1fb2dd3d6d3b53edf95beb76625ac0a7afe
Jens Hatlak <jh@junetz.de>
2012-04-15 00:47:25 +0200

    Bug 743312 - Implement click-to-play plugins (port FF bug 711552 and bug 744964). r=Neil

--- suite/browser/browser-prefs.js
+++ suite/browser/browser-prefs.js
@@ -728,6 +728,7 @@ pref("plugins.update.notifyUser", false);
 pref("plugins.hide_infobar_for_outdated_plugin", false);
 pref("plugins.hide_infobar_for_carbon_failure_plugin", false);
 pref("plugins.hide_infobar_for_missing_plugin", false);
+pref("plugins.click_to_play", false);
 pref("plugin.disable", false);
 
 #ifdef XP_MACOSX
