50d2fe6a4a519c17de93a8cb6a69529030c3bf63
Magnus Melin <mkmelin+mozilla@iki.fi>
2012-04-23 21:44:27 +0300

    Bug 425455 - Add tests for: Forwarding inline or replying to a base64-encoded rfc822 attachment results in garbled text. r=standard8

--- mail/test/mozmill/composition/test-eml-actions.js
+++ mail/test/mozmill/composition/test-eml-actions.js
@@ -40,7 +40,7 @@
 
 // make SOLO_TEST=composition/test-eml-actions.js mozmill-one
 
-const MODULE_NAME = "test-reply-to-eml";
+const MODULE_NAME = "test-eml-actions";
 
 const RELATIVE_ROOT = "../shared-modules";
 const MODULE_REQUIRES = ["folder-display-helpers", "window-helpers", "compose-helpers"];
@@ -49,15 +49,10 @@ Cu.import("resource:///modules/mailServices.js");
 var elib = {};
 Cu.import("resource://mozmill/modules/elementslib.js", elib);
 
-var composeHelper;
-
 var setupModule = function(module) {
-  let fdh = collector.getModule("folder-display-helpers");
-  fdh.installInto(module);
-  let wh = collector.getModule("window-helpers");
-  wh.installInto(module);
-  composeHelper = collector.getModule("compose-helpers");
-  composeHelper.installInto(module);
+  collector.getModule("folder-display-helpers").installInto(module);
+  collector.getModule("window-helpers").installInto(module);
+  collector.getModule("compose-helpers").installInto(module);
 }
 
 /**
@@ -66,17 +61,17 @@ var setupModule = function(module) {
  */
 function test_reply_to_eml_save_as_draft() {
   // Open an .eml file.
-  let file = os.getFileForPath(os.abspath("./testmsg.eml", os.getFileForPath(__file__)));
+  let file = os.getFileForPath(
+    os.abspath("./testmsg.eml", os.getFileForPath(__file__)));
   let msgc = open_message_from_file(file);
 
-  let replyWin = composeHelper.open_compose_with_reply(msgc);
+  let replyWin = open_compose_with_reply(msgc);
 
   // Ctrl+S saves as draft.
   replyWin.keypress(null, "s", {shiftKey: false, accelKey: true});
 
-  let acctMgr = Cc["@mozilla.org/messenger/account-manager;1"]
-                  .getService(Ci.nsIMsgAccountManager);
-  let draftsFolder = acctMgr.localFoldersServer.rootFolder.getChildNamed("Drafts");
+  let draftsFolder = MailServices.accounts.localFoldersServer.rootFolder
+                                 .getChildNamed("Drafts");
 
   be_in_folder(draftsFolder);
   let draftMsg = select_click_row(0);
@@ -94,17 +89,17 @@ function test_reply_to_eml_save_as_draft() {
  */
 function test_forward_eml_save_as_draft() {
   // Open an .eml file.
-  let file = os.getFileForPath(os.abspath("./testmsg.eml", os.getFileForPath(__file__)));
+  let file = os.getFileForPath(
+    os.abspath("./testmsg.eml", os.getFileForPath(__file__)));
   let msgc = open_message_from_file(file);
 
-  let replyWin = composeHelper.open_compose_with_forward(msgc);
+  let replyWin = open_compose_with_forward(msgc);
 
   // Ctrl+S saves as draft.
   replyWin.keypress(null, "s", {shiftKey: false, accelKey: true});
 
-  let acctMgr = Cc["@mozilla.org/messenger/account-manager;1"]
-                  .getService(Ci.nsIMsgAccountManager);
-  let draftsFolder = acctMgr.localFoldersServer.rootFolder.getChildNamed("Drafts");
+  let draftsFolder = MailServices.accounts.localFoldersServer.rootFolder
+                                 .getChildNamed("Drafts");
 
   be_in_folder(draftsFolder);
   let draftMsg = select_click_row(0);
@@ -120,13 +115,60 @@ function test_forward_eml_save_as_draft() {
  * Test that MIME encoded subject is decoded when replying to an opened .eml.
  */
 function test_reply_eml_subject() {
-  // Open an .eml file.
-  let file = os.getFileForPath(os.abspath("./mime-encoded-subject.eml", os.getFileForPath(__file__)));
+  // Open an .eml file whose subject is encoded.
+  let file = os.getFileForPath(
+    os.abspath("./mime-encoded-subject.eml", os.getFileForPath(__file__)));
   let msgc = open_message_from_file(file);
 
-  let replyWin = composeHelper.open_compose_with_reply(msgc);
+  let replyWin = open_compose_with_reply(msgc);
 
   assert_equals(replyWin.e("msgSubject").value, "Re: \u2200a\u220aA");
   close_compose_window(replyWin); // close compose window
   close_window(msgc); // close base .eml message
+}
+
+/**
+ * Test that replying to a base64 encoded .eml works.
+ */
+function test_reply_to_base64_eml() {
+  // Open an .eml file.
+  let file = os.getFileForPath(
+    os.abspath("./base64-encoded-msg.eml", os.getFileForPath(__file__)));
+  let msgc = open_message_from_file(file);
+
+  let compWin = open_compose_with_reply(msgc);
+
+  let bodyText = compWin.e("content-frame").contentDocument
+                        .getElementsByTagName("body")[0].textContent;
+
+  const message = "You have decoded this text from base64.";
+  if (bodyText.indexOf(message) == -1)
+    throw new Error("body text didn't contain the decoded text; message=" +
+                    message + ", bodyText=" + bodyText);
+
+  close_compose_window(compWin); 
+  close_window(msgc); 
+}
+
+/**
+ * Test that forwarding a base64 encoded .eml works.
+ */
+function test_forward_base64_eml() {
+  // Open an .eml file.
+  let file = os.getFileForPath(
+    os.abspath("./base64-encoded-msg.eml", os.getFileForPath(__file__)));
+  let msgc = open_message_from_file(file);
+
+  let compWin = open_compose_with_forward(msgc);
+
+  let bodyText = compWin.e("content-frame").contentDocument
+                        .getElementsByTagName("body")[0].textContent;
+
+  const message = "You have decoded this text from base64.";
+  if (bodyText.indexOf(message) == -1)
+    throw new Error("body text didn't contain the decoded text; message=" +
+                    message + ", bodyText=" + bodyText);
+
+  close_compose_window(compWin); 
+  close_window(msgc); 
 }
