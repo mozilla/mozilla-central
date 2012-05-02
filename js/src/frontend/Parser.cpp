b372286eb2f170120a2697f7f46484ac9ce5adca
Jason Orendorff <jorendorff@mozilla.com>
2012-05-02 08:48:31 -0500

    Bug 699682 - Only call checkForKeyword on an atom produced by a NAME token. ("Assertion failure: length != 0".) r=Waldo, a=akeybl.

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -6638,8 +6638,8 @@ Parser::primaryExpr(TokenKind tt, JSBool afterDot)
 
         for (;;) {
             JSAtom *atom;
-            tt = tokenStream.getToken(TSF_KEYWORD_IS_NAME);
-            switch (tt) {
+            TokenKind ltok = tokenStream.getToken(TSF_KEYWORD_IS_NAME);
+            switch (ltok) {
               case TOK_NUMBER:
                 pn3 = NullaryNode::create(tc);
                 if (!pn3)
@@ -6714,15 +6714,9 @@ Parser::primaryExpr(TokenKind tt, JSBool afterDot)
                     atom == context->runtime->atomState.protoAtom) {
                     pn->pn_xflags |= PNX_NONCONST;
                 }
-            } else {
+            }
 #if JS_HAS_DESTRUCTURING_SHORTHAND
-                if (tt != TOK_COMMA && tt != TOK_RC) {
-#endif
-                    reportErrorNumber(NULL, JSREPORT_ERROR, JSMSG_COLON_AFTER_ID);
-                    return NULL;
-#if JS_HAS_DESTRUCTURING_SHORTHAND
-                }
-
+            else if (ltok == TOK_NAME && (tt == TOK_COMMA || tt == TOK_RC)) {
                 /*
                  * Support, e.g., |var {x, y} = o| as destructuring shorthand
                  * for |var {x: x, y: y} = o|, per proposed JS2/ES4 for JS1.8.
@@ -6732,11 +6726,14 @@ Parser::primaryExpr(TokenKind tt, JSBool afterDot)
                     return NULL;
                 pn->pn_xflags |= PNX_DESTRUCT | PNX_NONCONST;
                 pnval = pn3;
-                if (pnval->isKind(TOK_NAME)) {
-                    pnval->setArity(PN_NAME);
-                    ((NameNode *)pnval)->initCommon(tc);
-                }
+                JS_ASSERT(pnval->isKind(TOK_NAME));
+                pnval->setArity(PN_NAME);
+                ((NameNode *)pnval)->initCommon(tc);
+            }
 #endif
+            else {
+                reportErrorNumber(NULL, JSREPORT_ERROR, JSMSG_COLON_AFTER_ID);
+                return NULL;
             }
 
             pn2 = ParseNode::newBinaryOrAppend(TOK_COLON, op, pn3, pnval, tc);
