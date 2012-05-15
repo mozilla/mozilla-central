b0f192255cdce3f614917dabe8920f669d7261e4
Florian Quèze <florian@queze.net>
2012-05-15 12:09:22 +0200

    Bug 742644 - IRC's username empty text ('and server') in the Chat Account Wizard is confusing, r=clokep, ui-r=bwinton.

--- chat/protocols/irc/irc.js
+++ chat/protocols/irc/irc.js
@@ -889,7 +889,6 @@ ircProtocol.prototype = {
   get iconBaseURI() "chrome://prpl-irc/skin/",
   get baseId() "prpl-irc",
 
-  get usernameEmptyText() _("usernameHint"),
   usernameSplits: [
     {label: _("options.server"), separator: "@",
      defaultValue: "chat.freenode.net", reverse: true}
