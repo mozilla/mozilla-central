2730dd41b58dfc825e3a00062f81d745757416e8
Joshua Cranmer <Pidgeot18@gmail.com>
2012-06-07 08:20:26 -0500

    Bug 760126 - Reduce amount of preprocessing in content files, mail/ part. r=asuth
    --HG--
    extra : rebase_source : 62080ad59b1210027cb3b70ae07ee597cf5c8787

--- mail/components/addrbook/content/addressbook.js
+++ mail/components/addrbook/content/addressbook.js
@@ -1,7 +1,7 @@
-# -*- Mode: Javascript; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+/* -*- Mode: Javascript; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 Components.utils.import("resource://gre/modules/Services.jsm");
 // Ensure the activity modules are loaded for this window.
