d4415be3c65bcb89b4f0a490fb3ba99cec9410e1
Florian Quèze <florian@queze.net>
2012-06-14 16:10:06 +0200

    Bug 763857 - Stop printing chat debug messages by default on debug builds, r=bienvenu.

--- mail/components/im/all-im.js
+++ mail/components/im/all-im.js
@@ -6,3 +6,4 @@ pref("messenger.options.messagesStyle.theme", "messenger-messagestyles");
 pref("messenger.options.emoticonsTheme", "messenger-emoticons");
 pref("messenger.conversations.textbox.autoResize", true);
 pref("messenger.conversations.doubleClickToReply", true);
+pref("purple.debug.loglevel", 3);
