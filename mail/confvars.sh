c9de5df6c9de7fe438c42e43caea823576ee97ff
Mark Banner <bugzilla@standard8.plus.com>
2012-07-05 15:25:02 +0100

    Bug 771085 - Define MOZ_APP_BASENAME for Thunderbird to help fix Permanent orange: TEST-UNEXPECTED-FAIL | test_0200_app_launch_apply_update.js failures (and test_0201..., test_0203...). r=mconley

--- mail/confvars.sh
+++ mail/confvars.sh
@@ -3,6 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+MOZ_APP_BASENAME=Thunderbird
 MOZ_APP_NAME=thunderbird
 MOZ_UPDATER=1
 MOZ_THUNDERBIRD=1
