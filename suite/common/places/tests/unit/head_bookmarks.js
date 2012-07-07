92b6904c8c8002647cabcfffd7b1f3c449cc7cd9
Serge Gautherie <sgautherie.bz@free.fr>
2012-07-07 18:20:37 +0200

    Bug 767776. (Av1) Port |Bug 753205 - bookmarks import ignores separators| to SeaMonkey. r=iann.

--- suite/common/places/tests/unit/head_bookmarks.js
+++ suite/common/places/tests/unit/head_bookmarks.js
@@ -61,12 +61,6 @@ let (XULAppInfo = {
                             XULAppInfoFactory);
 }
 
-
-const FILENAME_BOOKMARKS_HTML = "bookmarks.html";
-let (backup_date = new Date().toLocaleFormat("%Y-%m-%d")) {
-  const FILENAME_BOOKMARKS_JSON = "bookmarks-" + backup_date + ".json";
-}
-
 // Smart bookmarks constants.
 const SMART_BOOKMARKS_VERSION = 3;
 // 1 = "Most Visited".
