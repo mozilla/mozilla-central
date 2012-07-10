cb42472ae3712f41b346d3b4074adfc434626216
Daniel Veditz <dveditz@cruzio.com>
2012-07-10 08:27:54 -0700

    Bug 765139, prevent leaking oldDocument, r=peterv, a=dveditz

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -6115,7 +6115,7 @@ nsDocument::AdoptNode(nsIDOMNode *aAdoptedNode, nsIDOMNode **aResult)
     }
   }
 
-  nsIDocument *oldDocument = adoptedNode->OwnerDoc();
+  nsCOMPtr<nsIDocument> oldDocument = adoptedNode->OwnerDoc();
   bool sameDocument = oldDocument == this;
 
   JSContext *cx = nsnull;
