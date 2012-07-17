dc80931f1ee59a90e7e3685da067705b975f9012
Neil Rashbrook <neil@parkwaycc.co.uk>
2012-07-17 21:40:12 +0100

    Bug 774130 Implement desktop background on GNOME r=IanN

--- suite/shell/src/nsGNOMEShellService.cpp
+++ suite/shell/src/nsGNOMEShellService.cpp
@@ -9,11 +9,35 @@
 #include "nsDirectoryServiceUtils.h"
 #include "nsGNOMEShellService.h"
 #include "nsServiceManagerUtils.h"
+#include "nsIGSettingsService.h"
 #include "nsIGConfService.h"
 #include "nsIGnomeVFSService.h"
+#include "nsIStringBundle.h"
+#include "nsIImageLoadingContent.h"
+#include "nsIDOMElement.h"
+#include "imgIRequest.h"
+#include "imgIContainer.h"
+#include "nsIImageToPixbuf.h"
 #include "nsIFile.h"
 #include "nsIProcess.h"
 #include "prenv.h"
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+#include <gdk/gdk.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
+
+// GConf registry key constants
+#define DG_BACKGROUND "/desktop/gnome/background"
+
+#define DGB_OPTIONS DG_BACKGROUND "/picture_options"
+#define DGB_IMAGE DG_BACKGROUND "/picture_filename"
+#define DGB_DRAWBG DG_BACKGROUND "/draw_background"
+
+#define OGDB_SCHEMA "org.gnome.desktop.background"
+#define OGDB_OPTIONS "picture-options"
+#define OGDB_IMAGE "picture-uri"
+#define OGDB_DRAWBG "draw-background"
 
 NS_IMPL_ISUPPORTS1(nsGNOMEShellService, nsIShellService)
 
@@ -78,14 +102,121 @@ nsGNOMEShellService::SetShouldBeDefaultClientFor(PRUint16 aApps)
 NS_IMETHODIMP
 nsGNOMEShellService::GetCanSetDesktopBackground(bool* aResult)
 {
-  return NS_ERROR_NOT_IMPLEMENTED;
+  nsCOMPtr<nsIGConfService> gconf(do_GetService(NS_GCONFSERVICE_CONTRACTID));
+  *aResult = gconf && getenv("GNOME_DESKTOP_SESSION_ID");
+  return NS_OK;
 }
 
 NS_IMETHODIMP
 nsGNOMEShellService::SetDesktopBackground(nsIDOMElement* aElement, 
                                           PRInt32 aPosition)
 {
-  return NS_ERROR_NOT_IMPLEMENTED;
+  // get the product brand name from localized strings
+  nsresult rv;
+  nsString brandName;
+  nsCOMPtr<nsIStringBundleService> bundleService(do_GetService("@mozilla.org/intl/stringbundle;1", &rv));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsCOMPtr<nsIStringBundle> brandBundle;
+  rv = bundleService->CreateBundle(BRAND_PROPERTIES, getter_AddRefs(brandBundle));
+  NS_ENSURE_TRUE(brandBundle, rv);
+
+  rv = brandBundle->GetStringFromName(NS_LITERAL_STRING("brandShortName").get(),
+                                      getter_Copies(brandName));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  // build the file name
+  nsCAutoString filePath(PR_GetEnv("HOME"));
+  filePath.Append('/');
+  filePath.Append(NS_ConvertUTF16toUTF8(brandName));
+  filePath.AppendLiteral("_wallpaper.png");
+
+  // get the image container
+  nsCOMPtr<nsIImageLoadingContent> imageContent(do_QueryInterface(aElement, &rv));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsCOMPtr<imgIRequest> request;
+  rv = imageContent->GetRequest(nsIImageLoadingContent::CURRENT_REQUEST,
+                                getter_AddRefs(request));
+  NS_ENSURE_TRUE(request, rv);
+
+  nsCOMPtr<imgIContainer> container;
+  rv = request->GetImage(getter_AddRefs(container));
+  NS_ENSURE_TRUE(request, rv);
+
+  nsCOMPtr<nsIImageToPixbuf> imgToPixbuf(do_GetService("@mozilla.org/widget/image-to-gdk-pixbuf;1"));
+  if (!imgToPixbuf)
+    return NS_ERROR_NOT_AVAILABLE;
+
+  GdkPixbuf* pixbuf = imgToPixbuf->ConvertImageToPixbuf(container);
+  if (!pixbuf)
+    return NS_ERROR_NOT_AVAILABLE;
+
+  // write the image to a file in the home dir
+  gboolean res = gdk_pixbuf_save(pixbuf, filePath.get(), "png", NULL, NULL);
+
+  g_object_unref(pixbuf);
+  if (!res)
+    return NS_ERROR_FAILURE;
+
+  // set desktop wallpaper filling style
+  const char* options;
+  switch (aPosition) {
+    case BACKGROUND_TILE:
+      options = "wallpaper";
+      break;
+    case BACKGROUND_STRETCH:
+      options = "stretched";
+      break;
+    case BACKGROUND_FILL:
+      options = "zoom";
+      break;
+    case BACKGROUND_FIT:
+      options = "scaled";
+      break;
+    default:
+      options = "centered";
+      break;
+  }
+
+  // Try GSettings first. If we don't have GSettings or the right schema, fall back
+  // to using GConf instead. Note that if GSettings works ok, the changes get
+  // mirrored to GConf by the gsettings->gconf bridge in gnome-settings-daemon
+  nsCOMPtr<nsIGSettingsService> gsettings(do_GetService(NS_GSETTINGSSERVICE_CONTRACTID));
+  if (gsettings) {
+    nsCOMPtr<nsIGSettingsCollection> background_settings;
+    gsettings->GetCollectionForSchema(NS_LITERAL_CSTRING(OGDB_SCHEMA),
+                                      getter_AddRefs(background_settings));
+    if (background_settings) {
+      gchar *file_uri = g_filename_to_uri(filePath.get(), NULL, NULL);
+      if (!file_uri)
+       return NS_ERROR_FAILURE;
+
+      background_settings->SetString(NS_LITERAL_CSTRING(OGDB_OPTIONS),
+                                     nsDependentCString(options));
+      background_settings->SetString(NS_LITERAL_CSTRING(OGDB_IMAGE),
+                                     nsDependentCString(file_uri));
+      g_free(file_uri);
+      background_settings->SetBoolean(NS_LITERAL_CSTRING(OGDB_DRAWBG), true);
+      return NS_OK;
+    }
+  }
+
+  // if the file was written successfully, set it as the system wallpaper
+  nsCOMPtr<nsIGConfService> gconf(do_GetService(NS_GCONFSERVICE_CONTRACTID));
+
+  if (gconf) {
+    gconf->SetString(NS_LITERAL_CSTRING(DGB_OPTIONS), nsDependentCString(options));
+
+    // Set the image to an empty string first to force a refresh (since we could
+    // be writing a new image on top of an existing SeaMonkey_wallpaper.png
+    // and nautilus doesn't monitor the file for changes).
+    gconf->SetString(NS_LITERAL_CSTRING(DGB_IMAGE), EmptyCString());
+    gconf->SetString(NS_LITERAL_CSTRING(DGB_IMAGE), filePath);
+    gconf->SetBool(NS_LITERAL_CSTRING(DGB_DRAWBG), true);
+  }
+
+  return NS_OK;
 }
 
 NS_IMETHODIMP
