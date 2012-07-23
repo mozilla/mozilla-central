2f2e45c1bfd02c443880adab6b9af8ebfdf1a707
Florian Quèze <florian@queze.net>
2012-07-23 15:21:56 -0400

    Bug 767155 - JS error when the contact of an ongoing conversation signs off. r=mconley.

--- mail/components/im/content/chat-messenger-overlay.js
+++ mail/components/im/content/chat-messenger-overlay.js
@@ -240,6 +240,13 @@ var chatHandler = {
       list.selectedItem = elt;
   },
 
+  _hasConversationForContact: function(aContact) {
+    let convs = document.getElementById("conversationsGroup").contacts;
+    return convs.some(function(aConversation)
+      aConversation.hasOwnProperty("imContact") &&
+      aConversation.imContact.id == aContact.id);
+  },
+
   _chatButtonUpdatePending: false,
   updateChatButtonState: function() {
     if (this._chatButtonUpdatePending)
@@ -817,14 +824,18 @@ var chatHandler = {
     }
 
     if (aTopic == "contact-signed-on") {
-      document.getElementById("onlinecontactsGroup").addContact(aSubject);
-      document.getElementById("offlinecontactsGroup").removeContact(aSubject);
+      if (!this._hasConversationForContact(aSubject)) {
+        document.getElementById("onlinecontactsGroup").addContact(aSubject);
+        document.getElementById("offlinecontactsGroup").removeContact(aSubject);
+      }
       this.onlineContacts[aSubject.preferredBuddy.normalizedName] = aSubject;
       return;
     }
     if (aTopic == "contact-signed-off") {
-      document.getElementById("offlinecontactsGroup").addContact(aSubject);
-      document.getElementById("onlinecontactsGroup").removeContact(aSubject);
+      if (!this._hasConversationForContact(aSubject)) {
+        document.getElementById("offlinecontactsGroup").addContact(aSubject);
+        document.getElementById("onlinecontactsGroup").removeContact(aSubject);
+      }
       delete this.onlineContacts[aSubject.preferredBuddy.normalizedName];
       return;
     }
