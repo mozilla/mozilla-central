73e73c0fd76b677281c07c4f22cc42ace9bdac9d
Hiroyuki Ikezoe <hiikezoe@mozilla-japan.org>
2012-08-04 10:07:10 -0400

    Bug 707078 - Add new test. r=mconley

---
+++ mailnews/local/test/unit/test_nsIMsgParseMailMsgState.js
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const MSG_LINEBREAK = "\r\n";
+
+function run_test() {
+  loadLocalMailAccount();
+
+  test_parse_headers_without_crash("./data/mailformed_recipients.eml");
+  test_parse_headers_without_crash("./data/mailformed_subject.eml");
+}
+
+function test_parse_headers_without_crash(eml) {
+  let file = do_get_file(eml);
+
+  let parser = Cc["@mozilla.org/messenger/messagestateparser;1"]
+                 .createInstance(Ci.nsIMsgParseMailMsgState);
+
+  parser.SetMailDB(gLocalInboxFolder.getDatabaseWOReparse());
+  parser.state = Ci.nsIMsgParseMailMsgState.ParseHeadersState;
+
+  let mailData = loadFileToString(file);
+  let lines = mailData.split(MSG_LINEBREAK);
+
+  for (let line = 0; line < lines.length; line++) {
+    parser.ParseAFolderLine(lines[line] + MSG_LINEBREAK, lines[line].length + 2);
+  }
+}
