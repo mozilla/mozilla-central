81e3b22ceffcb76b9e60383174f473bc25d41750
Tony Mechelynck [:tonymec] <antoine.mechelynck@gmail.com>
2012-08-07 16:44:16 -0400

    Bug 768015 - Override Toolkit and increase image.mem.max_decoded_image_kb to 250MB. r=IanN, sr=neil

--- suite/browser/browser-prefs.js
+++ suite/browser/browser-prefs.js
@@ -321,6 +321,11 @@ pref("mail.biff.alert.preview_length", 40);
 
 pref("mailnews.ui.deleteMarksRead", true);
 
+// The maximum amount of decoded image data we'll willingly keep around (we
+// might keep around more than this, but we'll try to get down to this value).
+// (This is intentionally on the high side; see bugs 746055 and 768015.)
+pref("image.mem.max_decoded_image_kb", 256000);
+
 pref("spellchecker.dictionaries.download.url", "chrome://branding/locale/brand.properties");
 
 // this will automatically enable inline spellchecking (if it is available) for
