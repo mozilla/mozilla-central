3ccf7fb347f89f7a466ac10f4392bbb971e83f89
Honza Bambas <honzab.moz@firemni.cz>
2012-08-09 18:46:56 +0200

    Bug 768568, r=kaie, a=lsblakk

--- security/manager/boot/src/nsSecureBrowserUIImpl.h
+++ security/manager/boot/src/nsSecureBrowserUIImpl.h
@@ -131,6 +131,7 @@ protected:
   PRInt32 mSubRequestsBrokenSecurity;
   PRInt32 mSubRequestsNoSecurity;
   bool mRestoreSubrequests;
+  bool mOnLocationChangeSeen;
 #ifdef DEBUG
   /* related to mReentrantMonitor */
   PRInt32 mOnStateLocationChangeReentranceDetection;
