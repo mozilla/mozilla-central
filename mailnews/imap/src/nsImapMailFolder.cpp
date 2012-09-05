a522a9f27b8060b1520d5ec3c8797588df991d99
Ian Neal <iann_cvs@blueyonder.co.uk>
2012-09-05 23:54:03 +0100

    Bug 786595 - Deleted template displays incorrect message body once deleted r=neil

--- mailnews/imap/src/nsImapMailFolder.cpp
+++ mailnews/imap/src/nsImapMailFolder.cpp
@@ -8255,6 +8255,16 @@ nsImapMailFolder::CopyFileToOfflineStore(nsIFile *srcFile, nsMsgKey msgKey)
 
       if (NS_SUCCEEDED(rv) && offlineStore)
       {
+        PRInt64 curOfflineStorePos = 0;
+        nsCOMPtr<nsISeekableStream> seekable = do_QueryInterface(offlineStore);
+        if (seekable)
+          seekable->Tell(&curOfflineStorePos);
+        else
+        {
+          NS_ERROR("needs to be a random store!");
+          return NS_ERROR_FAILURE;
+        }
+
         nsCOMPtr<nsIInputStream> inputStream;
         nsCOMPtr<nsIMsgParseMailMsgState> msgParser =
           do_CreateInstance(NS_PARSEMAILMSGSTATE_CONTRACTID, &rv);
@@ -8292,6 +8302,10 @@ nsImapMailFolder::CopyFileToOfflineStore(nsIFile *srcFile, nsMsgKey msgKey)
 
           msgParser->FinishHeader();
           uint32_t resultFlags;
+          fakeHdr->SetMessageOffset(curOfflineStorePos);
+          char storeToken[100];
+          PR_snprintf(storeToken, sizeof(storeToken), "%lld", curOfflineStorePos);
+          fakeHdr->SetStringProperty("storeToken", storeToken);
           fakeHdr->OrFlags(nsMsgMessageFlags::Offline | nsMsgMessageFlags::Read, &resultFlags);
           fakeHdr->SetOfflineMessageSize(fileSize);
           fakeHdr->SetUint32Property("pseudoHdr", 1);
