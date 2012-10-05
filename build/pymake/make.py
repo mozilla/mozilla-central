08f04a1c91332f67b4967eb3b88c4879c7760585
Siddharth Agarwal <sid.bugzilla@gmail.com>
2012-10-05 19:44:02 +0100

    Bug 787982 Enable pymake on Thunderbird builds - add a wrapper at build/pymake/make.py that pulls in mozilla-central if necessary, and then uses it for the build. r=Standard8. Landing on CLOSED TREE

--- build/pymake/make.py
+++ build/pymake/make.py
@@ -1,17 +1,34 @@
-#!/usr/bin/env python
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# This is a wrapper around mozilla-central's pymake. If that isn't found then
+# this uses client.py to pull it in.
+
 import os
 import sys
 import subprocess
+import shlex
 
-if __name__ == '__main__':
-    print >> sys.stderr, ("Warning: This is a stub that runs GNU make. " +
-        "If you're building locally you'll want to run mozilla-central's pymake.")
-    # Work around bug 777798.
-    if os.environ.has_key("PWD"):
-        del os.environ["PWD"]
+def getpath(relpath):
+    thisdir = os.path.dirname(__file__)
+    return os.path.abspath(os.path.join(thisdir, *relpath))
 
-    subprocess.check_call(["make"] + sys.argv[1:])
+PYMAKE = getpath(["..", "..", "mozilla", "build", "pymake", "make.py"])
+CLIENT_PY = getpath(["..", "..", "client.py"])
+CLIENT_PY_ARGS = getpath(["..", "client.py-args"])
+
+def main(args):
+    if not os.path.exists(PYMAKE):
+        clientpyargs = open(CLIENT_PY_ARGS, "r").read().strip()
+        clientpyargs = shlex.split(clientpyargs)
+        subprocess.check_call([sys.executable, CLIENT_PY, "checkout"] +
+                              clientpyargs)
+
+        if not os.path.exists(PYMAKE):
+            raise Exception("Pymake not found even after client.py was run")
+
+    subprocess.check_call([sys.executable, PYMAKE] + args)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
