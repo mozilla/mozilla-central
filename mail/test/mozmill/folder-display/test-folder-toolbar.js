a7635da656863a660d3148bced765c56e8fc6031
aceman <acelists@atlas.sk>
2013-01-13 09:41:45 -0500

    Bug 813218 - Fix padding in the Folder picker in the Toolbar widget. ui-r=bwinton, r=mkmelin

--- mail/test/mozmill/folder-display/test-folder-toolbar.js
+++ mail/test/mozmill/folder-display/test-folder-toolbar.js
@@ -26,6 +26,43 @@ function setupModule(module)
   make_new_sets_in_folder(folderB, [{count: 1}]);
 }
 
+/**
+ * Returns the value of an individual entity in a DTD file.
+ * Code borrowed from https://hg.mozilla.org/qa/mozmill-tests/file/lib/utils.js.
+ * This function could be put into some shared module if more tests need it.
+ *
+ * @param [string] urls
+ *        Array of DTD urls.
+ * @param {string} entityId
+ *        The ID of the entity to get the value of.
+ *
+ * @return The value of the requested entity
+ * @type string
+ */
+function getEntity(urls, entityId) {
+  // Add xhtml11.dtd to prevent missing entity errors with XHTML files
+  urls.push("resource:///res/dtd/xhtml11.dtd");
+
+  // Build a string of external entities
+  var extEntities = "";
+  for (i = 0; i < urls.length; i++) {
+    extEntities += '<!ENTITY % dtd' + i + ' SYSTEM "' +
+    urls[i] + '">%dtd' + i + ';';
+  }
+
+  var parser = Cc["@mozilla.org/xmlextras/domparser;1"]
+                 .createInstance(Ci.nsIDOMParser);
+  var header = '<?xml version="1.0"?><!DOCTYPE elem [' + extEntities + ']>';
+  var elem = '<elem id="elementID">&' + entityId + ';</elem>';
+  var doc = parser.parseFromString(header + elem, 'text/xml');
+  var elemNode = doc.querySelector('elem[id="elementID"]');
+
+  if (elemNode == null)
+    throw new Error(arguments.callee.name + ": Unknown entity - " + entityId);
+
+  return elemNode.textContent;
+}
+
 function test_add_folder_toolbar()
 {
   // It should not be present by default
@@ -37,9 +74,11 @@ function test_add_folder_toolbar()
   folderLoc = mc.eid("locationFolders");
   mc.assertNode(folderLoc);
 
+  let defaultLabel = getEntity(["chrome://messenger/locale/messenger.dtd"],
+                               "folderLocationToolbarItem.title");
   // XXX I'm not sure we actually want this behavior...
-  assert_equals(folderLoc.node.label, " ",
-                "Uninitialized Folder doesn't have a blank label.");
+  assert_equals(folderLoc.node.label, defaultLabel,
+                "Uninitialized Folder doesn't have the default label.");
 }
 
 function test_folder_toolbar_shows_correct_item()
