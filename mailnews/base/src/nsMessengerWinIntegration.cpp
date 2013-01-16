9d8b2a665311589dd968e4fd47b14cbc9da670b3
Philip Chee <philip.chee@gmail.com>
2013-01-16 23:53:32 +0800

    Bug 831300 - Cannot build MailNews after mozilla-central Bug 786533 rs=Standard8 r=bustage. Additional fix for comm-central/mailnews/
    See Bug 786533 Replace NS_MIN/NS_MAX with std::min/std::max and #include <algorithm> where needed.

--- mailnews/base/src/nsMessengerWinIntegration.cpp
+++ mailnews/base/src/nsMessengerWinIntegration.cpp
@@ -3,6 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Suppress windef.h min and max macros - they make std::min/max not compile.
+#define NOMINMAX 1
+
 #include <windows.h>
 #include <shellapi.h>
 
