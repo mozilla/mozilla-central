953d9b18d3b6ad1b795cfe4040c5c89e0c414194
Javi Rueda <leofigueres@yahoo.com>
2013-01-25 11:38:48 -0500

    Bug 530099 - Remove extensions.getMore* preferences. r=bwinton

--- mail/app/profile/all-thunderbird.js
+++ mail/app/profile/all-thunderbird.js
@@ -195,7 +195,6 @@ pref("extensions.update.url", "https://versioncheck.addons.mozilla.org/update/Ve
 pref("extensions.update.interval", 86400);  // Check for updates to Extensions and 
                                             // Themes every day
 
-pref("extensions.getMoreThemesURL", "https://addons.mozilla.org/%LOCALE%/%APP%/themes/");
 pref("extensions.dss.enabled", false);          // Dynamic Skin Switching                                               
 pref("extensions.dss.switchPending", false);    // Non-dynamic switch pending after next
 
