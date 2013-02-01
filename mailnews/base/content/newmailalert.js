f14240ea431bdeb95c94fbedc85ef69ab67134d8
Philip Chee <philip.chee@gmail.com>
2013-02-01 21:37:17 +0800

    Bug 831737 Re-write and simplify newmailalert.{js|xul|css} (Backport changes from SeaMonkey Bug 404580) ui-r=paenglab r=mkmelin.
    --HG--
    rename : suite/mailnews/newmailalert.css => mailnews/base/content/newmailalert.css
    rename : suite/mailnews/newmailalert.js => mailnews/base/content/newmailalert.js
    rename : suite/mailnews/newmailalert.xul => mailnews/base/content/newmailalert.xul

--- mailnews/base/content/newmailalert.js
+++ mailnews/base/content/newmailalert.js
@@ -22,10 +22,10 @@ function prefillAlertInfo()
   // unwrap all the args....
   // arguments[0] --> array of folders with new mail
   // arguments[1] --> the observer to call back with notifications about the alert
-  // arguments[2] --> user initiated boolean. true if the user initiated opening the alert 
+  // arguments[2] --> user initiated boolean. true if the user initiated opening the alert
   //                 (which means skip the fade effect and don't auto close the alert)
   // arguments[3] --> the alert origin returned by the look and feel
-  var foldersWithNewMail = window.arguments[0];  
+  var foldersWithNewMail = window.arguments[0];
   gAlertListener = window.arguments[1];
   gUserInitiated = window.arguments[2];
   gOrigin = window.arguments[3];
@@ -46,7 +46,7 @@ function prefillAlertInfo()
   var message = totalNumNewMessages == 1 ? "newMailNotification_message"
                                          : "newMailNotification_messages";
   label.value = document.getElementById('bundle_messenger')
-                        .getFormattedString(message, 
+                        .getFormattedString(message,
                                             [rootFolder.prettiestName, totalNumNewMessages]);
 
   // This is really the root folder and we have to walk through the list to
@@ -100,28 +100,33 @@ function onAlertLoad()
 {
   prefillAlertInfo();
   // read out our initial settings from prefs.
-  try 
+  try
   {
     gOpenTime = Services.prefs.getIntPref("alerts.totalOpenTime");
   } catch (ex) {}
-  
+
   // bogus call to make sure the window is moved offscreen until we are ready for it.
   resizeAlert(true);
 
-  // if we aren't waiting to fetch preview text, then go ahead and 
+  // if we aren't waiting to fetch preview text, then go ahead and
   // start showing the alert.
   if (!gPendingPreviewFetchRequests)
-    setTimeout(showAlert, 0); // let the JS thread unwind, to give layout 
+    setTimeout(showAlert, 0); // let the JS thread unwind, to give layout
                               // a chance to recompute the styles and widths for our alert text.
 }
 
 // If the user initiated the alert, show it right away, otherwise start opening the alert with
-// the fade effect. 
+// the fade effect.
 function showAlert()
 {
-  // resize the alert based on our current content  
+  if (!document.getElementById("folderSummaryInfo").hasMessages) {
+    closeAlert(); // no mail, so don't bother showing the alert...
+    return;
+  }
+
+  // resize the alert based on our current content
   resizeAlert(false);
-  
+
   var alertContainer = document.getElementById("alertContainer");
   // Don't fade in if the user opened the alert or the pref is true.
   if (gUserInitiated ||
@@ -130,22 +135,15 @@ function showAlert()
     setTimeout(closeAlert, gOpenTime);
     return;
   }
-  
-  if (document.getElementById('folderSummaryInfo').hasMessages)
-  {
-    alertContainer.addEventListener("animationend", function hideAlert(event) {
-      if (event.animationName == "alert-animation") {
-        alertContainer.removeEventListener("animationend", hideAlert, false);
-        let remaining = Math.max(Math.round(gOpenTime - event.elapsedTime * 1000), 0);
-        setTimeout(closeAlert, remaining);
-      }
-    }, false);
-    alertContainer.setAttribute("animate", true);
-  }
-  else
-  {
-    closeAlert(); // no mail, so don't bother showing the alert...
-  }
+
+  alertContainer.addEventListener("animationend", function hideAlert(event) {
+    if (event.animationName == "fade-in") {
+      alertContainer.removeEventListener("animationend", hideAlert, false);
+      let remaining = Math.max(Math.round(gOpenTime - event.elapsedTime * 1000), 0);
+      setTimeout(fadeOutAlert, remaining);
+    }
+  }, false);
+  alertContainer.setAttribute("fade-in", true);
 }
 
 function resizeAlert(aMoveOffScreen)
@@ -155,7 +153,7 @@ function resizeAlert(aMoveOffScreen)
   alertImageBox.style.minHeight = alertTextBox.scrollHeight + "px";
 
   sizeToContent();
-  
+
   // leftover hack to get the window properly hidden when we first open it
   if (aMoveOffScreen)
     window.outerHeight = 1;
@@ -173,9 +171,21 @@ function resizeAlert(aMoveOffScreen)
   window.moveTo(x, y);
 }
 
+function fadeOutAlert()
+{
+  var alertContainer = document.getElementById("alertContainer");
+  alertContainer.addEventListener("animationend", function fadeOut(event) {
+    if (event.animationName == "fade-out") {
+      alertContainer.removeEventListener("animationend", fadeOut, false);
+      closeAlert();
+    }
+  }, false);
+  alertContainer.setAttribute("fade-out", true);
+}
+
 function closeAlert()
 {
   if (gAlertListener)
-    gAlertListener.observe(null, "alertfinished", ""); 
-  window.close(); 
+    gAlertListener.observe(null, "alertfinished", "");
+  window.close();
 }
