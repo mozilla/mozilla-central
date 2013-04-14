f0c8645a2238659b23346200ceff81c675053eeb
rsx11m <rsx11m.pub@gmail.com>
2013-04-13 21:32:23 -0500

    Bug 856454 - Add ability to customize new mail alert information. ui-r=Neil, r=IanN

--- suite/mailnews/prefs/pref-notifications.js
+++ suite/mailnews/prefs/pref-notifications.js
@@ -11,17 +11,47 @@ function Startup()
 {
   // if we don't have the alert service, hide the pref UI for using alerts to notify on new mail
   // see bug #158711
-  var newMailNotificationAlertUI = document.getElementById("newMailNotificationAlert");
+  var newMailNotificationAlertUI = document.getElementById("newMailNotificationAlertBox");
   newMailNotificationAlertUI.hidden = !("@mozilla.org/alerts-service;1" in Components.classes);
-  if (!/Mac/.test(navigator.platform))
-    document.getElementById('newMailNotificationBounce').setAttribute("hidden", true);
+
+  // animate dock icon option currently available for Mac OSX only
+  var newMailNotificationBouncePref = document.getElementById("newMailNotificationBounceBox");
+  newMailNotificationBouncePref.hidden = !navigator.platform.startsWith("Mac");
+
   // show tray icon option currently available for Windows only
-  var newMailNotificationTrayIconPref = document.getElementById("newMailNotificationTrayIcon");
-  newMailNotificationTrayIconPref.hidden = !/^Win/.test(navigator.platform);
+  var newMailNotificationTrayIconPref = document.getElementById("newMailNotificationTrayIconBox");
+  newMailNotificationTrayIconPref.hidden = !navigator.platform.startsWith("Win");
+
+  EnableAlert(document.getElementById("mail.biff.show_alert").value);
+  EnableTrayIcon(document.getElementById("mail.biff.show_tray_icon").value);
 
   gSoundUrlPref = document.getElementById("mail.biff.play_sound.url");
 
   PlaySoundCheck(document.getElementById("mail.biff.play_sound").value);
+}
+
+function EnableAlert(aEnable)
+{
+  // switch off the balloon if the user wants regular alerts
+  if (aEnable)
+    document.getElementById("mail.biff.show_balloon").value = false;
+
+  EnableElementById("showAlertTime", aEnable, false);
+  EnableElementById("showAlertPreviewText", aEnable, false);
+  EnableElementById("showAlertSubject", aEnable, false);
+  EnableElementById("showAlertSender", aEnable, false);
+}
+
+function EnableTrayIcon(aEnable)
+{
+  EnableElementById("newMailNotificationBalloon", aEnable, false);
+}
+
+function ClearAlert(aEnable)
+{
+  // switch off the regular alerts if the user wants the balloon
+  if (aEnable)
+    document.getElementById("mail.biff.show_alert").value = false;
 }
 
 function PlaySoundCheck(aPlaySound)
