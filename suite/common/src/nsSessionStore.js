0338f79a92071694deb152f7269d289b70a320ee
Phoenix <pppx@i.com.ua>
2013-06-23 15:30:18 +0300

    Bug 886113 - Port | Bug 871057 - Simplify session store initialization | to SeaMonkey. r=IanN

--- suite/common/src/nsSessionStore.js
+++ suite/common/src/nsSessionStore.js
@@ -325,22 +325,17 @@ SessionStoreService.prototype = {
     if (!this._initialized)
       this.initService();
 
-    if (!aWindow || this._loadState == STATE_RUNNING) {
-      // make sure that all browser windows which try to initialize
-      // SessionStore are really tracked by it
-      if (aWindow && (!aWindow.__SSi || !this._windows[aWindow.__SSi]))
-        this.onLoad(aWindow);
-      // If init is being called with a null window, it's possible that we
-      // just want to tell sessionstore that a session is live (as is the case
-      // with starting Firefox with -private, for example; see bug 568816),
-      // so we should mark the load state as running to make sure that
-      // things like setBrowserState calls will succeed in restoring the session.
-      if (!aWindow && this._loadState == STATE_STOPPED)
+    // If init is being called with a null window, it's possible that we
+    // just want to tell sessionstore that a session is live (as is the case
+    // with starting Firefox with -private, for example; see bug 568816),
+    // so we should mark the load state as running to make sure that
+    // things like setBrowserState calls will succeed in restoring the session.
+    if (!aWindow) {
+      if (this._loadState == STATE_STOPPED)
         this._loadState = STATE_RUNNING;
       return;
     }
 
-    // As this is called at delayedStartup, restoration must be initiated here
     this.onLoad(aWindow);
   },
 
