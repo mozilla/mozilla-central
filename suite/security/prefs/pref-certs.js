ec00da7ad0e317b785803028e58d5b31f5b87e9e
Neil Rashbrook <neil@parkwaycc.co.uk>
2013-07-10 21:21:14 +0100

    Bug 886099 Remove "Manage CRLs..." button from Preferences r=IanN

--- suite/security/prefs/pref-certs.js
+++ suite/security/prefs/pref-certs.js
@@ -4,6 +4,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+function Startup()
+{
+  var securityOCSPEnabled = document.getElementById("security.OCSP.enabled");
+  DoEnabling(securityOCSPEnabled.value);
+}
+
+function DoEnabling(aOCSPPrefValue)
+{
+  EnableElementById("requireWorkingOCSP", aOCSPPrefValue != 0, false);
+}
+
 function OpenCertManager()
 {
     document.documentElement
