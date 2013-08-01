f265847bb3268ee23c74eef754e7d40ce525d496
Mark Banner <bugzilla@standard8.plus.com>
2013-08-01 21:50:29 +0100

    Follow-up to bug 896738, add IOUtils import for compose tests to fix broken tests. a=bustage-fix for CLOSED TREE

--- mailnews/compose/test/unit/head_compose.js
+++ mailnews/compose/test/unit/head_compose.js
@@ -1,5 +1,6 @@
 Components.utils.import("resource://gre/modules/Services.jsm");
 Components.utils.import("resource:///modules/mailServices.js");
+Components.utils.import("resource:///modules/IOUtils.js");
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 Components.utils.import("resource://testing-common/mailnews/mailDirService.js");
 Components.utils.import("resource://testing-common/mailnews/mailTestUtils.js");
