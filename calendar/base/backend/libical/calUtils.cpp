08aeab83e40eebbcad5ad044d2e2c0a603a953e9
Mark Banner <bugzilla@standard8.plus.com>
2013-08-02 10:29:44 +0100

    Attempt to fix bustage in calUtils.cpp from bug 897945 - move the include so that strings definitions are included before nsIScriptError.h. a=bustage fix for CLOSED TREE

--- calendar/base/backend/libical/calUtils.cpp
+++ calendar/base/backend/libical/calUtils.cpp
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include "nsComponentManagerUtils.h"
-#include "nsIScriptError.h"
 
 #include "calUtils.h"
+#include "nsIScriptError.h"
 
 extern "C" {
 #include "ical.h"
