d5b5edb3cfb4fe329e01fcc2fa89de093cf79c69
Joshua Cranmer <Pidgeot18@gmail.com>
2013-08-02 12:05:48 -0500

    Fix build bustage from bug 881323, rs=NeilAway a=bustage fix for a CLOSED TREE.

--- mailnews/base/src/nsMsgRDFDataSource.cpp
+++ mailnews/base/src/nsMsgRDFDataSource.cpp
@@ -61,6 +61,8 @@ void nsMsgRDFDataSource::Cleanup()
   mInitialized = false;
 }
 
+NS_IMPL_CYCLE_COLLECTION_CLASS(nsMsgRDFDataSource)
+
 NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(nsMsgRDFDataSource)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mObservers)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mWindow)
