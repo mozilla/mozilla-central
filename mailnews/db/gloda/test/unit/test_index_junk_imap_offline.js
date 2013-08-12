da056205d6d533095174a6819e8bbf3984b9d951
Mark Banner <bugzilla@standard8.plus.com>
2013-08-12 14:49:53 +0100

    Bug 903402 Workaround errors appearing on the console when the cache service tries to get cachePDir which isn't defined for non-mobile platforms. r=jcranmer,a=part bustage fix for CLOSED TREE

--- mailnews/db/gloda/test/unit/test_index_junk_imap_offline.js
+++ mailnews/db/gloda/test/unit/test_index_junk_imap_offline.js
@@ -4,6 +4,15 @@
 load("base_index_junk.js");
 
 function run_test() {
+  // Set these preferences to stop the cache value "cachePDir" being fetched. This
+  // avoids errors on the javascript console, for which the test would otherwise fail.
+  // See bug 903402 for follow-up information.
+  Services.prefs.setComplexValue("browser.cache.disk.parent_directory",
+                                 Ci.nsIFile, do_get_profile());
+  Services.prefs.setComplexValue("browser.cache.offline.parent_directory",
+                                 Ci.nsIFile, do_get_profile());
+
   configure_message_injection({mode: "imap", offline: true});
   glodaHelperRunTests(tests);
 }
+
