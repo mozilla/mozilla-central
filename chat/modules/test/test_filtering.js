3881e861e6813ced6e9625f509f1b3ec6cfae25a
Florian Quèze <florian@queze.net>
2013-08-12 12:11:39 +0100

    Fix chat unit test bustage from bug 812995 Support 'blink' value at -moz-text-decoration-line and drop -moz-text-blink - rs=bustage fix for CLOSED TREE

--- chat/modules/test/test_filtering.js
+++ chat/modules/test/test_filtering.js
@@ -200,13 +200,14 @@ function test_permissiveMode() {
   }
 
   // Allow hr
-  string = "foo<hr>bar";
+  let string = "foo<hr>bar";
   do_check_eq(string, cleanupImMarkup(string));
 
   // Allow most CSS rules changing the text appearance.
   const okCSS = [
     "font-style: italic",
     "font-weight: bold",
+    "text-decoration: underline",
     "color: pink;",
     "font-family: Times",
     "font-size: larger"
@@ -220,11 +221,6 @@ function test_permissiveMode() {
   do_check_eq("<span style=\"font-family: normal; font-style: normal;" +
               " font-weight: normal; font-size: 15px;\">foo</span>",
               cleanupImMarkup("<span style=\"font: 15px normal\">foo</span>"));
-  // text-decoration is a shorthand for several -moz-text-decoration properties.
-  do_check_eq("<span style=\"-moz-text-decoration-color: -moz-use-text-color;" +
-              " -moz-text-decoration-line: underline;" +
-              " -moz-text-decoration-style: solid;\">foo</span>",
-              cleanupImMarkup("<span style=\"text-decoration: underline\">foo</span>"));
 
   // But still filter out dangerous CSS rules.
   const badCSS = [
