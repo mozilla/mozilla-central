3613c7846f22babe66bb4db5007c86e0b6bb9429
aceman <acelists@atlas.sk>
2013-10-10 10:41:38 -0400

    Bug 922614 - Only show "Newsgroup:" adressee type in compose window if there exists a newsgroup account defined in the TB profile. r=mkmelin, a=RyanVM CLOSED TREE

--- mail/test/mozmill/composition/test-address-widgets.js
+++ mail/test/mozmill/composition/test-address-widgets.js
@@ -25,30 +25,9 @@ function setupModule(module) {
   // up for this test.
   let server = MailServices.accounts.FindServer("tinderbox", "tinderbox", "pop3");
   accountPOP3 = MailServices.accounts.FindAccountForServer(server);
-
-  // There may be pre-existing accounts from other tests.
-  originalAccountCount = MailServices.accounts.allServers.length;
-
-  // Create a NNTP server
-  let nntpServer = MailServices.accounts
-    .createIncomingServer(null, "example.nntp.invalid", "nntp")
-    .QueryInterface(Components.interfaces.nsINntpIncomingServer);
-
-  identity = MailServices.accounts.createIdentity();
-  identity.email = "tinderbox2@example.invalid";
-
-  accountNNTP = MailServices.accounts.createAccount();
-  accountNNTP.incomingServer = nntpServer;
-  accountNNTP.addIdentity(identity);
-  // Now there should be 1 more account.
-  assert_equals(MailServices.accounts.allServers.length, originalAccountCount + 1);
 };
 
 function teardownModule(module) {
-  // Remove our test accounts to leave the profile clean.
-  MailServices.accounts.removeAccount(accountNNTP);
-  // There should be only the original accounts left.
-  assert_equals(MailServices.accounts.allServers.length, originalAccountCount);
 }
 
 /**
@@ -71,56 +50,101 @@ function check_address_types_state(aItemsEnabled) {
 }
 
 /**
- * For a POP3 account, no News related address types should be enabled.
+ * With only a POP3 account, no News related address types should be enabled.
  */
-function allowed_mail_types() {
+function check_mail_address_types() {
   check_address_types_state(["addr_to", "addr_cc", "addr_reply", "addr_bcc"]);
 }
 
 /**
- * On a NNTP account, all address types should be enabled.
+ * With a NNTP account, all address types should be enabled.
  */
-function allowed_nntp_types() {
+function check_nntp_address_types() {
   check_address_types_state(["addr_to", "addr_cc", "addr_reply", "addr_bcc",
                              "addr_newsgroups", "addr_followup"]);
 }
 
+function add_NNTP_account() {
+  // There may be pre-existing accounts from other tests.
+  originalAccountCount = MailServices.accounts.allServers.length;
+
+  // Create a NNTP server
+  let nntpServer = MailServices.accounts
+    .createIncomingServer(null, "example.nntp.invalid", "nntp")
+    .QueryInterface(Components.interfaces.nsINntpIncomingServer);
+
+  identity = MailServices.accounts.createIdentity();
+  identity.email = "tinderbox2@example.invalid";
+
+  accountNNTP = MailServices.accounts.createAccount();
+  accountNNTP.incomingServer = nntpServer;
+  accountNNTP.addIdentity(identity);
+  // Now there should be 1 more account.
+  assert_equals(MailServices.accounts.allServers.length, originalAccountCount + 1);
+}
+
+function remove_NNTP_account() {
+  // Remove our NNTP account to leave the profile clean.
+  MailServices.accounts.removeAccount(accountNNTP);
+  // There should be only the original accounts left.
+  assert_equals(MailServices.accounts.allServers.length, originalAccountCount);
+}
+
 /**
- * Bug 399446
+ * Bug 399446 & bug 922614
  * Test that the allowed address types depend on the account type
  * we are sending from.
  */
 function test_address_types() {
-  // Open compose window on each account individually.
+  // Be sure there is no NNTP account yet.
+  for (let account in fixIterator(MailServices.accounts.accounts,
+                                  Components.interfaces.nsIMsgAccount)) {
+    assert_not_equals(account.incomingServer.type, "nntp",
+                      "There is a NNTP account existing unexpectedly");
+  }
+
+  // Open compose window on the existing POP3 account.
   be_in_folder(accountPOP3.incomingServer.rootFolder);
   cwc = open_compose_new_mail();
-  allowed_mail_types();
+  check_mail_address_types();
   close_compose_window(cwc);
 
+  add_NNTP_account();
+
+  // From now on, we should always get all possible address types offered,
+  // regardless of which account is used of composing (bug 922614).
   be_in_folder(accountNNTP.incomingServer.rootFolder);
   cwc = open_compose_new_mail();
-  allowed_nntp_types();
+  check_nntp_address_types();
   close_compose_window(cwc);
 
   // Now try the same accounts but choosing them in the From dropdown
   // inside compose window.
   be_in_folder(accountPOP3.incomingServer.rootFolder);
   cwc = open_compose_new_mail();
-  allowed_mail_types();
+  check_nntp_address_types();
 
   let NNTPidentity = accountNNTP.defaultIdentity.key;
   cwc.click_menus_in_sequence(cwc.e("msgIdentityPopup"), [ { value: NNTPidentity } ]);
-  allowed_nntp_types();
+  check_nntp_address_types();
 
   // In a News account, choose "Newsgroup:" as the address type.
   cwc.click_menus_in_sequence(cwc.e("addressCol1#1").menupopup,
                               [ { value: "addr_newsgroups" } ]);
-  allowed_nntp_types();
+  check_nntp_address_types();
 
   // And switch back to the POP3 account.
   let POP3identity = accountPOP3.defaultIdentity.key;
   cwc.click_menus_in_sequence(cwc.e("msgIdentityPopup"), [ { value: POP3identity } ]);
-  allowed_mail_types();
+  check_nntp_address_types();
 
+  close_compose_window(cwc);
+
+  remove_NNTP_account();
+
+  // Now the NNTP account is lost, so we should be back to mail only addressees.
+  be_in_folder(accountPOP3.incomingServer.rootFolder);
+  cwc = open_compose_new_mail();
+  check_mail_address_types();
   close_compose_window(cwc);
 }
